2076cb2f41a83cde276d933afe0e2c877e3a1d01
fro <frol.rage@gmail.com>
2018-06-27 22:33:32 +0300

    SecurityStore fixes

--- src/stores/SecurityStore.js
+++ src/stores/SecurityStore.js
@@ -31,6 +31,9 @@ export default class SecurityStore extends Container<SecurityState> {
     super(...args);
     this.connectionChangeListener = this.connectionChangeListener.bind(this);
     NetInfo.addEventListener('connectionChange', this.connectionChangeListener);
+    NetInfo.getConnectionInfo().then(info =>
+      this.connectionChangeListener(info)
+    );
   }
 
   connectionChangeListener(connectionInfo) {
