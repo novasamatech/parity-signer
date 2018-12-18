d3fbd7101e7492364b48dcd1c0fec802bbf37c64
fro <frol.rage@gmail.com>
2018-12-18 11:22:27 +0100

    --amend

--- rust/signer/src/eth.rs
+++ rust/signer/src/eth.rs
@@ -63,8 +63,6 @@ pub unsafe extern fn ethkey_keypair_sign(keypair: *mut KeyPair, message: *mut St
   Box::into_raw(Box::new(signature))
 }
 
-
-
 fn blockies_icon_in_base64(seed: Vec<u8>) -> String {
   let mut result = Vec::new();
   let options = ethereum::Options {
