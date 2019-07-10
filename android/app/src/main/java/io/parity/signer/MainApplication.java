e1363ffb7b05df836d146d560b357d336d0be544
YJ <yjkimjunior@gmail.com>
2019-07-10 12:53:28 +0200

    fix: bump minor version

--- android/app/src/main/java/io/parity/signer/MainApplication.java
+++ android/app/src/main/java/io/parity/signer/MainApplication.java
@@ -3,9 +3,6 @@ package io.parity.signer;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.reactlibrary.RNSecureStoragePackage;
-import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,9 +23,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
-            new RNSecureStoragePackage(),
-            new RNCameraPackage(),
             new EthkeyBridgePackage()
       );
     }
