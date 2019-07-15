b91956ea840a55eb4b81220a712b8f9599eff690
YJ <yjkimjunior@gmail.com>
2019-07-15 12:50:36 +0200

    fix: bump babel deps

--- babel.config.js
+++ babel.config.js
@@ -3,10 +3,5 @@ module.exports = {
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-transform-runtime'
   ],
-  presets: [
-    '@babel/preset-env',
-    '@babel/preset-react',
-    'react-native',
-    'module:metro-react-native-babel-preset'
-  ]
+  presets: ['module:metro-react-native-babel-preset']
 };
