926c16adb7d8bf4c7891c7546582d7f17560de34
YJ <yjkimjunior@gmail.com>
2019-07-15 18:47:18 +0200

    fix: react-navigation and jetify to forward all this sheiße to androidX

--- android/app/src/main/java/io/parity/signer/MainApplication.java
+++ android/app/src/main/java/io/parity/signer/MainApplication.java
@@ -8,6 +8,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,7 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new EthkeyBridgePackage()
+          new EthkeyBridgePackage(),
+          new RNGestureHandlerPackage()
       );
     }
 
