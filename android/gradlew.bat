df80d466acdaeac284325d3095c6a7718fb4a4ac
Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
2019-07-16 22:44:36 +0200

    Update android/gradlew.bat
    Co-Authored-By: YJ <yjkimjunior@gmail.com>

--- android/gradlew.bat
+++ android/gradlew.bat
@@ -1,6 +1,6 @@
 
 @rem
-@rem Copyright 2015 the original author or authors.
+@rem Copyright 2019 the original author or authors.
 @rem
 @rem Licensed under the Apache License, Version 2.0 (the "License");
 @rem you may not use this file except in compliance with the License.
