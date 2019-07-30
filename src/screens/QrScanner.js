9005646f9f7a146e199857c91b433528e7ea5cf0
Maciej Hirsz <maciej.hirsz@gmail.com>
2019-07-30 18:51:39 +0200

    feat: Convert rawData to Uint8Array

--- src/screens/QrScanner.js
+++ src/screens/QrScanner.js
@@ -43,6 +43,11 @@ export default class Scanner extends React.PureComponent {
                 if (scannerStore.isBusy()) {
                   return;
                 }
+
+                // TODO: Actually use this to read UOS
+                // type is Uint8Array or null
+                const bytes = rawDataToU8A(txRequestData.rawData);
+
                 try {
                   const data = JSON.parse(txRequestData.data);
                   if (data.action === undefined) {
@@ -207,3 +212,45 @@ const styles = StyleSheet.create({
     paddingBottom: 20,
   }
 });
+
+function rawDataToU8A(rawData) {
+  if (!rawData) {
+    return null;
+  }
+
+  // Strip SQRC filler bytes
+  while (rawData.substr(-4) === 'ec11') {
+    rawData = rawData.substr(0, rawData.length - 4);
+  }
+
+  // Verify that the QR encoding is binary and it's ending with a proper terminator
+  if (rawData.substr(0, 1) !== '4' || rawData.substr(-1) !== '0') {
+    return null;
+  }
+
+  // Strip the encoding indicator and terminator for ease of reading
+  rawData = rawData.substr(1, rawData.length - 2);
+
+  const length8 = parseInt(rawData.substr(0, 2), 16) || 0;
+  const length16 = parseInt(rawData.substr(0, 4), 16) || 0;
+  let length = 0;
+
+  // Strip length prefix
+  if (length8 * 2 + 2 == rawData.length) {
+    rawData = rawData.substr(2);
+    length = length8;
+  } else if (length16 * 2 + 4 == rawData.length) {
+    rawData = rawData.substr(4);
+    length = length16;
+  } else {
+    return null;
+  }
+
+  const bytes = new Uint8Array(length);
+
+  for (let i = 0; i < length; i++) {
+    bytes[i] = parseInt(rawData.substr(i * 2, 2), 16);
+  }
+
+  return bytes;
+}
