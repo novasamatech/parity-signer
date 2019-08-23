cbb91daffa8962593f7c38dca98e79dc2b6bcb36
thib <thibaut.sardan@gmail.com>
2019-08-23 10:17:14 +0200

    fix: add back rn camera

--- android/app/src/main/java/io/parity/signer/MainApplication.java
+++ android/app/src/main/java/io/parity/signer/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.reactlibrary.RNSecureStoragePackage;
+import org.reactnative.camera.RNCameraPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.horcrux.svg.SvgPackage;
 import com.bitgo.randombytes.RandomBytesPackage;
@@ -29,7 +30,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNSecureStoragePackage(),
+          new RNSecureStoragePackage(),
+          new RNCameraPackage(),
           new SvgPackage(),
           new NetInfoPackage(),
           new RandomBytesPackage(),
