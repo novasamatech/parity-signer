22a76c2a7ffc0f0ca41dfb763111bb90d4ade9fd
Kriyszig <lelouch.cpp@gmail.com>
2019-10-14 22:09:51 +0530

    chore: Enable "Substrate Dev" netwok in Dev mode
    * Removed in #384, this brings back the Substrate Dev network when
    __DEV__ is active

--- src/components/PayloadDetailsCard.js
+++ src/components/PayloadDetailsCard.js
@@ -30,7 +30,7 @@ import fonts from '../fonts';
 import colors from '../colors';
 import { SUBSTRATE_NETWORK_LIST, SubstrateNetworkKeys } from '../constants';
 import kusamaMetadata from '../util/static-kusama';
-// import substrateDevMetadata from '../util/static-substrate';
+import substrateDevMetadata from '../util/static-substrate';
 
 export default class PayloadDetailsCard extends React.PureComponent {
 	static propTypes = {
@@ -51,7 +51,6 @@ export default class PayloadDetailsCard extends React.PureComponent {
 		const isKusama =
 			this.props.prefix ===
 			SUBSTRATE_NETWORK_LIST[SubstrateNetworkKeys.KUSAMA].prefix;
-		// const isSubstrateDev = this.props.prefix === SUBSTRATE_NETWORK_LIST[SubstrateNetworkKeys.SUBSTRATE_DEV].prefix;
 
 		let metadata;
 		if (isKusama) {
@@ -61,15 +60,14 @@ export default class PayloadDetailsCard extends React.PureComponent {
 				decimals: SUBSTRATE_NETWORK_LIST[SubstrateNetworkKeys.KUSAMA].decimals,
 				unit: SUBSTRATE_NETWORK_LIST[SubstrateNetworkKeys.KUSAMA].unit
 			});
+		} else if (__DEV__) {
+			metadata = new Metadata(substrateDevMetadata);
+			formatBalance.setDefaults({
+				decimals:
+					SUBSTRATE_NETWORK_LIST[SubstrateNetworkKeys.SUBSTRATE_DEV].decimals,
+				unit: SUBSTRATE_NETWORK_LIST[SubstrateNetworkKeys.SUBSTRATE_DEV].unit
+			});
 		}
-		// } else if (isSubstrateDev) {
-		//   metadata = new Metadata(substrateDevMetadata);
-
-		//   formatBalance.setDefaults({
-		//     decimals: SUBSTRATE_NETWORK_LIST[SubstrateNetworkKeys.SUBSTRATE_DEV].decimals,
-		//     unit: SUBSTRATE_NETWORK_LIST[SubstrateNetworkKeys.SUBSTRATE_DEV].unit
-		//   });
-		// }
 
 		if (!metadata) {
 			this.setState({
