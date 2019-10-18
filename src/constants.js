0efc12cfdf048dfc62baa2aaa5f213c25bf04089
Prateek Nayak <45075669+Kriyszig@users.noreply.github.com>
2019-10-18 14:09:41 +0530

    chore: Enable "Substrate Dev" netwok in Dev mode (#417)
    * chore: Enable "Substrate Dev" netwok in Dev mode
    
    * Removed in #384, this brings back the Substrate Dev network when
    __DEV__ is active
    
    * Added Substrate Dev to AccountNetworkChooser
    
    * Brought back static-substrate.js
    * Added Substrate Dev in menu option
    * Uncommented relavent fields in constants.js
    
    * Uncommented isSubstrateDev

--- src/constants.js
+++ src/constants.js
@@ -25,8 +25,9 @@ export const EthereumNetworkKeys = Object.freeze({
 
 // genesisHash is used as Network key for Substrate networks
 export const SubstrateNetworkKeys = Object.freeze({
-	KUSAMA: '0xe3777fa922cafbff200cadeaea1a76bd7898ad5b89f7848999058b50e715f636' // https://polkascan.io/pre/kusama-cc2/block/0
-	// SUBSTRATE_DEV: '0x4393a679e1830a487e8ae92733f089a80f3e24ba515b08dd8adb40fc6cedee8d', // substrate --dev commit ac6a2a783f0e1f4a814cf2add40275730cd41be1 hosted on wss://dev-node.substrate.dev .
+	KUSAMA: '0xe3777fa922cafbff200cadeaea1a76bd7898ad5b89f7848999058b50e715f636', // https://polkascan.io/pre/kusama-cc2/block/0
+	SUBSTRATE_DEV:
+		'0x4393a679e1830a487e8ae92733f089a80f3e24ba515b08dd8adb40fc6cedee8d' // substrate --dev commit ac6a2a783f0e1f4a814cf2add40275730cd41be1 hosted on wss://dev-node.substrate.dev .
 });
 
 const unknownNetworkBase = {
@@ -46,15 +47,15 @@ const substrateNetworkBase = {
 		prefix: 2,
 		title: 'Kusama CC2',
 		unit: 'KSM'
+	},
+	[SubstrateNetworkKeys.SUBSTRATE_DEV]: {
+		color: '#ff8c00',
+		decimals: 15,
+		genesisHash: SubstrateNetworkKeys.SUBSTRATE_DEV,
+		prefix: 42,
+		title: 'Substrate Development',
+		unit: 'UNIT'
 	}
-	// [SubstrateNetworkKeys.SUBSTRATE_DEV]: {
-	//   color: '#ff8c00',
-	//   decimals: 15,
-	//   genesisHash: SubstrateNetworkKeys.SUBSTRATE_DEV,
-	//   prefix: 42,
-	//   title: 'Substrate Development',
-	//   unit: 'UNIT'
-	// },
 	// [SubstrateNetworkKeys.POLKADOT]: {
 	//   color: '#e6007a',
 	//   decimals: 12,
