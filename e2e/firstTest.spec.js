7f62dd6d721399f9997a717dff1af8a95b04fa32
Hanwen Cheng <heawen.cheng@gmail.com>
2019-10-23 16:15:07 +0200

    remove before each reloading

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -1,10 +1,6 @@
 import testIDs from "./testIDs";
 
 describe('Load test', () => {
-  beforeEach(async () => {
-    await device.reloadReactNative();
-  });
-
   it('should have account list screen', async () => {
     await expect(element(by.id(testIDs.TacScreen.tacView))).toBeVisible();
     await element(by.id(testIDs.TacScreen.agreePrivacyButton)).tap();
