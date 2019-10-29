cb6ddcc96fd50752db95ca0c8534700611f12645
goldsteinsveta <svetlana.goldstein@gmail.com>
2019-10-29 17:00:45 +0100

    font styles component

--- src/fontStyles.js
+++ src/fontStyles.js
@@ -14,13 +14,43 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
+import fonts from './fonts';
+import colors from './colors';
 export default {
-	bold: 'ManifoldCF-Bold',
-	light: 'ManifoldCF-Light',
-	regular: 'Manifold CF',
-	roboto: 'Roboto-regular',
-	robotoBold: 'Roboto-Bold',
-	robotoLight: 'Roboto-Light',
-	robotoMedium: 'Roboto-Medium',
-	semiBold: 'ManifoldCF-DemiBold'
+	h1: {
+		color: colors.bg_text,
+		fontFamily: fonts.robotoBold,
+		fontSize: 22
+	},
+	h2: {
+		color: colors.bg_text,
+		fontFamily: fonts.robotoMedium,
+		fontSize: 18
+	},
+	quote: {
+		color: colors.bg_text,
+		fontFamily: fonts.robotoLight,
+		fontSize: 28
+	},
+	t_big: {
+		color: colors.bg_text,
+		fontFamily: fonts.roboto,
+		fontSize: 16
+	},
+	t_important: {
+		color: colors.bg_text,
+		fontFamily: fonts.robotoBold,
+		fontSize: 12
+	},
+	t_prefix: {
+		color: colors.bg_text,
+		fontFamily: fonts.roboto,
+		fontSize: 14,
+		textTransform: 'uppercase'
+	},
+	t_regular: {
+		color: colors.bg_text,
+		fontFamily: fonts.robotoRegular,
+		fontSize: 12
+	}
 };
