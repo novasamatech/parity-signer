bbe3bf4db398a982f2bfec56476ef8f6a5e3b3ab
Hanwen Cheng <heawen.cheng@gmail.com>
2019-10-30 18:37:25 +0100

    basic account creation for identity backup screen

--- src/screens/IdentityBackup.js
+++ src/screens/IdentityBackup.js
@@ -14,9 +14,23 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Subscribe } from 'unstated';
 import AccountsStore from '../stores/AccountsStore';
+import { words } from '../util/native';
+import {
+	Alert,
+	Clipboard,
+	ScrollView,
+	StyleSheet,
+	Text,
+	View
+} from 'react-native';
+import TouchableItem from '../components/TouchableItem';
+import colors from '../colors';
+import fonts from '../fonts';
+import Button from '../components/Button';
+import { NavigationActions, StackActions } from 'react-navigation';
 
 export default class IdentityBackup extends React.PureComponent {
 	static navigationOptions = {
@@ -31,6 +45,112 @@ export default class IdentityBackup extends React.PureComponent {
 	}
 }
 
-function IdentityBackupView() {
-	return <></>;
+function IdentityBackupView({ navigation }) {
+	const [seed, setSeed] = useState('');
+	useEffect(() => {
+		const setSeedAsync = async () => {
+			const newSeed = await words();
+			setSeed(newSeed);
+		};
+
+		setSeedAsync();
+		return () => {
+			setSeed('');
+		};
+	}, []);
+
+	const resetStack = () => {
+		const resetAction = StackActions.reset({
+			actions: [
+				NavigationActions.navigate({ routeName: 'AccountList' }),
+				NavigationActions.navigate({ routeName: 'IdentityNew' })
+			],
+			index: 1, // FIXME workaround for now, use SwitchNavigator later: https://github.com/react-navigation/react-navigation/issues/1127#issuecomment-295841343
+			key: undefined
+		});
+		navigation.dispatch(resetAction);
+	};
+
+	return (
+		<ScrollView style={styles.body}>
+			<Text style={styles.titleTop}>BACKUP ACCOUNT</Text>
+			<View>
+				<Text style={styles.titleTop}>RECOVERY PHRASE</Text>
+				<Text style={styles.hintText}>
+					Write these words down on paper. Keep the backup paper safe. These
+					words allow anyone to recover this account and access its funds.
+				</Text>
+			</View>
+			<TouchableItem
+				onPress={() => {
+					// only allow the copy of the recovery phrase in dev environment
+					if (__DEV__) {
+						Alert.alert(
+							'Write this recovery phrase on paper',
+							`It is not recommended to transfer or store a recovery phrase digitally and unencrypted. Anyone in possession of this recovery phrase is able to spend funds from this account.
+                `,
+							[
+								{
+									onPress: () => {
+										Clipboard.setString(seed);
+									},
+									style: 'default',
+									text: 'Copy anyway'
+								},
+								{
+									style: 'cancel',
+									text: 'Cancel'
+								}
+							]
+						);
+					}
+				}}
+			>
+				<Text style={styles.seedText}>{seed}</Text>
+			</TouchableItem>
+			<Button
+				title="Next"
+				onPress={() => {
+					resetStack();
+				}}
+			/>
+		</ScrollView>
+	);
 }
+
+const styles = StyleSheet.create({
+	body: {
+		backgroundColor: colors.bg,
+		flex: 1,
+		flexDirection: 'column',
+		padding: 20
+	},
+	hintText: {
+		color: colors.bg_text_sec,
+		fontFamily: fonts.bold,
+		fontSize: 12,
+		paddingBottom: 20,
+		textAlign: 'center'
+	},
+	seedText: {
+		backgroundColor: colors.card_bg,
+		fontFamily: fonts.regular,
+		fontSize: 20,
+		lineHeight: 26,
+		minHeight: 160,
+		padding: 10
+	},
+	title: {
+		color: colors.bg_text_sec,
+		fontFamily: fonts.bold,
+		fontSize: 18,
+		paddingBottom: 20
+	},
+	titleTop: {
+		color: colors.bg_text_sec,
+		fontFamily: fonts.bold,
+		fontSize: 24,
+		paddingBottom: 20,
+		textAlign: 'center'
+	}
+});
