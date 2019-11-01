ada16802a35a388b8e6e7cea707081f7f73758e9
Hanwen Cheng <heawen.cheng@gmail.com>
2019-11-01 17:51:03 +0100

    chore: Eslint now works on root folder (#453)
    * chore: es lint with root folder
    
    * update eslint ignore path

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,9 +1,9 @@
-import Reactotron from "reactotron-react-native";
+import Reactotron from 'reactotron-react-native';
 
 if (__DEV__) {
-  Reactotron.configure({
-    host: "192.168.1.63"
-  }) // controls connection & communication settings
-    .useReactNative() // add all built-in react native plugins
-    .connect(); // let's connect!
+	Reactotron.configure({
+		host: '192.168.1.63'
+	}) // controls connection & communication settings
+		.useReactNative() // add all built-in react native plugins
+		.connect(); // let's connect!
 }
