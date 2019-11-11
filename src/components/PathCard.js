3c69de06491f419db9953bdf584a8ed3c628935d
goldsteinsveta <svetlana.goldstein@gmail.com>
2019-11-11 01:16:58 +0100

    style: pathCard for non-Substrate

--- src/components/PathCard.js
+++ src/components/PathCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import Button from './Button';
 import {
 	getNetworkKeyBySubstratePath,
@@ -8,6 +8,11 @@ import {
 	isSubstratePath
 } from '../util/identitiesUtils';
 import { NETWORK_LIST } from '../constants';
+import Separator from '../components/Separator';
+import AccountIcon from './AccountIcon';
+import Address from './Address';
+import colors from '../colors';
+import fontStyles from '../fontStyles';
 
 PathCard.propTypes = {
 	identity: PropTypes.object.isRequired,
@@ -21,14 +26,72 @@ export default function PathCard({ onPress, identity, path, testID }) {
 	const address = identity.meta.get(path).address;
 
 	const networkKey = getNetworkKeyBySubstratePath(path);
-	const networkParams = NETWORK_LIST[networkKey];
+	const network = NETWORK_LIST[networkKey];
 
 	return (
 		<View testID={testID}>
-			<Text>{networkParams.color.toString()}</Text>
-			<Text>{pathName}</Text>
-			{isSubstratePath(path) && <Button onPress={onPress} title={path} />}
-			<Text>{address}</Text>
+			<Separator
+				shadow={true}
+				style={{
+					backgroundColor: 'transparent',
+					height: 0,
+					marginVertical: 0
+				}}
+			/>
+			<View style={styles.content}>
+				<AccountIcon
+					address={address}
+					protocol={network.protocol}
+					network={network}
+					style={styles.icon}
+				/>
+				<View style={styles.desc}>
+					<View>
+						<Text style={[fontStyles.t_regular, { color: colors.bg_text_sec }]}>
+							{network.title}
+						</Text>
+					</View>
+					<Text numberOfLines={1} style={[fontStyles.h2, { marginTop: -2 }]}>
+						{pathName}
+					</Text>
+					{isSubstratePath(path) && <Button onPress={onPress} title={path} />}
+					<Address address={address} protocol={network.protocol} />
+				</View>
+				<View
+					style={[
+						styles.footer,
+						{
+							backgroundColor: network.color
+						}
+					]}
+				/>
+			</View>
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	content: {
+		alignItems: 'center',
+		backgroundColor: colors.bg,
+		flexDirection: 'row',
+		paddingLeft: 16
+	},
+	desc: {
+		flex: 1,
+		flexDirection: 'column',
+		justifyContent: 'space-between',
+		paddingLeft: 16
+	},
+	footer: {
+		alignSelf: 'stretch',
+		backgroundColor: '#977CF6',
+		height: 100,
+		marginLeft: 8,
+		width: 8
+	},
+	icon: {
+		height: 40,
+		width: 40
+	}
+});
