148786def9efaf73fe91999646f2fb2ade8f5065
Hanwen Cheng <heawen.cheng@gmail.com>
2019-11-19 14:45:05 +0100

    Revert "Merge branch 'hanwen-fix-loading' into sveta-hanwen-hdkd"
    This reverts commit 7fbcfb25eb8cf0be6dc3b95b96267568c58ab582, reversing
    changes made to b03754443c6243811b25e7e5698f0c9a1f7f2d75.

--- src/screens/Loading.js
+++ src/screens/Loading.js
@@ -48,14 +48,14 @@ export default class Loading extends React.PureComponent {
 		const oldAccounts_v2 = await loadAccounts(2);
 		const oldAccounts = [...oldAccounts_v1, ...oldAccounts_v2];
 		const accounts = oldAccounts.map(([_, value]) => {
+			let result = {};
 			if (value.chainId) {
 				// The networkKey for Ethereum accounts is the chain id
-				const result = { ...value, networkKey: value.chainId, recovered: true };
+				result = { ...value, networkKey: value.chainId, recovered: true };
 				delete result.chainId;
 				delete result.networkType;
-				return result;
 			}
-			return value;
+			return result;
 		});
 
 		accounts.forEach(account => {
