fea74cf675b1b3bdc839c4b51de88a9ef3d84bb9
Hanwen Cheng <heawen.cheng@gmail.com>
2019-12-18 18:06:48 +0100

    grammar ...

--- src/components/UnknownAccountWarning.js
+++ src/components/UnknownAccountWarning.js
@@ -30,7 +30,7 @@ export default function UnknownAccountWarning({ isPath }) {
 					The account is now using Polkadot Canary Address Format.
 					{'\n'}
 					In the future user may specify different address formats like Polkadot
-					Live, which will lead to different address.
+					Live, which will lead to a different address.
 				</Text>
 			) : (
 				<Text style={styles.warningText}>
