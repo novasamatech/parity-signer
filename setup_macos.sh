2ed41aaa0bd506294609bd7bcb102ea64af46861
Hanwen Cheng <heawen.cheng@gmail.com>
2020-03-18 23:08:45 +0100

    revert change to rust target

--- setup_macos.sh
+++ setup_macos.sh
@@ -3,10 +3,10 @@
 set -e
 
 # ios
-#rustup target add i386-apple-ios
+rustup target add i386-apple-ios
 rustup target add x86_64-apple-ios
-#rustup target add armv7-apple-ios
-#rustup target add armv7s-apple-ios
+rustup target add armv7-apple-ios
+rustup target add armv7s-apple-ios
 rustup target add aarch64-apple-ios
 
 # android
