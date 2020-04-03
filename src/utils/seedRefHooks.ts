c23f19104c549b4e89ba8c37d331102b3b463724
Joseph Mark <sjeohp@gmail.com>
2020-04-03 10:02:20 +0200

    Add signing functions to seedRefHooks

--- src/utils/seedRefHooks.ts
+++ src/utils/seedRefHooks.ts
@@ -1,11 +1,23 @@
 import { useState } from 'react';
 
-import { decryptDataRef, destroyDataRef } from 'utils/native';
+import {
+	decryptDataRef,
+	destroyDataRef,
+	brainWalletSignWithRef,
+	substrateSignWithRef
+} from 'utils/native';
 
 type TryCreateFunc = (encryptedSeed: string, password: string) => Promise<void>;
 type TryDestroyFunc = () => Promise<void>;
+type TrySignFunc = (message: string) => Promise<string>;
 
-type SeedRefHooks = [number, boolean, TryCreateFunc, TryDestroyFunc];
+type SeedRefHooks = [
+	number,
+	boolean,
+	TryCreateFunc,
+	TryDestroyFunc,
+	TrySignFunc
+];
 
 export function useSeedRef(): SeedRefHooks {
 	const [dataRef, setDataRef] = useState<number>(0);
@@ -13,6 +25,10 @@ export function useSeedRef(): SeedRefHooks {
 
 	// Decrypt a seed and store the reference. Must be called before signing.
 	const create: TryCreateFunc = function (encryptedSeed, password) {
+		// Seed reference was already created.
+		if (valid) {
+			throw new Error('cannot create a seed reference when one already exists');
+		}
 		return decryptDataRef(encryptedSeed, password).then((ref: number) => {
 			setDataRef(ref);
 			setValid(true);
@@ -22,10 +38,34 @@ export function useSeedRef(): SeedRefHooks {
 	// Destroy the decrypted seed. Must be called before this leaves scope or
 	// memory will leak.
 	const destroy: TryDestroyFunc = function () {
+		if (!valid) {
+			// Seed reference was never created or was already destroyed.
+			throw new Error('cannot destroy an invalid seed reference');
+		}
 		return destroyDataRef(dataRef).then(() => {
 			setValid(false);
 		});
 	};
 
-	return [dataRef, valid, create, destroy];
+	// Use the seed reference to sign a message. Will throw an error if
+	// `tryDestroy` has already been called or if `tryCreate` failed.
+	const brainWalletSign: TrySignFunc = function () {
+		if (!valid) {
+			// Seed reference was never created or was already destroyed.
+			throw new Error('cannot sign with an invalid seed reference');
+		}
+		return brainWalletSignWithRef(dataRef, message);
+	};
+
+	// Use the seed reference to sign a message. Will throw an error if
+	// `tryDestroy` has already been called or if `tryCreate` failed.
+	const substrateSign: TrySignFunc = function () {
+		if (!valid) {
+			// Seed reference was never created or was already destroyed.
+			throw new Error('cannot sign with an invalid seed reference');
+		}
+		return substrateSignWithRef(dataRef, message);
+	};
+
+	return [dataRef, valid, create, destroy, brainWalletSign, substrateSign];
 }
