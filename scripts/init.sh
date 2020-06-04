6221e1f39db75b36621884a3c215a42a697257f5
Hanwen Cheng <heawen.cheng@gmail.com>
2020-06-04 15:23:38 +0200

    build: use latest ndk

---
+++ scripts/init.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+source ./scripts/variables.sh
+
+cargo install cargo-lipo
+
+case "$(uname | tr '[:upper:]' '[:lower:]')" in
+   darwin)
+      echo 'Add rust toolchains for android and ios'
+      for i in "${IOS_ARCHS[@]}";
+        do rustup target add "$i";
+      done
+      for i in "${ANDROID_ARCHS[@]}";
+       do rustup target add "$i" ;
+      done
+      ;;
+   linux)
+      echo 'Add rust toolchains for android'
+      for i in "${ANDROID_ARCHS[@]}";
+       do rustup target add "$i" ;
+      done
+     ;;
+   *)
+     echo 'Please use a Linux or Mac to build'
+     ;;
+esac
+
+
+#cargo install cbindgen
+
+#ln -s ./litentry_rust/rust/target/aarch64-linux-android/release/libexample.so ./litentry_app/android/src/main/arm64-v8a/
+#ln -s ./litentry_rust/rust/target/armv7-linux-androideabi/release/libexample.so ./litentry_app/android/src/main/armeabi-v7a/
+#ln -s ./litentry_rust/rust/target/i686-linux-android/release/libexample.so ./litentry_app/android/src/main/x86/
+#
+#ln -s ./litentry_rust/rust/target/universal/release/libexample.a ./litentry_app/ios/
+
+
+
+
