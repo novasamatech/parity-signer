cffbe02320b2d09e2d6ce0c9b2299b598852989c
Hanwen Cheng <heawen.cheng@gmail.com>
2020-06-04 15:34:04 +0200

    use script to generate android debug keystore

--- scripts/init.sh
+++ scripts/init.sh
@@ -24,6 +24,14 @@ case "$(uname | tr '[:upper:]' '[:lower:]')" in
      ;;
 esac
 
+FILE=./android/app/debug.keystore
+if [ -f "$FILE" ]; then
+    echo "$FILE exist, skip.."
+    else
+    echo "generating andriod debug keystore file.."
+    cd ./android/app && keytool -genkey -v -keystore debug.keystore -storepass android -alias androiddebugkey -keypass android -keyalg RSA -keysize 2048 -validity 10000
+
+fi
 
 #cargo install cbindgen
 
