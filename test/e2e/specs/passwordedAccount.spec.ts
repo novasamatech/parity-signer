80cfa0304f0b0ff5f34870c3f2caf17215e22ca5
Hanwen Cheng <heawen.cheng@gmail.com>
2020-06-22 23:36:44 +0200

    fix e2e test

--- test/e2e/specs/passwordedAccount.spec.ts
+++ test/e2e/specs/passwordedAccount.spec.ts
@@ -47,7 +47,6 @@ describe('passworded account test', () => {
 			await testExist(
 				'secret:0xffa534554346807099dfbf034157798cf94541b357a3fe27f37c2175594f4bf5:0xb0a8d493285c2df73290dfb7e61f870f17b41801197a149ca93654499ea3dafe:passworded'
 			);
-			await testVisible(PathDetail.screen);
 		});
 	});
 
