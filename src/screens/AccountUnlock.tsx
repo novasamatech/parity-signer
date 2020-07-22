439efe1f44b8f600230473bf7389a72300234ff5
Hanwen Cheng <heawen.cheng@gmail.com>
2020-07-22 15:59:01 +0200

    remove unstated from account unlock

--- src/screens/AccountUnlock.tsx
+++ src/screens/AccountUnlock.tsx
@@ -14,65 +14,55 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-import React, { useContext } from 'react';
+import React, {useContext, useState} from 'react';
 import { StyleSheet } from 'react-native';
 import { CommonActions } from '@react-navigation/native';
-import { Subscribe } from 'unstated';
 
 import { AccountsContext } from 'stores/AccountsContext';
 import { SafeAreaViewContainer } from 'components/SafeAreaContainer';
+import {ScannerContext} from 'stores/ScannerContext';
 import { NavigationProps } from 'types/props';
 import colors from 'styles/colors';
 import fontStyles from 'styles/fontStyles';
 import ScreenHeading from 'components/ScreenHeading';
 import TextInput from 'components/TextInput';
-import AccountsStore from 'stores/AccountsStore';
-import ScannerStore from 'stores/ScannerStore';
 
 /* Used for unlock and sign tx and messages for legacy accounts */
-export class AccountUnlockAndSign extends React.PureComponent<
-	NavigationProps<'AccountUnlockAndSign'>
-> {
-	render(): React.ReactElement {
-		const { navigation, route } = this.props;
-		const next = route.params.next ?? 'SignedTx';
+export function AccountUnlockAndSign (props: NavigationProps<'AccountUnlockAndSign'>): React.ReactElement{
+	const { navigation, route } = props;
+	const next = route.params.next ?? 'SignedTx';
+	const scannerStore = useContext(ScannerContext);
 
-		return (
-			<Subscribe to={[ScannerStore]}>
-				{(scannerStore: ScannerStore): React.ReactElement => (
-					<AccountUnlockView
-						{...this.props}
-						checkPin={async (pin: string): Promise<boolean> => {
-							try {
-								await scannerStore.signDataLegacy(pin);
-								return true;
-							} catch (e) {
-								return false;
-							}
-						}}
-						navigate={(): void => {
-							const resetAction = CommonActions.reset({
-								index: 1,
-								routes: [
-									{
-										name: 'LegacyAccountList'
-									},
-									{ name: next }
-								]
-							});
-							navigation.dispatch(resetAction);
-						}}
-					/>
-				)}
-			</Subscribe>
-		);
-	}
+	return (
+		<AccountUnlockView
+			checkPin={async (pin: string): Promise<boolean> => {
+				try {
+					await scannerStore.signDataLegacy(pin);
+					return true;
+				} catch (e) {
+					return false;
+				}
+			}}
+			navigate={(): void => {
+				const resetAction = CommonActions.reset({
+					index: 1,
+					routes: [
+						{
+							name: 'LegacyAccountList'
+						},
+						{ name: next }
+					]
+				});
+				navigation.dispatch(resetAction);
+			}}
+		/>
+	)
 }
 
 export function AccountUnlock({
-	navigation,
-	route
-}: NavigationProps<'AccountUnlock'>): React.ReactElement {
+																navigation,
+																route
+															}: NavigationProps<'AccountUnlock'>): React.ReactElement {
 	const next = route.params.next ?? 'LegacyAccountList';
 	const onDelete = route.params.onDelete ?? ((): any => null);
 	const accounts = useContext(AccountsContext);
@@ -115,48 +105,38 @@ interface AccountUnlockViewState {
 	pin: string;
 }
 
-class AccountUnlockView extends React.PureComponent<
-	AccountUnlockViewProps,
-	AccountUnlockViewState
-> {
-	state = {
-		hasWrongPin: false,
-		pin: ''
-	};
+function AccountUnlockView (props: AccountUnlockViewProps): React.ReactElement{
+	const [hasWrongPin, setHasWrongPin] = useState(false);
+	const [pin, setPin] = useState('');
 
-	showErrorMessage = (): string => {
-		return this.state.hasWrongPin ? 'Wrong pin, please try again' : '';
-	};
+	const showErrorMessage = (): string => hasWrongPin ? 'Wrong pin, please try again' : '';
 
-	render(): React.ReactElement {
-		const { checkPin, navigate } = this.props;
-		const { hasWrongPin, pin } = this.state;
+	const { checkPin, navigate } = props;
 
-		return (
-			<SafeAreaViewContainer style={styles.body}>
-				<ScreenHeading
-					title={'Unlock Account'}
-					subtitle={this.showErrorMessage()}
-					error={hasWrongPin}
-				/>
-				<PinInput
-					label="PIN"
-					onChangeText={async (inputPin: string): Promise<void> => {
-						this.setState({ pin: inputPin });
-						if (inputPin.length < 4) {
-							return;
-						}
-						if (await checkPin(inputPin)) {
-							navigate();
-						} else if (inputPin.length > 5) {
-							this.setState({ hasWrongPin: true });
-						}
-					}}
-					value={pin}
-				/>
-			</SafeAreaViewContainer>
-		);
-	}
+	return (
+		<SafeAreaViewContainer style={styles.body}>
+			<ScreenHeading
+				title={'Unlock Account'}
+				subtitle={showErrorMessage()}
+				error={hasWrongPin}
+			/>
+			<PinInput
+				label="PIN"
+				onChangeText={async (inputPin: string): Promise<void> => {
+					setPin(inputPin);
+					if (inputPin.length < 4) {
+						return;
+					}
+					if (await checkPin(inputPin)) {
+						navigate();
+					} else if (inputPin.length > 5) {
+						setHasWrongPin(true);
+					}
+				}}
+				value={pin}
+			/>
+		</SafeAreaViewContainer>
+	);
 }
 
 function PinInput(props: any): React.ReactElement {
