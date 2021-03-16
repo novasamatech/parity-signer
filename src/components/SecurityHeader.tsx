c5ab3494a7a7f42bcf8525b462f644c693741ea3
Raymond Zhong <raykyri@gmail.com>
2021-03-16 14:24:53 -0700

    Remove security warning and header

--- src/components/SecurityHeader.tsx
+++ src/components/SecurityHeader.tsx
@@ -14,40 +14,14 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-import NetInfo from '@react-native-community/netinfo';
-import { StackNavigationProp } from '@react-navigation/stack';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
 
-import ButtonIcon from './ButtonIcon';
-
-import { RootStackParamList } from 'types/routes';
-import colors from 'styles/colors';
 import IdentitiesSwitch from 'components/IdentitiesSwitch';
 
 function SecurityHeader(): React.ReactElement {
-	const [isConnected, setIsConnected] = useState(false);
-	const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
-	useEffect(
-		() =>
-			NetInfo.addEventListener(state => {
-				setIsConnected(state.isConnected);
-			}),
-		[]
-	);
-
 	return (
 		<View style={styles.body}>
-			{isConnected && (
-				<ButtonIcon
-					onPress={(): void => navigation.navigate('Security')}
-					iconName="shield-off"
-					iconType="feather"
-					iconColor={colors.signal.error}
-					iconSize={26}
-				/>
-			)}
 			<IdentitiesSwitch />
 		</View>
 	);
