23671a849b9e264722d2fa66cedcbcf69a27ade4
Raymond Zhong <raykyri@gmail.com>
2021-03-17 01:27:40 -0700

    Fix rendering

--- src/App.tsx
+++ src/App.tsx
@@ -73,7 +73,7 @@ export default function App(props: AppProps): React.ReactElement {
 
 	const renderStacks = (): React.ReactElement => {
 		if (globalContext.dataLoaded) {
-			<AppNavigator />
+			return <AppNavigator />
 		} else {
 			return (
 				<ScreenStack.Navigator>
