b5bc1bb74f2ac37365bdb6c9f8ae5e7bbf5c5c54
Slesarev <slesarew@gmail.com>
2021-04-20 16:14:40 +0300

    test: increase e2e timeout

--- test/e2e/setup.ts
+++ test/e2e/setup.ts
@@ -24,7 +24,7 @@ import testIDs from 'e2e/testIDs';
 import { testTap, testVisible } from 'e2e/utils';
 
 // Set the default timeout
-jest.setTimeout(120000);
+jest.setTimeout(360000);
 jasmine.getEnv().addReporter(adapter);
 
 // This takes care of generating status logs on a per-spec basis. By default, jest only reports at file-level.
