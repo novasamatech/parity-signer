9e06137af8144267e059a0c2c88030ccb8f44c65
Slesarev <slesarew@gmail.com>
2021-05-10 11:31:00 +0300

    docs: bump license year in ios

--- scripts/license-year-upgrade.sh
+++ scripts/license-year-upgrade.sh
@@ -3,3 +3,4 @@
 
 find ./src -name '*.ts?' -exec sed -i -e 's/Copyright 2015-2020 Parity Technologies (UK) Ltd./Copyright 2015-2021 Parity Technologies (UK) Ltd./g' {} \;
 find ./src -name '*.ts' -exec sed -i -e 's/Copyright 2015-2020 Parity Technologies (UK) Ltd./Copyright 2015-2021 Parity Technologies (UK) Ltd./g' {} \;
+find ./ios -name '*.h' -exec sed -i -e 's/Copyright 2015-2020 Parity Technologies (UK) Ltd./Copyright 2015-2021 Parity Technologies (UK) Ltd./g' {} \;
