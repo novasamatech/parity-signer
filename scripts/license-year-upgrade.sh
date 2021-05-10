1849e9646a0a028e805fc1b43696efbbd26ff614
Slesarev <slesarew@gmail.com>
2021-05-10 11:18:22 +0300

    docs: bump license year in ts

---
+++ scripts/license-year-upgrade.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This is temporary solution
+
+find ./src -name '*.ts?' -exec sed -i -e 's/Copyright 2015-2020 Parity Technologies (UK) Ltd./Copyright 2015-2021 Parity Technologies (UK) Ltd./g' {} \;
+find ./src -name '*.ts' -exec sed -i -e 's/Copyright 2015-2020 Parity Technologies (UK) Ltd./Copyright 2015-2021 Parity Technologies (UK) Ltd./g' {} \;
