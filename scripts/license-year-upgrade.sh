199f253511f945f6947c8156a7bce2ca4af0bc33
Slesarev <slesarew@gmail.com>
2021-05-10 11:35:10 +0300

    docs: bump version year in rust

--- scripts/license-year-upgrade.sh
+++ scripts/license-year-upgrade.sh
@@ -4,3 +4,4 @@
 find ./src -name '*.ts?' -exec sed -i -e 's/Copyright 2015-2020 Parity Technologies (UK) Ltd./Copyright 2015-2021 Parity Technologies (UK) Ltd./g' {} \;
 find ./src -name '*.ts' -exec sed -i -e 's/Copyright 2015-2020 Parity Technologies (UK) Ltd./Copyright 2015-2021 Parity Technologies (UK) Ltd./g' {} \;
 find ./ios -name '*.h' -exec sed -i -e 's/Copyright 2015-2020 Parity Technologies (UK) Ltd./Copyright 2015-2021 Parity Technologies (UK) Ltd./g' {} \;
+find ./rust/ -name '*.rs' -exec sed -i -e 's/Copyright 2015-2020 Parity Technologies (UK) Ltd./Copyright 2015-2021 Parity Technologies (UK) Ltd./g' {} \;
