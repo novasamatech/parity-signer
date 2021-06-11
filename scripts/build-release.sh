48885eb7d4b85a070c6e78b25a9c514350c06bd6
Martin Pugh <martin@parity.io>
2021-06-11 16:52:54 +0200

    fix

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -5,9 +5,9 @@ keyalias=$2
 keypass_arg="${3:+-storepass $3}"
 
 set -e
-# "$(dirname "${0}")"/build.sh android
+"$(dirname "${0}")"/build.sh android
 pushd "$(dirname "${0}")"/../android
-  # ./gradlew assembleRelease
+  ./gradlew assembleRelease
   jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore "$keystore" $keypass_arg app/build/outputs/apk/release/app-release-unsigned.apk "$keyalias"
-  #zipalign -p 4 app/build/outputs/apk/release/app-release-unsigned.apk ../signer-app-release-signed.apk
+  zipalign -p 4 app/build/outputs/apk/release/app-release-unsigned.apk ../signer-app-release-signed.apk
 popd
