217afff9eee4d37dafaf056f72b55f26de5440ce
Martin Pugh <martin@parity.io>
2021-06-11 16:52:29 +0200

    add signing

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -2,10 +2,12 @@
 
 keystore="$(readlink -f "$1")"
 keyalias=$2
+keypass_arg="${3:+-storepass $3}"
+
 set -e
-"$(dirname "${0}")"/build.sh android
+# "$(dirname "${0}")"/build.sh android
 pushd "$(dirname "${0}")"/../android
-  ./gradlew assembleRelease
-  jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore "$keystore" app/build/outputs/apk/release/app-release-unsigned.apk "$keyalias"
-  zipalign -p 4 app/build/outputs/apk/release/app-release-unsigned.apk ../signer-app-release-signed.apk
+  # ./gradlew assembleRelease
+  jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore "$keystore" $keypass_arg app/build/outputs/apk/release/app-release-unsigned.apk "$keyalias"
+  #zipalign -p 4 app/build/outputs/apk/release/app-release-unsigned.apk ../signer-app-release-signed.apk
 popd
