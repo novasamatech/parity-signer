fe6801db8cd8d961b3f14fd37329f4b9c77b0015
Slesarev <slesarew@gmail.com>
2021-07-13 16:02:38 +0300

    refactor: remove old rust code, some general cleanup

--- rust/signer/src/result.rs
+++ rust/signer/src/result.rs
@@ -1,7 +1,3 @@
-use thiserror::Error;
-
-pub type Result<T> = anyhow::Result<T>;
-
 pub fn return_json_array(mut string: String) -> std::result::Result<String, Box<dyn std::error::Error>> {
     match string.pop() {
             None | Some('[') => return Ok("[]".to_string()),
@@ -12,20 +8,4 @@ pub fn return_json_array(mut string: String) -> std::result::Result<String, Box<
             _ => return Err(Box::from("Database corrupted!"))
         }
 
-}
-
-#[derive(Error, Debug)]
-pub enum Error {
-	#[error("Could not derive key pair")]
-	KeyPairIsNone,
-	#[error("Error converting from hex: {0:?}")]
-	FromHex(rustc_hex::FromHexError),
-	#[error("Ethsign error: {0:?}")]
-	Ethsign(ethsign::Error),
-	#[error("Signature error: {0:?}")]
-	Signature(schnorrkel::SignatureError),
-	#[error("Error creating icon: {0:?}")]
-	Blockies(blockies::Error),
-	#[error("Error rendering QR code: {0:?}")]
-	Pixelate(pixelate::Error),
 }
