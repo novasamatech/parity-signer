46c0e374dd7c68e00bfdc6c96138991fe06d3314
Slesarev <slesarew@gmail.com>
2021-09-10 17:33:59 +0300

    fix: merge camera progress bar fix

--- android/app/src/main/java/io/parity/signer/modals/CameraModal.kt
+++ android/app/src/main/java/io/parity/signer/modals/CameraModal.kt
@@ -9,6 +9,7 @@ import androidx.camera.view.PreviewView
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -16,6 +17,7 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
@@ -76,8 +78,11 @@ fun CameraModal(signerDataModel: SignerDataModel) {
 			)
 		}
 		LinearProgressIndicator(
-			progress = progress.value!!
+			progress = progress.value!!,
+			color = Color.Red,
+			backgroundColor = Color.White,
+			modifier = Modifier.fillMaxWidth()
 		)
-		Text("Scanning progress: " + (progress.value?:0 * 100.0).toString() + "%")
+		Text("Scanning progress: " + ((progress.value?:0.0f) * 100.0f).toInt().toString() + "%")
 	}
 }
