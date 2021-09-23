a799882de395d77a507273df987ea87cdcf1517f
Slesarev <slesarew@gmail.com>
2021-09-23 15:48:27 +0300

    fix: return docs to transaction cards and format them slightly better

--- ios/NativeSigner/Cards/TransactionCards/TCFieldName.swift
+++ ios/NativeSigner/Cards/TransactionCards/TCFieldName.swift
@@ -14,28 +14,30 @@ struct TCFieldName: View {
         Button (action: {
             self.showDoc.toggle()
         }) {
-            HStack {
-                Text(value.name)
-                    .foregroundColor(Color("textMainColor"))
-                Spacer()
-                if value.docs != "" {
-                    Text("?")
-                        .foregroundColor(Color("AccentColor"))
+            VStack {
+                HStack {
+                    Text(value.name)
+                        .foregroundColor(Color("textMainColor"))
+                    Spacer()
+                    if value.docs != "" {
+                        Text("?")
+                            .foregroundColor(Color("AccentColor"))
+                    }
                 }
-            }
-            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("backgroundCard")/*@END_MENU_TOKEN@*/)
-            if showDoc {
-                Text(String(decoding: Data(fromHexEncodedString: value.docs) ?? Data(), as: UTF8.self))
-                    .foregroundColor(Color("textMainColor"))
-                    .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("backgroundCard")/*@END_MENU_TOKEN@*/)
+                .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("backgroundCard")/*@END_MENU_TOKEN@*/)
+                if showDoc {
+                    Text(String(decoding: Data(fromHexEncodedString: value.docs) ?? Data(), as: UTF8.self))
+                        .foregroundColor(Color("textMainColor"))
+                        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("backgroundCard")/*@END_MENU_TOKEN@*/)
+                }
             }
         }
     }
 }
 
 /*
-struct TCFieldName_Previews: PreviewProvider {
-    static var previews: some View {
-        TCFieldName()
-    }
-}*/
+ struct TCFieldName_Previews: PreviewProvider {
+ static var previews: some View {
+ TCFieldName()
+ }
+ }*/
--- ios/NativeSigner/Cards/TransactionCards/TCPathDocs.swift
+++ ios/NativeSigner/Cards/TransactionCards/TCPathDocs.swift
@@ -14,29 +14,31 @@ struct TCPathDocs: View {
         Button (action: {
             self.showDoc.toggle()
         }) {
-            HStack {
-                Text(value.path)
-                    .foregroundColor(Color("textMainColor"))
-                Spacer()
-                if value.docs != "" {
-                    Text("?")
-                        .foregroundColor(Color("AccentColor"))
+            VStack {
+                HStack {
+                    Text(value.path.joined(separator: " : "))
+                        .foregroundColor(Color("textMainColor"))
+                    Spacer()
+                    if value.docs != "" {
+                        Text("?")
+                            .foregroundColor(Color("AccentColor"))
+                    }
                 }
-            }
-            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("backgroundCard")/*@END_MENU_TOKEN@*/)
-            if showDoc {
-                Text(String(decoding: Data(fromHexEncodedString: value.docs) ?? Data(), as: UTF8.self))
-                    .foregroundColor(Color("textMainColor"))
-                    .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("backgroundCard")/*@END_MENU_TOKEN@*/)
+                .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("backgroundCard")/*@END_MENU_TOKEN@*/)
+                if showDoc {
+                    Text(String(decoding: Data(fromHexEncodedString: value.docs) ?? Data(), as: UTF8.self))
+                        .foregroundColor(Color("textMainColor"))
+                        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("backgroundCard")/*@END_MENU_TOKEN@*/)
+                }
             }
         }
     }
 }
 
 /*
-struct TCPathDocs_Previews: PreviewProvider {
-    static var previews: some View {
-        TCPathDocs()
-    }
-}
-*/
+ struct TCPathDocs_Previews: PreviewProvider {
+ static var previews: some View {
+ TCPathDocs()
+ }
+ }
+ */
