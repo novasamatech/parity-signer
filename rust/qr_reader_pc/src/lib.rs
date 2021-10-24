d6cee53f08d8f5bf6fe005f6fad5b83f899f1080
Alexey Kostenko <alexey.a.kostenko@gmail.com>
2021-10-24 20:20:56 +0300

    rscam replaced by nokhwa (crossplatform video capture) crate. Added video output using minifb crate.

--- rust/qr_reader_pc/src/lib.rs
+++ rust/qr_reader_pc/src/lib.rs
@@ -1,6 +1,8 @@
-use rscam::{Camera, Config};
-use std::io::Write;
-use image::{GenericImageView, Pixel, Luma, ImageBuffer, GrayImage};
+extern crate minifb;
+use minifb::{Window, WindowOptions};
+
+use nokhwa::{Camera, CameraFormat, FrameFormat};
+use image::{Pixel, Luma, ImageBuffer, GrayImage};
 use quircs;
 use hex;
 use qr_reader_phone::process_payload::{process_decoded_payload, Ready, InProgress};
@@ -9,30 +11,37 @@ use anyhow::anyhow;
 const WIDTH: u32 = 640;
 const HEIGHT: u32 = 480;
 
-pub fn run_with_camera() -> anyhow::Result<String> {
+pub fn run_with_camera(camera_num : usize) -> anyhow::Result<String> {
 
-    let mut camera = match Camera::new("/dev/video0") {
+    let mut camera = match Camera::new(camera_num, Some(CameraFormat::new_from(WIDTH, HEIGHT, FrameFormat::MJPEG, 30)),) {
         Ok(x) => x,
         Err(e) => return Err(anyhow!("Error opening camera. {}", e)),
-    };
+    };   
 
-    match camera.start(&Config {
-        interval: (1, 30),      // 30 fps.
-        resolution: (WIDTH, HEIGHT),
-        format: b"MJPG",
-        ..Default::default()
-    }) {
+    let mut window = match Window::new(
+        "Test - ESC to exit",
+        WIDTH as usize,
+        HEIGHT as usize,
+        WindowOptions::default(),
+    ) {
+        Ok(x) => x,
+        Err(e) => return Err(anyhow!("Error creating new output window. {}", e)),
+    };
+    
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+
+    match camera.open_stream() {
         Ok(_) => (),
         Err(e) => return Err(anyhow!("Error starting camera. {}", e)),
     };
-    
+
     let mut out = Ready::NotYet(InProgress::None);
     let mut line = String::new();
     
     loop {
         match out {
             Ready::NotYet(decoding) => {
-                out = match camera_capture(&camera) {
+                out = match camera_capture(&mut camera, &mut window) {
                     Ok(img) => process_qr_image (&img, decoding)?,
                     Err(_) => Ready::NotYet(decoding),
                 };
@@ -50,31 +59,32 @@ pub fn run_with_camera() -> anyhow::Result<String> {
     Ok(line)
 }
 
-
-fn camera_capture(camera: &Camera) -> anyhow::Result<ImageBuffer<Luma<u8>, Vec<u8>>> {
-    let frame = match camera.capture() {
+fn camera_capture(camera: &mut Camera, window: &mut Window) -> anyhow::Result<ImageBuffer<Luma<u8>, Vec<u8>>> {
+    
+    let frame = match camera.frame() {
         Ok(x) => x,
         Err(e) => return Err(anyhow!("Error with camera capture. {}", e)),
     };
-    let mut captured_data: Vec<u8> = Vec::new();
-    match captured_data.write_all(&frame[..]) {
-        Ok(_) => (),
-        Err(e) => return Err(anyhow!("Error writing data from camera into buffer. {}", e)),
-    };
-    match image::load_from_memory(&captured_data[..]) {
-        Ok(a) => {
-            let mut gray_img: GrayImage = ImageBuffer::new(WIDTH, HEIGHT);
-            for y in 0..HEIGHT {
-                for x in 0..WIDTH {
-                    let new_pixel = a.get_pixel(x, y).to_luma();
-                    gray_img.put_pixel(x, y, new_pixel);
-                }
-            }
-//        println!("got gray img");
-            Ok(gray_img)
-        },
-        Err(e) => return Err(anyhow!("Error loading data from buffer. {}", e)),
+
+    let mut out_buf: Vec<u32> = Vec::new();
+    let mut gray_img: GrayImage = ImageBuffer::new(WIDTH, HEIGHT);
+
+    for y in 0..HEIGHT {
+        for x in 0..WIDTH {
+            let new_pixel = frame.get_pixel(x, y).to_luma();
+            let buf_pix = frame.get_pixel(x, y).0;
+            let buf_col = u32::from_be_bytes([0,buf_pix[0], buf_pix[1], buf_pix[2]]);
+            gray_img.put_pixel(x, y, new_pixel);
+            out_buf.push(buf_col);
+        }            
     }
+
+    match window.update_with_buffer(&out_buf[..], 640, 480) {
+        Ok(x) => x,
+        Err(e) => return Err(anyhow!("Error writing videobuffer. {}", e)),
+    };
+
+    Ok(gray_img)
 }
 
 fn process_qr_image (img: &ImageBuffer<Luma<u8>, Vec<u8>>, decoding: InProgress) -> anyhow::Result<Ready> {
--- rust/qr_reader_pc/src/main.rs
+++ rust/qr_reader_pc/src/main.rs
@@ -1,7 +1,14 @@
 use qr_reader_pc::run_with_camera;
+use std::env;
 
 fn main() {
-    match run_with_camera() {
+    let args: Vec<String> = env::args().collect();
+    let camera_num = match args[1].parse() {
+        Ok(num) => num,
+        Err(_) => 0,
+    };
+    
+    match run_with_camera(camera_num) {
         Ok(line) => println!("Success! {}", line),
         Err(e) => println!("Error. {}", e),
     }
