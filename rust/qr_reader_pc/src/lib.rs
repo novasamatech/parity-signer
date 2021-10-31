4fdac6111a7c8e34fb1c85f3753335f613a35f2a
Alexey Kostenko <alexey.a.kostenko@gmail.com>
2021-10-31 21:31:48 +0300

    Argument parser refactoring.

--- rust/qr_reader_pc/src/lib.rs
+++ rust/qr_reader_pc/src/lib.rs
@@ -8,14 +8,14 @@
 extern crate minifb;
 use minifb::{Window, WindowOptions};
 
-use nokhwa::{Camera, CameraFormat, FrameFormat};
 use nokhwa::{query_devices, CaptureAPIBackend};
+use nokhwa::{Camera, CameraFormat, FrameFormat};
 
-use image::{Pixel, Luma, ImageBuffer, GrayImage};
-use quircs;
-use hex;
-use qr_reader_phone::process_payload::{process_decoded_payload, Ready, InProgress};
 use anyhow::anyhow;
+use hex;
+use image::{GrayImage, ImageBuffer, Luma, Pixel};
+use qr_reader_phone::process_payload::{process_decoded_payload, InProgress, Ready};
+use quircs;
 use std::env;
 
 const DEFAULT_WIDTH: u32 = 640;
@@ -24,15 +24,20 @@ const DEFAULT_FRAME_FORMAT: FrameFormat = FrameFormat::YUYV;
 const DEFAULT_FPS: u32 = 30;
 
 /// Main cycle of video input reading.
-/// 
-pub fn run_with_camera(camera_settings : CameraSettings) -> anyhow::Result<String> {
-
-    let mut camera = match Camera::new(camera_settings.index, 
-        Some(CameraFormat::new_from(DEFAULT_WIDTH, DEFAULT_HEIGHT, camera_settings.frame_format, camera_settings.fps)),) {
-
+///
+pub fn run_with_camera(camera_settings: CameraSettings) -> anyhow::Result<String> {
+    let mut camera = match Camera::new(
+        camera_settings.index.unwrap(),
+        Some(CameraFormat::new_from(
+            DEFAULT_WIDTH,
+            DEFAULT_HEIGHT,
+            camera_settings.frame_format,
+            camera_settings.fps,
+        )),
+    ) {
         Ok(x) => x,
         Err(e) => return Err(anyhow!("Error opening camera. {}", e)),
-    };   
+    };
 
     let mut window = match Window::new(
         "Camera capture",
@@ -43,7 +48,7 @@ pub fn run_with_camera(camera_settings : CameraSettings) -> anyhow::Result<Strin
         Ok(x) => x,
         Err(e) => return Err(anyhow!("Error creating new output window. {}", e)),
     };
-    
+
     window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
 
     match camera.open_stream() {
@@ -53,30 +58,29 @@ pub fn run_with_camera(camera_settings : CameraSettings) -> anyhow::Result<Strin
 
     let mut out = Ready::NotYet(InProgress::None);
     let mut line = String::new();
-    
+
     loop {
         match out {
             Ready::NotYet(decoding) => {
                 out = match camera_capture(&mut camera, &mut window) {
-                    Ok(img) => process_qr_image (&img, decoding)?,
+                    Ok(img) => process_qr_image(&img, decoding)?,
                     Err(_) => Ready::NotYet(decoding),
                 };
-            },
+            }
             Ready::Yes(a) => {
                 line.push_str(&hex::encode(&a));
                 match std::fs::write("decoded_output.txt", &line) {
                     Ok(_) => (),
-                    Err(e) => println!("Unable to write decoded information in the file. {}", e)
+                    Err(e) => println!("Unable to write decoded information in the file. {}", e),
                 };
                 break;
-            },
+            }
         }
     }
     Ok(line)
 }
 
-fn camera_capture(camera: &mut Camera, window: &mut Window) -> anyhow::Result<ImageBuffer<Luma<u8>, Vec<u8>>> {
-    
+fn camera_capture(camera: &mut Camera, window: &mut Window,) -> anyhow::Result<ImageBuffer<Luma<u8>, Vec<u8>>> {
     let frame = match camera.frame() {
         Ok(x) => x,
         Err(e) => return Err(anyhow!("Error with camera capture. {}", e)),
@@ -89,13 +93,17 @@ fn camera_capture(camera: &mut Camera, window: &mut Window) -> anyhow::Result<Im
         for x in 0..DEFAULT_WIDTH {
             let new_pixel = frame.get_pixel(x, y).to_luma();
             let buf_pix = frame.get_pixel(x, y).0;
-            let buf_col = u32::from_be_bytes([0,buf_pix[0], buf_pix[1], buf_pix[2]]);
+            let buf_col = u32::from_be_bytes([0, buf_pix[0], buf_pix[1], buf_pix[2]]);
             gray_img.put_pixel(x, y, new_pixel);
             out_buf.push(buf_col);
-        }            
+        }
     }
 
-    match window.update_with_buffer(&out_buf[..], DEFAULT_WIDTH as usize, DEFAULT_HEIGHT as usize) {
+    match window.update_with_buffer(
+        &out_buf[..],
+        DEFAULT_WIDTH as usize,
+        DEFAULT_HEIGHT as usize,
+    ) {
         Ok(x) => x,
         Err(e) => return Err(anyhow!("Error writing videobuffer. {}", e)),
     };
@@ -103,84 +111,99 @@ fn camera_capture(camera: &mut Camera, window: &mut Window) -> anyhow::Result<Im
     Ok(gray_img)
 }
 
-fn process_qr_image (img: &ImageBuffer<Luma<u8>, Vec<u8>>, decoding: InProgress) -> anyhow::Result<Ready> {
+fn process_qr_image(img: &ImageBuffer<Luma<u8>, Vec<u8>>, decoding: InProgress,) -> anyhow::Result<Ready> {    
     let mut qr_decoder = quircs::Quirc::new();
     let codes = qr_decoder.identify(img.width() as usize, img.height() as usize, img);
     match codes.last() {
         Some(x) => {
             if let Ok(code) = x {
                 match code.decode() {
-                    Ok(decoded) => {
-                        process_decoded_payload(decoded.payload, decoding)
-                    },
+                    Ok(decoded) => process_decoded_payload(decoded.payload, decoding),
                     Err(_) => {
-//                        println!("Error with this scan: {}", e);
+                        //                        println!("Error with this scan: {}", e);
                         Ok(Ready::NotYet(decoding))
                     }
                 }
+            } else {
+                Ok(Ready::NotYet(decoding))
             }
-        else {Ok(Ready::NotYet(decoding))}
-        },
+        }
         None => {
-//            println!("no qr in this scan");
+            //            println!("no qr in this scan");
             Ok(Ready::NotYet(decoding))
-        },
+        }
     }
 }
 
 /// Structure for camera settings.
 ///
 pub struct CameraSettings {
-    index: usize,
+    index: Option<usize>,
     frame_format: FrameFormat,
     fps: u32,
 }
 
+fn print_list_of_cameras() {
+    println!("List of available devices:");
+    match query_devices(CaptureAPIBackend::Video4Linux) {
+        Ok(list) => {
+            for device in list {
+                println!("{:?}", device);
+            }
+        }
+        Err(_) => println!("Can`t capture list of cameras."),
+    };
+}
+
 /// Program's argument parser.
 ///
-pub fn arg_parser(mut args: env::Args) -> anyhow::Result<CameraSettings>
-{
+pub fn arg_parser(mut args: env::Args) -> anyhow::Result<CameraSettings> {
     args.next(); // skip program name
 
-    match (args.next(), args.next()) {
+    let mut settings = CameraSettings {
+        index: None,
+        frame_format: DEFAULT_FRAME_FORMAT,
+        fps: DEFAULT_FPS,
+    };
 
-        (Some(argument), Some(camera_index)) if argument == "d" => { 
+    while let Some(arg) = args.next() {
+        let par = match args.next() {
+            Some(x) => x,
+            None => String::from(""),
+        };
 
-            let index = match camera_index.trim().parse() {
-                Ok(index) => index,
-                Err(e) => return Err(anyhow!("Index parsing error: {}", e)),
-            };  
+        match &arg[..] {
+            "d" | "-d" | "--device" => match par.trim().parse() {
+                Ok(index) => settings.index = Some(index),
+                Err(e) => return Err(anyhow!("Camera index parsing error: {}", e)),
+            },
 
-            let frame_format = match args.next() {
-                Some(argument) if argument == "YUYV" => FrameFormat::YUYV,
-                Some(argument) if argument == "MJPEG" => FrameFormat::MJPEG,
-                _ => {println!("Frame format parsing error. Set default frame format"); DEFAULT_FRAME_FORMAT},
-            };  
-            
-            let fps = match args.next() {
-                Some(fps) => match fps.trim().parse() {
-                        Ok(fps) => fps,
-                        Err(_) => {println!("FPS parsing error. Set default framerate"); DEFAULT_FPS},
-                    },
-                None => {println!("FPS parsing error. Set default framerate"); DEFAULT_FPS},
-            };                    
+            "ff" | "-ff" => match &par[..] {
+                "YUYV" => settings.frame_format = FrameFormat::YUYV,
+                "MJPEG" => settings.frame_format = FrameFormat::MJPEG,
+                _ => println!("Frame format parsing error. Set default frame format."),
+            },
 
-            Ok(CameraSettings {index, frame_format, fps})
+            "fps" | "-fps" => match par.trim().parse() {
+                Ok(fps) => settings.fps = fps,
+                Err(_) => println!("FPS parsing error. Set default framerate."),
+            },
+
+            "h" | "-h" | "--help" => {
+                // TODO
+            }
+
+            _ => return Err(anyhow!("Argument parsing error.")),
+        };
+    }
+
+    match settings.index {
+        Some(_) => Ok(settings),
+        None => {
+            print_list_of_cameras();
+            Err(anyhow!(
+                "Need to provide camera index. Please read readme.md file."
+            ))
         }
-        (Some(_), ..) => Err(anyhow!("Can`t recognize arguments.")),
-        (None, ..) => {
-            println!("\nNot enough arguments. Use 'd' argument to set index of camera, frame format (YUYV or MJPEG) and fps.\
-            \nExample: cargo run d 0 MJPEG 30\
-            \nYou can only provide index. Default frame format: YUYV, default fps: 30, hardcoded framesize: 640x480\
-            \nExample: cargo run d 0");
-
-            println!("List of available devices:");
-            if let Ok(list) = query_devices(CaptureAPIBackend::Video4Linux) {
-                for device in list {
-                    println!("{:?}", device);
-                };
-                println!();
-            };
-            Err(anyhow!("Error parsing arguments."))},
     }
 }
--- rust/qr_reader_pc/src/main.rs
+++ rust/qr_reader_pc/src/main.rs
@@ -1,19 +1,16 @@
+use qr_reader_pc::{arg_parser, run_with_camera};
 use std::env;
-use qr_reader_pc::{run_with_camera, arg_parser};
 
 fn main() -> Result<(), String> {
-   
     let camera_setings = match arg_parser(env::args()) {
         Ok(x) => x,
         Err(e) => return Err(format!("{}", e)),
     };
-    
+
     match run_with_camera(camera_setings) {
         Ok(line) => println!("Result HEX: {}", line),
         Err(e) => return Err(format!("QR reading error. {}", e)),
     }
 
     Ok(())
-
 }
-
