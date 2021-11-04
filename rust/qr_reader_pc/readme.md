e0b95f981348f30c8f48225de96d20c6854da80a
Alexey Kostenko <alexey.a.kostenko@gmail.com>
2021-11-04 04:27:00 +0300

    QR decoding added. Extra code removed. Documentation updated.

--- rust/qr_reader_pc/readme.md
+++ rust/qr_reader_pc/readme.md
@@ -1,36 +1,37 @@
 # QR reader crate for PC
 
-QR reader crate for PC is a utility to scan (via webcam) QR codes from Signer mobile app
+QR reader crate for PC is a utility to capture (via webcam) QR codes from Signer mobile app
 and extracting data from it.  
-Prints on display (and to file "decoded_output.txt") a string with decoded QR message in HEX format.
+It prints a string with decoded QR message in HEX format on display (and to file "decoded_output.txt").
 
 ## Getting Started
 
 ### Dependencies
 
-* Only Linux is the current supported platform,
-* minifb (video output) crate dependencies for debian/ubuntu: `sudo apt install libxkbcommon-dev libwayland-cursor0 libwayland-dev`,
+You need to install `OpenCV`. You can check this manual: https://crates.io/crates/opencv and https://docs.opencv.org
+
+#### Linux
+
+* For Debian/Ubuntu you need: `clang` and `libclang-dev`
+* For Gentoo/Fedora you need: `clang`
+* For Linux system, it is preferable to build latest version of opencv+opencv_contrib from source. OpenCV package from the system repository may not contain the necessary libraries. Use this manual: https://docs.opencv.org/4.5.3/d7/d9f/tutorial_linux_install.html
 
 ### Executing program
 
 * Run the program: `cargo run` + arguments
 
-To check available camera parameters: `v4l2-ctl --list-formats-ext`
-
-### Arguments
+#### Arguments
 
 * When you run program without arguments, program provide a list of available cameras for v4l backend,
-* `d`/`-d`/`--device` : set index of camera (from list of available cameras),
-* `ff`/`-ff` : set frame format for camera, `YUYV` and `MJPEG` are suported,
-* `fps`/`-fps` : set fps parameter for camera,
+* `d` | `-d` | `--device` : set index of camera (from list of available cameras),
+* `l` | `-l` | `--list` : get a list of available camera indexes,
+* `h` | `-h` | `--help` : refers to this manual,
 
-You can only provide camera index. Other parameters will be setted by default (YUYV frame format and 30 fps).
 Camera resolution is hardcoded (640x480).
 
-### Examples
+#### Examples
 
 * `cargo run d 0` : camera index = 0,
-* `cargo run d 2 ff MJPEG fps 30`: camera index = 2, frame format = MJPEG, fps = 30,
-
+* `cargo run l`
 
 
--- rust/qr_reader_pc/src/lib.rs
+++ rust/qr_reader_pc/src/lib.rs
@@ -2,35 +2,31 @@
 
 //! # QR reader crate for PC
 //!
-//! `qr_reader_pc` is a utility to scan (via webcam) QR codes from Signer
+//! `qr_reader_pc` is a utility to capture (via webcam) QR codes from Signer
 //! and extracting data from it.
 
-//use minifb::{Window, WindowOptions};
-//use nokhwa::{query_devices, CaptureAPIBackend};
-//use nokhwa::{Camera, CameraFormat, FrameFormat};
 use anyhow::anyhow;
-//use image::{GrayImage, ImageBuffer, Luma, Pixel};
 use qr_reader_phone::process_payload::{process_decoded_payload, InProgress, Ready};
 use std::env;
-// use opencv::prelude::*;
-// use quircs;
-// use hex;
 
 use opencv::{
 	highgui,
-	imgproc,
 	prelude::*,
 	Result,
 	videoio,
+    videoio::{CAP_PROP_FRAME_HEIGHT, CAP_PROP_FRAME_WIDTH,},
     objdetect::QRCodeDetector,
 };
 
 // Default camera settings
 const DEFAULT_WIDTH: u32 = 640;
 const DEFAULT_HEIGHT: u32 = 480;
-//const DEFAULT_FRAME_FORMAT: FrameFormat = FrameFormat::YUYV;
-const DEFAULT_FPS: u32 = 30;
-//const DEFAULT_BACKEND: CaptureAPIBackend = CaptureAPIBackend::Video4Linux;
+const MAX_CAMERA_INDEX: i32 = 6;
+
+/// Structure for storing camera settings.
+pub struct CameraSettings {
+    index: Option<i32>,
+}
 
 /// Main cycle of video capture.
 /// Returns a string with decoded QR message in HEX format or error
@@ -39,89 +35,76 @@ const DEFAULT_FPS: u32 = 30;
 ///
 /// * `camera_settings` - A CameraSettings struct that holds the camera parameters
 pub fn run_with_camera(camera_settings: CameraSettings) -> anyhow::Result<String> {
-    // let mut camera = match Camera::new(
-    //     camera_settings.index.unwrap(),
-    //     Some(CameraFormat::new_from(
-    //         DEFAULT_WIDTH,
-    //         DEFAULT_HEIGHT,
-    //         camera_settings.frame_format,
-    //         camera_settings.fps,
-    //     )),
-    // ) {
-    //     Ok(x) => x,
-    //     Err(e) => return Err(anyhow!("Error opening camera. {}", e)),
-    // };
-
-    // let mut window = match Window::new(
-    //     "Camera capture",
-    //     DEFAULT_WIDTH as usize,
-    //     DEFAULT_HEIGHT as usize,
-    //     WindowOptions::default(),
-    // ) {
-    //     Ok(x) => x,
-    //     Err(e) => return Err(anyhow!("Error creating new output window. {}", e)),
-    // };
-
-    // window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
-
-
-
-    // match camera.open_stream() {
-    //     Ok(_) => (),
-    //     Err(e) => return Err(anyhow!("Error starting camera. {}", e)),
-    // };
+    
+    let camera_index = match camera_settings.index {
+        Some(index) => index,
+        None => return Err(anyhow!("There is no camera index.")),
+    };
 
 	let window = "video capture";   
 	highgui::named_window(window, 1)?;
-    #[cfg(ocvrs_opencv_branch_32)]
-	let mut cam = videoio::VideoCapture::new_default(2)?; // 0 is the default camera
-	#[cfg(not(ocvrs_opencv_branch_32))]
-	let mut cam = videoio::VideoCapture::new(2, videoio::CAP_ANY)?; // 0 is the default camera
-	let opened = videoio::VideoCapture::is_opened(&cam)?;
-    if !opened {
-		panic!("Unable to open default camera!");
-	}
 
-    let mut out: Result<Option<String>> = Ok(None); //Ready::NotYet(InProgress::None);
+    let mut camera = create_camera(camera_index, DEFAULT_WIDTH, DEFAULT_HEIGHT)?;
+
+    let mut out = Ready::NotYet(InProgress::None);
     let mut line = String::new();
-
+    
     loop {
         match out {
-            Ok(None) => {
-                out = camera_capture(&mut cam, &window);
+            Ready::NotYet(decoding) => {
+                out = match camera_capture(&mut camera, window) {
+                    Ok(img) => process_qr_image (&img, decoding)?,
+                    Err(_) => Ready::NotYet(decoding),
+                };
             },
-            Ok(Some(a)) => {
+            Ready::Yes(a) => {
                 line.push_str(&hex::encode(&a));
                 match std::fs::write("decoded_output.txt", &line) {
                     Ok(_) => (),
-                    Err(e) => println!("Unable to write decoded information in the file. {}", e),
+                    Err(e) => println!("Unable to write decoded information in the file. {}", e)
                 };
                 break;
             },
-            Err(_) => {
-                println!("Err");
-            },
         }
-        //camera_capture(&mut cam, &window);
-    };
+    }
     Ok(line)
 }
 
-fn camera_capture(camera: &mut videoio::VideoCapture, window: &str) -> Result<Option<String>> {
-
-    let mut qrDet = QRCodeDetector::default()?;
+fn create_camera(camera_index: i32, width: u32, height: u32) -> anyhow::Result<videoio::VideoCapture>
+{
+    #[cfg(ocvrs_opencv_branch_32)]
+	let mut camera = videoio::VideoCapture::new_default(camera_index)?;
+	#[cfg(not(ocvrs_opencv_branch_32))]
+	let mut camera = videoio::VideoCapture::new(camera_index, videoio::CAP_ANY)?;
+	
+    match videoio::VideoCapture::is_opened(&camera) {
+        Ok(opened) if opened => {
+            match (
+                camera.set(CAP_PROP_FRAME_WIDTH, width.into()),
+                camera.set(CAP_PROP_FRAME_HEIGHT, height.into()),
+            ) {
+                (Ok(_), Ok(_)) => println!("Resolution setted."),
+                (_,_) => return Err(anyhow!("Can not set camera resolution.")),
+            }
+        },
+        Ok(_) => return Err(anyhow!("Camera already opened.")),
+        Err(e) => return Err(anyhow!("Can`t open camera. {}", e)),
+    };
 
     let mut frame = Mat::default();
+
+    match camera.read(&mut frame) {
+        Ok(_) if frame.size()?.width > 0 => Ok(camera),
+        Ok(_) => Err(anyhow!("Zero frame size.")),
+        Err(e) => Err(anyhow!("Can`t read camera. {}", e)),
+    }
+}
+
+fn camera_capture(camera: &mut videoio::VideoCapture, window: &str) -> Result<Mat> {
+    let mut frame = Mat::default();
     camera.read(&mut frame)?;
-    // let mut gray_img = Mat::default();
 
     if frame.size()?.width > 0 {        
-        // imgproc::cvt_color(
-        //     &frame,
-        //     &mut gray_img,
-        //     imgproc::COLOR_BGR2GRAY,
-        //     0,
-        // )?;
         highgui::imshow(window, &frame)?;
     };
 
@@ -129,79 +112,33 @@ fn camera_capture(camera: &mut videoio::VideoCapture, window: &str) -> Result<Op
         println!("Ok");
     };
 
-    // let mut out_buf: Vec<u32> = Vec::new();
-    // let mut gray_img: GrayImage = ImageBuffer::new(DEFAULT_WIDTH, DEFAULT_HEIGHT);
-
-    // for y in 0..DEFAULT_HEIGHT {
-    //     for x in 0..DEFAULT_WIDTH {
-    //         let new_pixel = frame.get_pixel(x, y).to_luma();
-    //         let buf_pix = frame.get_pixel(x, y).0;
-    //         let buf_col = u32::from_be_bytes([0, buf_pix[0], buf_pix[1], buf_pix[2]]);
-    //         gray_img.put_pixel(x, y, new_pixel);
-    //         out_buf.push(buf_col);
-    //     }
-    // };
-
-    // match window.update_with_buffer(
-    //     &out_buf[..],
-    //     DEFAULT_WIDTH as usize,
-    //     DEFAULT_HEIGHT as usize,
-    // ) {
-    //     Ok(x) => x,
-    //     Err(e) => return Err(anyhow!("Error writing videobuffer. {}", e)),
-    // };
-    let mut points = Mat::default();
-    let mut rect_image = Mat::default();
-    let code = qrDet.detect_and_decode(&frame,&mut points,&mut rect_image);
-        
-    // if code != "" {
-    //     Ok(Ready::Yes(code))
-    // } else {
-    //     Ok(Ready::NotYet("1")
-    // }
-
-    match code {
-        Ok(data) if data !="" => Ok(Some(data)),
-        Ok(_) | Err(_) => Ok(None),
-    }
-    
+    Ok(frame) 
 }
 
-// fn process_qr_image(img: &ImageBuffer<Luma<u8>, Vec<u8>>, decoding: InProgress,) -> anyhow::Result<Ready> {    
-//     let mut qr_decoder = quircs::Quirc::new();
-//     let codes = qr_decoder.identify(img.width() as usize, img.height() as usize, img);
-//     match codes.last() {
-//         Some(Ok(code)) => {
-//             match code.decode() {
-//                 Ok(decoded) => process_decoded_payload(decoded.payload, decoding),
-//                 Err(_) => {
-//                     Ok(Ready::NotYet(decoding))
-//                 }
-//             }
-//         },
-//         None | Some(Err(_)) => {
-//             Ok(Ready::NotYet(decoding))
-//         },
-//     }
-// }
+fn process_qr_image(frame: &Mat, decoding: InProgress,) -> anyhow::Result<Ready> {   
+        
+    let mut qr_detector = QRCodeDetector::default()?;
+    let mut points = Mat::default();
+    let mut rect_image = Mat::default();
+    let code = qr_detector.detect_and_decode(&frame, &mut points, &mut rect_image);
 
-/// Structure for storing camera settings.
-pub struct CameraSettings {
-    index: Option<usize>,
-    //frame_format: FrameFormat,
-    fps: u32,
+    match code {
+        Ok(code) if !code.is_empty() => process_decoded_payload(code.into_bytes(), decoding),
+        Ok(_) | Err(_) => Ok(Ready::NotYet(decoding)),
+    }
 }
 
 fn print_list_of_cameras() {
-    println!("List of available devices:");
-    // match query_devices(DEFAULT_BACKEND) {
-    //     Ok(list) => {
-    //         for device in list {
-    //             println!("{:?}", device);
-    //         }
-    //     }
-    //     Err(_) => println!("Can`t capture list of cameras."),
-    // };
+    let mut indexes: Vec<i32> = vec![];
+    for dev_port in 0..=MAX_CAMERA_INDEX {
+        if create_camera(dev_port, DEFAULT_WIDTH, DEFAULT_HEIGHT).is_ok() {
+            indexes.push(dev_port);
+        };
+    };
+    println!("\nList of available devices:");
+    for index in indexes {
+        println!("Camera index: {}", index);
+    }
 }
 
 /// Program's argument parser.
@@ -212,8 +149,6 @@ pub fn arg_parser(mut args: env::Args) -> anyhow::Result<CameraSettings> {
 
     let mut settings = CameraSettings {
         index: None,
-        //frame_format: DEFAULT_FRAME_FORMAT,
-        fps: DEFAULT_FPS,
     };
 
     while let Some(arg) = args.next() {
@@ -227,33 +162,14 @@ pub fn arg_parser(mut args: env::Args) -> anyhow::Result<CameraSettings> {
                 Ok(index) => settings.index = Some(index),
                 Err(e) => return Err(anyhow!("Camera index parsing error: {}", e)),
             },
-
-            // "ff" | "-ff" => match &par[..] {
-            //     "YUYV" => settings.frame_format = FrameFormat::YUYV,
-            //     "MJPEG" => settings.frame_format = FrameFormat::MJPEG,
-            //     _ => println!("Frame format parsing error. Set default frame format."),
-            // },
-
-            "fps" | "-fps" => match par.trim().parse() {
-                Ok(fps) => settings.fps = fps,
-                Err(_) => println!("FPS parsing error. Set default framerate."),
-            },
-
-            "h" | "-h" | "--help" => {
-                // TODO. The reference to the readme.md implemented now.
-            }
-
+            "h" | "-h" | "--help" => println!("Please read readme.md file."),
+            "l" | "-l" | "--list" => print_list_of_cameras(),
             _ => return Err(anyhow!("Argument parsing error.")),
         };
     }
 
     match settings.index {
         Some(_) => Ok(settings),
-        None => {
-            print_list_of_cameras();
-            Err(anyhow!(
-                "Need to provide camera index. Please read readme.md file."
-            ))
-        }
+        None => Err(anyhow!("Need to provide camera index. Please read readme.md file.")),
     }
 }
