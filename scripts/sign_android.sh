f35bf3553bc5f781d40799e5ea420f1a5732818b
Martin Pugh <martin@parity.io>
2021-11-18 15:43:52 +0100

    separate build and sign jobs

--- scripts/sign_android.sh
+++ scripts/sign_android.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 keystore="$(readlink -f "$1")"
 keypass="$2"
@@ -6,17 +7,8 @@ keypass="$2"
 # Get latest android-sdk-linux version for apksigner path
 ANDROID_BUILD_TOOLS_PATH=$(find /opt/android-sdk-linux/build-tools/ -maxdepth 1 -type d | sort -V | tail -n 1)
 
-set -e
-
-# Build rust lib
-pushd "$(dirname "${0}")"/..
-  ./scripts/build.sh android
-popd
-
 pushd "$(dirname "${0}")"/../android
-  echo "[+] Running Gradle"
-  ./gradlew assembleRelease
-  echo "[+] Build complete! Signing bundle"
+  echo "[+] Signing bundle"
   "$ANDROID_BUILD_TOOLS_PATH/apksigner" sign --ks "$keystore" --ks-pass "pass:$keypass" app/build/outputs/apk/release/app-release-unsigned.apk
   cp app/build/outputs/apk/release/app-release-unsigned.apk ../signer-ci-build.apk
 popd
