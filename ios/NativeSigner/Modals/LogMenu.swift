7e7e63f81b1df8fdf9fbd6d5dbf87cf31ca380e9
Slesarev <slesarew@gmail.com>
2021-12-21 14:02:49 +0200

    feat: ios: clear history confirmation

--- ios/NativeSigner/Modals/LogMenu.swift
+++ ios/NativeSigner/Modals/LogMenu.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct LogMenu: View {
     @EnvironmentObject var data: SignerDataModel
+    @State var clearConfirm = false
     var body: some View {
         VStack {
             Spacer()
@@ -16,9 +17,9 @@ struct LogMenu: View {
                 HeaderBar(line1: "LOG", line2: "Manage log" )
                 MenuButtonsStack {
                     BigButton(
-                        text: "Erase log",
+                        text: "Clear log",
                         action: {
-                            data.pushButton(buttonID: .ClearLog)
+                            clearConfirm = true
                         }
                     )
                 }
@@ -26,6 +27,9 @@ struct LogMenu: View {
             .padding([.leading, .trailing, .top])
             .padding(.bottom, 24)
             .background(Color("Bg000"))
+            .alert(isPresented: $clearConfirm, content: {
+                Alert(title: Text("Clear log?"), message: Text("Do you want this Signer to forget all logged events? This is not reversible."), primaryButton: .cancel(Text("Cancel")), secondaryButton: .destructive(Text("Clear log"), action: {data.pushButton(buttonID: .ClearLog)}))
+            })
         }
     }
 }
