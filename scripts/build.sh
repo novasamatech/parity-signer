58a522ca65654f03f8bc07f7796f7a846a4bc142
Wilfried Kopp <wilfried@parity.io>
2022-01-14 15:29:09 +0100

    Add --locked

--- scripts/build.sh
+++ scripts/build.sh
@@ -34,28 +34,28 @@ if [ "$1" == "android" ]
     CXX_aarch64_linux_android="${ANDROID_PREBUILD_BIN}/aarch64-linux-android${API_LEVEL}-clang++" \
     CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER="${ANDROID_PREBUILD_BIN}/aarch64-linux-android${API_LEVEL}-clang" \
     AR_aarch64_linux_android="${ANDROID_PREBUILD_BIN}/aarch64-linux-android-ar" \
-      cargo build --target=aarch64-linux-android --release
+      cargo build --locked --target=aarch64-linux-android --release
 
     printf "Building ARMv7 Andriod targets...";
     CC_armv7_linux_androideabi="${ANDROID_PREBUILD_BIN}/armv7a-linux-androideabi${API_LEVEL}-clang" \
     CXX_armv7_linux_androideabi="${ANDROID_PREBUILD_BIN}/armv7a-linux-androideabi${API_LEVEL}-clang++" \
     CARGO_TARGET_ARMV7_LINUX_ANDROIDEABI_LINKER="${ANDROID_PREBUILD_BIN}/armv7a-linux-androideabi${API_LEVEL}-clang" \
     AR_armv7_linux_androideabi="${ANDROID_PREBUILD_BIN}/arm-linux-androideabi-ar" \
-      cargo build --target=armv7-linux-androideabi --release
+      cargo build --locked --target=armv7-linux-androideabi --release
 
     printf "Building 32-bit x86  Andriod targets...";
     CC_i686_linux_android="${ANDROID_PREBUILD_BIN}/i686-linux-android${API_LEVEL}-clang" \
     CXX_i686_linux_android="${ANDROID_PREBUILD_BIN}/i686-linux-android${API_LEVEL}-clang++" \
     CARGO_TARGET_I686_LINUX_ANDROID_LINKER="${ANDROID_PREBUILD_BIN}/i686-linux-android${API_LEVEL}-clang" \
     AR_i686_linux_android="${ANDROID_PREBUILD_BIN}/i686-linux-android-ar" \
-      cargo  build --target=i686-linux-android --release
-    
+      cargo build --locked --target=i686-linux-android --release
+
     printf "Building 64-bit x86  Andriod targets...";
     CC_x86_64_linux_android="${ANDROID_PREBUILD_BIN}/x86_64-linux-android${API_LEVEL}-clang" \
     CXX_x86_64_linux_android="${ANDROID_PREBUILD_BIN}/x86_64-linux-android${API_LEVEL}-clang++" \
     CARGO_TARGET_X86_64_LINUX_ANDROID_LINKER="${ANDROID_PREBUILD_BIN}/x86_64-linux-android${API_LEVEL}-clang" \
     AR_x86_64_linux_android="${ANDROID_PREBUILD_BIN}/x86_64-linux-android-ar" \
-      cargo  build --target=x86_64-linux-android --release
+      cargo build --locked --target=x86_64-linux-android --release
 
     for i in "${!ANDROID_ARCHS[@]}";
       do
@@ -74,13 +74,13 @@ if [ "$1" == "ios" ]
     # Build iOS
 
     printf "Building iOS targets...";
-    
+
     cp ios-hack/Cargo.toml .
 
     for i in "${IOS_ARCHS[@]}";
       do
         rustup target add "$i";
-        cargo build --target "$i" --release --no-default-features
+        cargo build --locked --target "$i" --release --no-default-features
     done
 
     lipo -create -output "../../ios/NativeSigner/lib${LIB_NAME}.a" ../target/x86_64-apple-ios/release/libsigner.a ../target/aarch64-apple-ios/release/libsigner.a
