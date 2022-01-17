f2944459305b236995252b709b35f3274e4f502e
Slesarev <slesarew@gmail.com>
2022-01-17 21:13:03 +0200

    feat: android: partially NewSeedBackup screen

--- android/app/src/main/java/io/parity/signer/modals/NewSeedBackup.kt
+++ android/app/src/main/java/io/parity/signer/modals/NewSeedBackup.kt
@@ -1,13 +1,24 @@
 package io.parity.signer.modals
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Checkbox
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.RoundRect
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
+import io.parity.signer.ButtonID
+import io.parity.signer.components.BigButton
 import io.parity.signer.models.SignerDataModel
+import io.parity.signer.models.addSeed
+import io.parity.signer.models.pushButton
+import io.parity.signer.ui.theme.Bg200
 import io.parity.signer.ui.theme.Crypto400
 import io.parity.signer.ui.theme.CryptoTypography
 
@@ -15,23 +26,45 @@ import io.parity.signer.ui.theme.CryptoTypography
 fun NewSeedBackup(signerDataModel: SignerDataModel) {
 	val confirmBackup = remember { mutableStateOf(false) }
 	val createRoots = remember { mutableStateOf(true) }
-	Column {
-		Text(
-			signerDataModel.modalData.value?.optString("seed_phrase") ?: "",
-			style = CryptoTypography.body1,
-			color = Crypto400
-		)
-		Row {
-			Checkbox(
-				checked = confirmBackup.value,
-				onCheckedChange = { confirmBackup.value = it })
-			Text ("I have written down my seed phrase")
-		}
-		Row {
-			Checkbox(
-				checked = createRoots.value,
-				onCheckedChange = { createRoots.value = it })
-			Text ("Create root keys")
+	Surface(
+		color = Bg200,
+		shape = MaterialTheme.shapes.large
+	) {
+		Column(Modifier.padding(horizontal = 8.dp)) {
+			Text(
+				signerDataModel.modalData.value?.optString("seed_phrase") ?: "",
+				style = CryptoTypography.body1,
+				color = Crypto400
+			)
+			Row {
+				Checkbox(
+					checked = confirmBackup.value,
+					onCheckedChange = { confirmBackup.value = it })
+				Text("I have written down my seed phrase")
+			}
+			Row {
+				Checkbox(
+					checked = createRoots.value,
+					onCheckedChange = { createRoots.value = it })
+				Text("Create root keys")
+			}
+			BigButton(
+				text = "Next",
+				action = {
+					signerDataModel.modalData.value?.let {modalData ->
+						modalData.optString("seed").let { seedName ->
+							modalData.optString("seed_phrase")
+								.let { seedPhrase ->
+									signerDataModel.addSeed(
+										seedName = seedName,
+										seedPhrase = seedPhrase
+									)
+								}
+						}
+					}
+				}
+			)
+			Spacer(Modifier.fillMaxHeight())
 		}
 	}
 }
