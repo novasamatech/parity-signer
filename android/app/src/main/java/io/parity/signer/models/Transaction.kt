1ea35980242c54a01f02c868e00716eb5166b4b2
Slesarev <slesarew@gmail.com>
2022-01-26 16:44:25 +0200

    fix: android: parse all tx card sets

--- android/app/src/main/java/io/parity/signer/models/Transaction.kt
+++ android/app/src/main/java/io/parity/signer/models/Transaction.kt
@@ -9,24 +9,33 @@ import org.json.JSONObject
  * Turn backend payload section into nice sorted array of transaction cards
  */
 fun JSONObject.parseTransaction(): JSONArray {
-	val warnings = this.optJSONArray("warning") ?: JSONArray()
-	val error = (this.optJSONArray("error") ?: JSONArray())
-	val typesInfo =
-		this.optJSONArray("types_info") ?: JSONArray()
-	val method = (this.optJSONArray("method") ?: JSONArray())
+	val author = this.optJSONArray("author") ?: JSONArray()
+	val error = this.optJSONArray("error") ?: JSONArray()
 	val extensions =
-		(this.optJSONArray("extensions") ?: JSONArray())
+		this.optJSONArray("extensions") ?: JSONArray()
+	val importingDerivations = this.optJSONArray("importing_derivations") ?: JSONArray()
+	val message = this.optJSONArray("message") ?: JSONArray()
+	val meta = this.optJSONArray("meta") ?: JSONArray()
+	val method = (this.optJSONArray("method") ?: JSONArray())
 	val newSpecs = (this.optJSONArray("new_specs") ?: JSONArray())
 	val verifier = (this.optJSONArray("verifier") ?: JSONArray())
+	val warning = this.optJSONArray("warning") ?: JSONArray()
+	val typesInfo =
+		this.optJSONArray("types_info") ?: JSONArray()
+
 	return sortCards(
 		concatJSONArray(
-			warnings,
+			author,
 			error,
-			typesInfo,
-			method,
 			extensions,
+			importingDerivations,
+			message,
+			meta,
+			method,
 			newSpecs,
-			verifier
+			verifier,
+			warning,
+			typesInfo
 		)
 	)
 }
