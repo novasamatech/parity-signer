fb9f30f46c95ba122abfc61a5147ed539b96cba8
Slesarev <slesarew@gmail.com>
2022-02-08 15:22:42 +0200

    style: android: paint the shield

--- android/app/src/main/java/io/parity/signer/components/NavbarShield.kt
+++ android/app/src/main/java/io/parity/signer/components/NavbarShield.kt
@@ -1,21 +1,25 @@
 package io.parity.signer.components
 
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import io.parity.signer.ShieldAlert
 import io.parity.signer.models.SignerDataModel
+import io.parity.signer.ui.theme.SignalDanger
+import io.parity.signer.ui.theme.SignalOn
+import io.parity.signer.ui.theme.SignalWarning
 
 @Composable
 fun NavbarShield(signerDataModel: SignerDataModel) {
 	val alert = signerDataModel.alertState.observeAsState()
 
 	when(alert.value) {
-		ShieldAlert.None -> Icon(Icons.Default.GppGood, "device is safe")
-		ShieldAlert.Active -> Icon(Icons.Default.GppBad, "device is online")
-		ShieldAlert.Past -> Icon(Icons.Default.GppBad, "potential security breach")
-		null -> Icon(Icons.Default.GppMaybe, "Safety indicator error")
+		ShieldAlert.None -> Icon(Icons.Default.GppGood, "device is safe", tint = MaterialTheme.colors.SignalOn)
+		ShieldAlert.Active -> Icon(Icons.Default.GppBad, "device is online", tint = MaterialTheme.colors.SignalDanger)
+		ShieldAlert.Past -> Icon(Icons.Default.GppBad, "potential security breach", tint = MaterialTheme.colors.SignalWarning)
+		null -> Icon(Icons.Default.GppMaybe, "Safety indicator error", tint = MaterialTheme.colors.SignalDanger)
 	}
 }
