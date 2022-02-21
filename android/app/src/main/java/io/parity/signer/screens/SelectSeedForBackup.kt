5a155ebce182cbdb196aa11c98fe8d0a776d4345
Slesarev <slesarew@gmail.com>
2022-02-21 19:27:33 +0200

    style: copy seedmanager element into seedselector (temporarily)

--- android/app/src/main/java/io/parity/signer/screens/SelectSeedForBackup.kt
+++ android/app/src/main/java/io/parity/signer/screens/SelectSeedForBackup.kt
@@ -1,36 +1,59 @@
 package io.parity.signer.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import io.parity.signer.ButtonID
 import io.parity.signer.components.SeedCard
 import io.parity.signer.models.SignerDataModel
 import io.parity.signer.models.pushButton
+import io.parity.signer.ui.theme.Bg200
 import org.json.JSONArray
 
 /**
  * Select seed for creating derivations
+ *
+ * TODO: really replace this with seedmanager element?
  */
 @Composable
 fun SelectSeedForBackup(signerDataModel: SignerDataModel) {
-	val content = signerDataModel.screenData.value?.optJSONArray("seedNameCards")
+	val cards = signerDataModel.screenData.value?.getJSONArray("seedNameCards")
 		?: JSONArray()
-	LazyColumn {
-		items(content.length()) { index ->
-			val seed = content.getJSONObject(index)
-			Row(Modifier.clickable {
-				signerDataModel.pushButton(
-					ButtonID.BackupSeed,
-					details = seed.optString("seed_name")
-				)
-			}) {
-				SeedCard(
-					seedName = seed.optString("seed_name"),
-					identicon = seed.optString("identicon")
-				)
+
+	LazyColumn(
+		contentPadding = PaddingValues(horizontal = 12.dp),
+		verticalArrangement = Arrangement.spacedBy(10.dp)
+	) {
+		items(cards.length()) { item ->
+			Row(
+				Modifier
+					//.padding(top = 3.dp, start = 12.dp, end = 12.dp)
+					.background(MaterialTheme.colors.Bg200)
+			) {
+				Row(
+					Modifier
+						.clickable {
+							signerDataModel.pushButton(
+								ButtonID.BackupSeed,
+								details = cards
+									.getJSONObject(item)
+									.getString("seed_name")
+							)
+						}
+						.weight(1f, true)
+				) {
+					SeedCard(
+						seedName = cards.getJSONObject(item).getString("seed_name"),
+						identicon = cards.getJSONObject(item).getString("identicon")
+					)
+				}
 			}
 		}
 	}
