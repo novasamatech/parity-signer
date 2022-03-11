a74314f493ae5f9b5f857968687c6c6062990642
Vera Abramova <abramova.vera@gmail.com>
2022-03-11 21:43:19 +0200

    docs: more docs in definitions; also, changed cargo deps to git links for volatile crates

--- rust/definitions/src/network_specs.rs
+++ rust/definitions/src/network_specs.rs
@@ -1,6 +1,84 @@
-//! Network specs and verifier certificates
+//! Network specs and verifier related types  
 //!
+//! Signer could be used only within networks introduced to the database.  
+//! The network parameters are stored in the cold database as [`NetworkSpecs`].  
+//! Hot database stores [`NetworkSpecsToSend`], that also is transferred
+//! through `add_specs` QR codes into Signer when a new network is added.  
+//! [`NetworkSpecs`] have all [`NetworkSpecsToSend`] information, and also `u8` 
+//! order, for the order in which network appears on Signer device screen.  
+//! [`ShortSpecs`] is the minimal [`NetworkSpecs`] subset needed to properly 
+//! format decoded transactions.  
 //! 
+//! # `NetworkSpecs` in cold database
+//!
+//! [`NetworkSpecs`] are stored in `SPECSTREE` tree of the cold database,
+//! with [`NetworkSpecsKey`] in key form as a key and SCALE-encoded [`NetworkSpecs`] 
+//! as a value.  
+//!
+//! [`NetworkSpecs`] include both the `encryption` ([`Encryption`]) and network  
+//! genesis hash (`[u8; 32]`), that are used for [`NetworkSpecsKey`] generation.  
+//! [`NetworkSpecs`] retrieved for given [`NetworkSpecsKey`] always get checked 
+//! for consistency.  
+//!
+//! If the network supports more than one encryption algorithm, each encryption
+//! corresponds to different [`NetworkSpecsKey`], and any or all of them could be
+//! coexisting in Signer simultaneously.  
+//!
+//! [`NetworkSpecs`] are generally expected to remain unchanged over time.  
+//!
+//! ## Adding new [`NetworkSpecs`]  
+//!
+//! New networks could be added to Signer through scanning `add_specs` QR code
+//! for the network.
+//!
+//! ## [`NetworkSpecs`] updating (replacing old ones with new ones without deleting old ones)
+//!
+//! Signer will not allow to update network specs if critical parameters
+//! have changed.  
+//! These critical parameters are:  
+//! - `base58prefix`, network-associated base58 prefix  
+//! - `decimals`  
+//! - `name`, network name, as it appears in the network metadata  
+//! - `unit`  
+//!
+//! However, if non-critical parameters have changes, Signer will permit the
+//! network specs updating.  
+//! These non-critical parameters are:  
+//! - `color`  
+//! - `logo`, network-associated logo picture  
+//! - `path_id`, default address derivation path for the network  
+//! - `secondary_color`  
+//! - `title`, network title, as it is displayed in Signer  
+//!
+//! Some quickly updating experimental networks are changing the genesis hash
+//! often. Network genesis hash participates in [`NetworkSpecsKey`] 
+//! generation. This way, if the genesis hash got updated, the network would 
+//! appear "unknown" to Signer, and to use it, network specs with new genesis hash
+//! would have to be added. Adding network specs with new genesis hash does not 
+//! require deleting network specs with old genesis hash.  
+//!
+//! ## Balance representation: decimals and units  
+//!
+//! To represent the balance-related values properly, each network has associated
+//! decimals and units. The balance-related values in, for example, transactions
+//! are integer numbers, and are formatted properly only during the transactions
+//! decoding. For this `decimals` and `unit` values from [`NetworkSpecs`] are used. 
+//! `decimals` indicate the order of magnitude, by which the token `unit` 
+//! exceeds the integer representing unit (see examples below).
+//! Both `decimals` and `unit` values could be queried through rpc calls for each 
+//! Signer-compatible network.  
+//! Sometimes the networks have several available decimals and units, or none at all.  
+//! This cases should be dealt with on case-by-case basis.  
+//!
+//! ## Examples: balance representation  
+//!
+//! Balance (`u64`) from transaction is decoded as `1`.
+//! Network `decimals` value is `12`, network `unit` is `WND`.
+//! The balance should be therefore represented as `1 pWND`.  
+//!
+//! Balance (`u64`) from transaction is decoded as `10000000`.
+//! Network `decimals` value is `12`, network `unit` is `WND`.
+//! The balance should be therefore represented as `10 uWND`.  
 
 use parity_scale_codec::{Decode, Encode};
 #[cfg(feature = "signer")]
@@ -23,44 +101,92 @@ use crate::{
     print::export_complex_single,
 };
 
-//TODO: rename fields to make them more clear
+/// Network parameters stored in the **cold** database 
+/// `SPECSTREE` tree under [`NetworkSpecsKey`]
+///
+/// These network parameters must be in Signer database for the Signer to be 
+/// able to operate with this network.
 #[derive(Decode, Encode, PartialEq, Debug, Clone)]
 pub struct NetworkSpecs {
+    /// Network-specific prefix for address representation in 
+    /// [base58 format](https://docs.rs/sp-core/6.0.0/sp_core/crypto/trait.Ss58Codec.html#method.to_ss58check_with_version)  
     pub base58prefix: u16,
+    /// Network-associated color.  
+    /// Historically is there, not doing much at the moment.  
     pub color: String,
+    /// Order of magnitude, by which the token unit exceeds the balance integer unit.  
+    /// Is used to display balance-related values properly.  
     pub decimals: u8,
+    /// Encryption algorithm the network uses
     pub encryption: Encryption,
+    /// Network genesis hash
     pub genesis_hash: [u8; 32],
+    /// Network associated logo
     pub logo: String,
+    /// Network name, as it appears in network metadata
     pub name: String,
+    /// Order in which the network is displayed by Signer
     pub order: u8,
+    /// Default derivation path for addresses in this network
     pub path_id: String,
+    /// Network-associated secondary color.  
+    /// Historically is there, not doing much at the moment.  
     pub secondary_color: String,
+    /// Network title, as it appears in Signer menus.
     pub title: String,
+    /// Token name, to display balance-related values properly.  
     pub unit: String,
 }
 
+/// Network parameters stored in the **hot** database 
+/// `SPECSTREEPREP` tree under [`NetworkSpecsKey`] and sent as QR code 
+/// in `add_specs` messages
+///
+/// These network parameters are sufficient to add network into Signer database.
 #[derive(Decode, Encode, PartialEq, Debug, Clone)]
 pub struct NetworkSpecsToSend {
+    /// Network-specific prefix for address representation in 
+    /// [base58 format](https://docs.rs/sp-core/6.0.0/sp_core/crypto/trait.Ss58Codec.html#method.to_ss58check_with_version)  
     pub base58prefix: u16,
+    /// Network-associated color.  
+    /// Historically is there, not doing much at the moment.  
     pub color: String,
+    /// Order of magnitude, by which the token unit exceeds the balance integer unit.  
+    /// Is used to display balance-related values properly.  
     pub decimals: u8,
+    /// Encryption algorithm the network uses  
     pub encryption: Encryption,
+    /// Network genesis hash  
     pub genesis_hash: [u8; 32],
+    /// Network associated logo  
     pub logo: String,
+    /// Network name, as it appears in network metadata  
     pub name: String,
+    /// Default derivation path for addresses in this network  
     pub path_id: String,
+    /// Network-associated secondary color.  
+    /// Historically is there, not doing much at the moment.  
     pub secondary_color: String,
+    /// Network title, as it appears in Signer menus.  
     pub title: String,
+    /// Token name, to display balance-related values properly.  
     pub unit: String,
 }
 
+/// Network parameters needed to decode and display transaction
 #[derive(Decode, Encode, PartialEq, Debug, Clone)]
 pub struct ShortSpecs {
+    /// Network-specific prefix for address representation in 
+    /// [base58 format](https://docs.rs/sp-core/6.0.0/sp_core/crypto/trait.Ss58Codec.html#method.to_ss58check_with_version)  
     pub base58prefix: u16,
+    /// Order of magnitude, by which the token unit exceeds the balance integer unit.  
+    /// Is used to display balance-related values properly.  
     pub decimals: u8,
+    /// Network genesis hash  
     pub genesis_hash: [u8; 32],
+    /// Network name, as it appears in network metadata  
     pub name: String,
+    /// Token name, to display balance-related values properly.  
     pub unit: String,
 }
 
