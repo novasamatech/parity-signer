e525a2b9d3e837e3efdab8c66d1457b80cf61555
Slesarev <slesarew@gmail.com>
2022-03-28 23:03:43 +0300

    feat: ios: minimal working rust seed recovery

--- ios/NativeSigner/Models/ScreenModels/MRecoverSeedPhrase.swift
+++ ios/NativeSigner/Models/ScreenModels/MRecoverSeedPhrase.swift
@@ -7,7 +7,27 @@
 
 import Foundation
 
-struct MRecoverSeedPhrase: Decodable {
+struct MRecoverSeedPhrase: Decodable, Equatable {
     var keyboard: Bool
     var seed_name: String
+    var user_input: String
+    var guess_set: [String]
+    var draft: [SeedWord]
+    var ready_seed: String?
+}
+
+struct SeedWord: Decodable, Equatable {
+    var order: Int
+    var content: String
+}
+
+extension MRecoverSeedPhrase {
+    func draftPhrase() -> String {
+        return self.draft
+            .sorted{word1, word2 in
+                return word1.order > word2.order
+            }.map{guessWord in
+                return guessWord.content
+            }.joined(separator: " ")
+    }
 }
