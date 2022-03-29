bda88de35e7d0fa9e280d81a29b4730d0ee3d6a7
Fedor Sakharov <fedor.sakharov@gmail.com>
2022-03-29 10:06:37 +0300

    rustfmt

--- rust/signer/build.rs
+++ rust/signer/build.rs
@@ -14,16 +14,16 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-use std::{env, path::Path, fs::create_dir_all};
+use std::{env, fs::create_dir_all, path::Path};
 
 use definitions::error::{Active, ErrorSource};
 use generate_message::{full_run, parser::Command};
 
 fn main() -> Result<(), String> {
-	  let manifest_dir = env::var_os("CARGO_MANIFEST_DIR").unwrap();
-		let cold_release_dir = Path::new(&manifest_dir)
-			                 .join("../../android/app/src/main/assets/Database/");
-		create_dir_all(&cold_release_dir).unwrap();
+    let manifest_dir = env::var_os("CARGO_MANIFEST_DIR").unwrap();
+    let cold_release_dir =
+        Path::new(&manifest_dir).join("../../android/app/src/main/assets/Database/");
+    create_dir_all(&cold_release_dir).unwrap();
     let command = Command::MakeColdRelease(Some(cold_release_dir));
 
     full_run(command).map_err(|e| <Active>::show(&e))?;
