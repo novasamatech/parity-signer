edaf867e011a21e0c831fde94e9398cc340e2a7f
Slesarev <slesarew@gmail.com>
2022-06-21 12:40:03 +0300

    docs: signer crate docs

--- rust/signer/src/lib.rs
+++ rust/signer/src/lib.rs
@@ -14,14 +14,22 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
+//! This crate serves as interface between native frontend and Rust code. Try to avoid placing any
+//! logic here, just interfacing. When porting to new platform, all Rust changes will probably
+//! happen here.
+
 #![deny(unused_crate_dependencies)]
 #![deny(rustdoc::broken_intra_doc_links)]
+#![deny(missing_docs)]
 
 mod ffi_types;
 
 use crate::ffi_types::*;
 use std::fmt::Display;
 
+/// Container for severe error message
+///
+/// TODO: implement properly or remove completely
 #[derive(Debug)]
 pub enum ErrorDisplayed {
     Str { s: String },
@@ -54,6 +62,17 @@ fn action_get_name(action: &Action) -> Option<FooterButton> {
     }
 }
 
+/// Perform action in frontend.
+///
+/// This call should be debounced.
+///
+/// Action tries to acquire lock on app state mutex and is ignored on failure.
+///
+/// 'seed_phrase' field is zeroized, it is expected to be used for secrets only.
+///
+/// `details` field is not always zeroized.
+///
+/// App view contents are returned as result, this should be sufficient to render view.
 fn backend_action(
     action: Action,
     details: &str,
@@ -62,18 +81,30 @@ fn backend_action(
     navigator::do_action(action, details, seed_phrase).map_err(|s| ErrorDisplayed::Str { s })
 }
 
+/// Should be called once at start of the app and could be called on app reset
+///
+/// Accepts list of seed names to avoid calling [`update_seed_names`] every time
 fn init_navigation(dbname: &str, seed_names: Vec<String>) {
     navigator::init_navigation(dbname, seed_names)
 }
 
+/// Should be called every time any change could have been done to seeds. Accepts updated list of
+/// seeds, completely disregards previously known list
 fn update_seed_names(seed_names: Vec<String>) {
     navigator::update_seed_names(seed_names)
 }
 
+/// Determines estimated required number of multiframe QR that should be gathered before decoding
+/// is attempted
 fn qrparser_get_packets_total(data: &str, cleaned: bool) -> anyhow::Result<u32, ErrorDisplayed> {
     qr_reader_phone::get_length(data, cleaned).map_err(Into::into)
 }
 
+/// Attempts to convert QR data (transfered as json-like string) into decoded but not parsed UOS
+/// payload
+///
+/// `cleaned` is platform-specific flag indicating whether QR payloads have QR prefix stripped by
+/// QR parsing code
 fn qrparser_try_decode_qr_sequence(
     data: &str,
     cleaned: bool,
@@ -81,6 +112,12 @@ fn qrparser_try_decode_qr_sequence(
     qr_reader_phone::decode_sequence(data, cleaned)
 }
 
+/// Checks derivation path for validity and collisions
+///
+/// Returns struct that has information on collisions, presence of password and validity of path;
+/// in case of valid path without collisions frontend should make a decision on whether to access
+/// secure storage already or check password by requesting user to re-type it, so this could not be
+/// isolated in backend navigation for now.
 fn substrate_path_check(
     seed_name: &str,
     path: &str,
@@ -90,35 +127,50 @@ fn substrate_path_check(
     db_handling::interface_signer::dynamic_path_check(dbname, seed_name, path, network)
 }
 
+/// Must be called once on normal first start of the app upon accepting conditions; relies on old
+/// data being already removed
 fn history_init_history_with_cert(dbname: &str) -> anyhow::Result<(), anyhow::Error> {
     db_handling::cold_default::signer_init_with_cert(dbname).map_err(|e| e.anyhow())
 }
 
+/// Must be called once upon jailbreak (removal of general verifier) after all old data was removed
 fn history_init_history_no_cert(dbname: &str) -> anyhow::Result<(), anyhow::Error> {
     db_handling::cold_default::signer_init_no_cert(dbname).map_err(|e| e.anyhow())
 }
 
+/// Must be called every time network detector detects network. Sets alert flag in database that could
+/// only be reset by full reset or calling [`history_acknowledge_warnings`]
+///
+/// This changes log, so it is expected to fail all operations that check that database remained
+/// intact
 fn history_device_was_online(dbname: &str) -> anyhow::Result<(), anyhow::Error> {
     db_handling::manage_history::device_was_online(dbname).map_err(|e| e.anyhow())
 }
 
+/// Checks if network alert flag was set
 fn history_get_warnings(dbname: &str) -> anyhow::Result<bool, anyhow::Error> {
     db_handling::helpers::get_danger_status(dbname).map_err(|e| e.anyhow())
 }
 
+/// Resets network alert flag; makes record of reset in log
 fn history_acknowledge_warnings(dbname: &str) -> anyhow::Result<(), anyhow::Error> {
     db_handling::manage_history::reset_danger_status_to_safe(dbname).map_err(|e| e.anyhow())
 }
 
+/// Allows frontend to send events into log; TODO: maybe this is not needed
 fn history_entry_system(event: Event, dbname: &str) -> anyhow::Result<(), anyhow::Error> {
     db_handling::manage_history::history_entry_system(dbname, event).map_err(|e| e.anyhow())
 }
 
+/// Must be called every time seed backup shows seed to user
+///
+/// Makes record in log
 fn history_seed_name_was_shown(seed_name: &str, dbname: &str) -> anyhow::Result<(), anyhow::Error> {
     db_handling::manage_history::seed_name_was_shown(dbname, seed_name.to_string())
         .map_err(|e| e.anyhow())
 }
 
+/// Test function to show all transaction cards
 fn get_all_tx_cards() -> TransactionCardSet {
     match transaction_parsing::test_all_cards::make_all_cards() {
         TransactionAction::Derivations { content, .. } => content,
@@ -128,11 +180,15 @@ fn get_all_tx_cards() -> TransactionCardSet {
     }
 }
 
+/// Test function to show all possible log events
 fn get_all_log_cards() -> String {
     String::new()
     // TODO: definitions::history::print_all_events()
 }
 
+/// Must be called once to initialize logging from Rust in development mode.
+///
+/// Do not use in production.
 #[cfg(target_os = "android")]
 fn init_logging(tag: String) {
     android_logger::init_once(
@@ -148,6 +204,9 @@ fn init_logging(tag: String) {
     );
 }
 
+/// Placeholder to init logging on non-android platforms
+///
+/// TODO: is this used?
 #[cfg(not(target_os = "android"))]
 fn init_logging(_tag: String) {
     env_logger::init();
