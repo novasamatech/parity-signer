7b71bced55fa9c5bfecbeb99a79db981059a3856
Krzysztof Rodak <krzysztof@parity.io>
2022-08-29 17:41:36 +0200

    feat: runtime SVG support on iOS with SwiftUI component

--- ios/NativeSigner/Components/Identicon.swift
+++ ios/NativeSigner/Components/Identicon.swift
@@ -5,6 +5,7 @@
 //  Created by Alexander Slesarev on 28.12.2021.
 //
 
+import SVGView
 import SwiftUI
 
 /// Parse identicon from backend into picture
@@ -18,9 +19,39 @@ struct Identicon: View {
     }
 }
 
-// struct Identicon_Previews: PreviewProvider {
-//    static var previews: some View {
-//        Identicon()
-//    }
-// }
-//
+/// Parse identicon from backend into picture
+struct SVGIdenticon: View {
+    private let svgAsData: Data
+    private let maxSize: CGSize
+
+    init(svgAsData: Data,
+         maxSize: CGSize = .init(width: 36, height: 36)) {
+        self.svgAsData = svgAsData
+        self.maxSize = maxSize
+    }
+
+    var body: some View {
+        SVGView(data: svgAsData)
+            .frame(width: maxSize.width, height: maxSize.height, alignment: .center)
+    }
+}
+
+// swiftlint: disable all
+struct Identicon_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack(alignment: .center, spacing: 10) {
+            Identicon(identicon: PreviewData.exampleIdenticon)
+            SVGIdenticon(
+                svgAsData: try! Data(
+                    contentsOf: Bundle.main.url(
+                        forResource: "identicon_example",
+                        withExtension: "svg"
+                    )!
+                ),
+                maxSize: .init(width: 200, height: 200)
+            )
+        }
+        .preferredColorScheme(.dark)
+        .previewLayout(.sizeThatFits)
+    }
+}
