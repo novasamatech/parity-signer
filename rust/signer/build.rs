d76166ec25e277e6c3a942b52b13181ae2c6718d
Dmitry Borodin <git@DmitryBorodin.com>
2022-09-08 14:30:03 +0200

    fix: fixed for scripts leftover after android folder move

--- rust/signer/build.rs
+++ rust/signer/build.rs
@@ -6,7 +6,7 @@ fn cold_release() -> Result<(), String> {
 
     let manifest_dir = env::var_os("CARGO_MANIFEST_DIR").unwrap();
     let cold_release_dir =
-        Path::new(&manifest_dir).join("../../android/app/src/main/assets/Database/");
+        Path::new(&manifest_dir).join("../../android/src/main/assets/Database/");
     create_dir_all(&cold_release_dir).unwrap();
     let command = Command::MakeColdRelease {
         path: Some(cold_release_dir),
