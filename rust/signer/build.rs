a64ffa45a1de44242ac2f54f28ddbf724c97c177
Dmitry Borodin <git@DmitryBorodin.com>
2022-09-08 14:43:15 +0200

    fix: rustfmt formatting instead of intellij one

--- rust/signer/build.rs
+++ rust/signer/build.rs
@@ -5,8 +5,7 @@ fn cold_release() -> Result<(), String> {
     use generate_message::{full_run, parser::Command};
 
     let manifest_dir = env::var_os("CARGO_MANIFEST_DIR").unwrap();
-    let cold_release_dir =
-        Path::new(&manifest_dir).join("../../android/src/main/assets/Database/");
+    let cold_release_dir = Path::new(&manifest_dir).join("../../android/src/main/assets/Database/");
     create_dir_all(&cold_release_dir).unwrap();
     let command = Command::MakeColdRelease {
         path: Some(cold_release_dir),
