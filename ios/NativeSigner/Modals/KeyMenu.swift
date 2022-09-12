42880c4b14106afac922dc5cbdf1808e107387b6
Krzysztof Rodak <krzysztof@parity.io>
2022-09-12 13:20:22 +0200

    feat: export private key on iOS

--- ios/NativeSigner/Modals/KeyMenu.swift
+++ ios/NativeSigner/Modals/KeyMenu.swift
@@ -6,21 +6,41 @@
 //
 
 import SwiftUI
+//
+// final class KeyMenuActionModel: ObservableObject {
+//    var networkInfo:
+// }
 
 struct KeyMenu: View {
     @State private var removeConfirm = false
+    @State private var shouldPresentExportKeysModal = false
+    @State private var isPresentingExportKeysWarningModal = false
+    @State private var isPresentingExportKeysModal = false
+    let exportKeyService: ExportPrivateKeyService
+    @ObservedObject var navigation: NavigationCoordinator
 
-    let navigationRequest: NavigationRequest
     var body: some View {
         MenuStack {
             HeaderBar(line1: Localizable.keyMenu.key, line2: Localizable.selectAction.key).padding(.top, 10)
             MenuButtonsStack {
+                // Don't show `Export Private Key` if intermediate state is broken or when key is password protected
+                if let currentKeyDetails = navigation.currentKeyDetails,
+                   currentKeyDetails.address.hasPwd == false {
+                    BigButton(
+                        text: Localizable.KeyScreen.Action.export.key,
+                        isShaded: false,
+                        isDangerous: false,
+                        action: {
+                            isPresentingExportKeysWarningModal = true
+                        }
+                    )
+                }
                 BigButton(
                     text: Localizable.forgetThisKeyForever.key,
                     isShaded: true,
                     isDangerous: true,
                     action: {
-                        removeConfirm = true
+                        removeConfirm.toggle()
                     }
                 )
             }
@@ -32,10 +52,42 @@ struct KeyMenu: View {
                 primaryButton: .cancel(Localizable.cancel.text),
                 secondaryButton: .destructive(
                     Localizable.removeKey.text,
-                    action: { navigationRequest(.init(action: .removeKey)) }
+                    action: { navigation.perform(navigation: .init(action: .removeKey)) }
                 )
             )
         })
+        .fullScreenCover(
+            isPresented: $isPresentingExportKeysWarningModal,
+            onDismiss: {
+                if shouldPresentExportKeysModal {
+                    shouldPresentExportKeysModal.toggle()
+                    isPresentingExportKeysModal.toggle()
+                } else {
+                    // If user cancelled, mimic Rust state machine and hide "..." modal menu
+                    navigation.perform(navigation: .init(action: .rightButtonAction))
+                }
+            }
+        ) {
+            ExportPrivateKeyWarningModal(
+                isPresentingExportKeysWarningModal: $isPresentingExportKeysWarningModal,
+                shouldPresentExportKeysModal: $shouldPresentExportKeysModal
+            )
+            .clearModalBackground()
+        }
+        .fullScreenCover(
+            isPresented: $isPresentingExportKeysModal,
+            onDismiss: {
+                // When user finished Export Private Key interaction, mimic Rust state machine and hide "..." modal menu
+                navigation.perform(navigation: .init(action: .rightButtonAction))
+            }
+        ) {
+            ExportPrivateKeyModal(
+                isPresentingExportKeysModal: $isPresentingExportKeysModal,
+                navigation: navigation,
+                viewModel: exportKeyService.exportPrivateKey(from: navigation.currentKeyDetails)
+            )
+            .clearModalBackground()
+        }
     }
 }
 
