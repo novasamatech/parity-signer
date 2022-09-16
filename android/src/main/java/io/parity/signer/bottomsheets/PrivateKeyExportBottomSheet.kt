94fb8507825b6345c2e3c80efd7ea68806c690d3
Dmitry Borodin <git@DmitryBorodin.com>
2022-09-16 21:56:38 +0200

    countdown work in progress

--- android/src/main/java/io/parity/signer/bottomsheets/PrivateKeyExportBottomSheet.kt
+++ android/src/main/java/io/parity/signer/bottomsheets/PrivateKeyExportBottomSheet.kt
@@ -5,22 +5,17 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.graphics.drawable.toBitmap
 import io.parity.signer.R
 import io.parity.signer.components.NetworkCardModel
+import io.parity.signer.components2.CountDown
 import io.parity.signer.components2.KeyCard
 import io.parity.signer.components2.KeyCardModel
 import io.parity.signer.models.EmptyNavigator
@@ -28,7 +23,6 @@ import io.parity.signer.models.Navigator
 import io.parity.signer.models.intoImageBitmap
 import io.parity.signer.ui.helpers.PreviewData
 import io.parity.signer.ui.theme.*
-import io.parity.signer.uniffi.Action
 import io.parity.signer.uniffi.Address
 
 @Composable
@@ -38,8 +32,8 @@ fun PrivateKeyExportBottomSheet(
 ) {
 	Column(
 		modifier = Modifier
-			.clickable { navigator.backAction() }
-			.fillMaxWidth()
+            .clickable { navigator.backAction() }
+            .fillMaxWidth()
 	) {
 		Spacer(Modifier.weight(1f))
 		Surface(
@@ -53,9 +47,11 @@ fun PrivateKeyExportBottomSheet(
 			) {
 				Row(
 					modifier = Modifier
-						.padding(top = sidePadding, bottom = sidePadding,
-							start = sidePadding, end = sidePadding)
-						.fillMaxWidth()
+                        .padding(
+                            top = sidePadding, bottom = sidePadding,
+                            start = sidePadding, end = sidePadding
+                        )
+                        .fillMaxWidth()
 				) {
 					Text(
 						text = stringResource(R.string.export_private_key_title),
@@ -65,24 +61,25 @@ fun PrivateKeyExportBottomSheet(
 				}
 
 				val qrRounding = 16.dp
-				val plateShape = RoundedCornerShape(qrRounding, qrRounding, qrRounding, qrRounding)
+				val plateShape =
+					RoundedCornerShape(qrRounding, qrRounding, qrRounding, qrRounding)
 				Column(
 					modifier = Modifier
-						.padding(start = sidePadding, end = sidePadding)
-						.clip(plateShape)
-						.border(
-							BorderStroke(1.dp, MaterialTheme.colors.fill12),
-							plateShape
-						)
-						.background(MaterialTheme.colors.fill12, plateShape)
+                        .padding(start = sidePadding, end = sidePadding)
+                        .clip(plateShape)
+                        .border(
+                            BorderStroke(1.dp, MaterialTheme.colors.fill12),
+                            plateShape
+                        )
+                        .background(MaterialTheme.colors.fill12, plateShape)
 				) {
 					Image(
 						bitmap = model.qrImage.intoImageBitmap(),
 						contentDescription = stringResource(R.string.qr_with_address_to_scan_description),
 						contentScale = ContentScale.FillWidth,
 						modifier = Modifier
-							.fillMaxWidth(1f)
-							.clip(RoundedCornerShape(qrRounding))
+                            .fillMaxWidth(1f)
+                            .clip(RoundedCornerShape(qrRounding))
 					)
 					KeyCard(
 						KeyCardModel.fromAddress(
@@ -93,6 +90,10 @@ fun PrivateKeyExportBottomSheet(
 					Spacer(modifier = Modifier.padding(bottom = 4.dp))
 				}
 				Spacer(modifier = Modifier.padding(bottom = 24.dp))
+				//autohide component
+				val timeout = 45 //seconds
+				val text = "Private Key will be hidden in "
+				CountDown(timeout, text) { navigator.backAction() }
 			}
 		}
 	}
@@ -120,10 +121,17 @@ class PrivateKeyExportModel(
 	}
 }
 
-@Preview(name = "day", group = "themes", uiMode = UI_MODE_NIGHT_NO, showBackground = true,
-	backgroundColor = 0)
-@Preview(name = "dark theme",	group = "themes",	uiMode = UI_MODE_NIGHT_YES,
-	showBackground = true, backgroundColor = 0xFF000000)
+@Preview(
+	name = "day",
+	group = "themes",
+	uiMode = UI_MODE_NIGHT_NO,
+	showBackground = true,
+	backgroundColor = 0
+)
+@Preview(
+	name = "dark theme", group = "themes", uiMode = UI_MODE_NIGHT_YES,
+	showBackground = true, backgroundColor = 0xFF000000
+)
 @Composable
 private fun PreviewPrivateKeyExportBottomSheet() {
 	SignerNewTheme {
