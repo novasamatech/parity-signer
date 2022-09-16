b618bc1d69150959573be87d49afb78eeb5a7eeb
Fedor Sakharov <fedor.sakharov@gmail.com>
2022-09-16 10:52:11 +0300

    fix: move android db generation to gradle (#1292)
    * fix: move android db generation to gradle
    
    * bring back build.rs
    
    * add db generation script
    
    * bash

---
+++ android/generate_database.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+RELEASE_PATH=../rust/database/database_cold_release
+DBPATH=./src/main/assets/Database
+
+set -e
+
+pushd ../rust/generate_message
+
+cargo run --locked make-cold-release
+
+popd
+
+# Move database to assets
+
+echo "Re-creating $DBPATH"
+rm -rf $DBPATH
+mkdir -p $DBPATH
+
+echo "Copying DB from $RELEASE_PATH to $DBPATH"
+cp -R $RELEASE_PATH/ $DBPATH
