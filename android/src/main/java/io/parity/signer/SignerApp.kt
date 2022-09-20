71d90b8c708e3bcb1692cfcbd55d6c25b4b81a8e
Dmitry Borodin <git@DmitryBorodin.com>
2022-09-20 10:58:55 +0200

    feat: show rust-caused error message in android logs when app is crashing

---
+++ android/src/main/java/io/parity/signer/SignerApp.kt
@@ -0,0 +1,28 @@
+package io.parity.signer
+
+import android.app.Application
+import android.util.Log
+import io.parity.signer.uniffi.ErrorDisplayed
+import java.lang.Thread.UncaughtExceptionHandler
+
+class SignerApp : Application() {
+	override fun onCreate() {
+		super.onCreate()
+		Thread.setDefaultUncaughtExceptionHandler(SignerExceptionHandler())
+	}
+}
+
+
+class SignerExceptionHandler : UncaughtExceptionHandler {
+	private val TAG = "SignerExceptionHandler"
+
+	override tailrec fun uncaughtException(t: Thread, e: Throwable) {
+		if (e is ErrorDisplayed.Str) {
+			Log.e(TAG, "Rust caused ErrorDisplay message was: ${e.s}")
+		}
+		val cause = e.cause
+		if (cause != null) {
+			uncaughtException(Thread.currentThread(), cause)
+		}
+	}
+}
