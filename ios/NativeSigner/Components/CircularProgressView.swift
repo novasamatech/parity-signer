c2a3a78cf064ca3b5c986d2d2791123282f3f39a
Krzysztof Rodak <krzysztof@parity.io>
2022-09-21 11:23:16 +0200

    style: seed phrase view and new snackbar style

--- ios/NativeSigner/Components/CircularProgressView.swift
+++ ios/NativeSigner/Components/CircularProgressView.swift
@@ -12,27 +12,41 @@ struct CircularCountdownViewModel {
     let lineWidth: CGFloat
     let backgroundColor: Color
     let foregroundColor: Color
+    let foregroundFontColor: Color
 
     /// Circular progress view configuration to be used in `Export Private Key` flow
     static let privateKeyCountdown = CircularCountdownViewModel(
         size: 32,
         lineWidth: 2.5,
         backgroundColor: .clear,
-        foregroundColor: Asset.accentPink300.swiftUIColor
+        foregroundColor: Asset.accentPink300.swiftUIColor,
+        foregroundFontColor: Asset.textAndIconsPrimary.swiftUIColor
+    )
+    /// Circular progress view configuration to be used in `Snackbar` component
+    static let snackbarCountdown = CircularCountdownViewModel(
+        size: 32,
+        lineWidth: 2.5,
+        backgroundColor: .clear,
+        foregroundColor: Asset.accentPink300.swiftUIColor,
+        foregroundFontColor: Asset.accentForegroundText.swiftUIColor
     )
 }
 
 struct CircularCountdownModel {
     // For how many seconds countdown should go
     let counter: CGFloat
+    /// View model for circular countdown
+    let viewModel: CircularCountdownViewModel
     /// Action that should get triggered on countdown completion
     let onCompletion: () -> Void
 
     init(
         counter: CGFloat,
+        viewModel: CircularCountdownViewModel,
         onCompletion: @escaping () -> Void
     ) {
         self.counter = counter
+        self.viewModel = viewModel
         self.onCompletion = onCompletion
     }
 }
@@ -44,15 +58,13 @@ struct CircularProgressView: View {
     }
 
     private let model: CircularCountdownModel
-    private let viewModel: CircularCountdownViewModel
     @State private var counter: CGFloat
     @State private var timer = Timer
         .publish(every: Constants.animationPrecision, on: .main, in: .common)
         .autoconnect()
 
-    init(_ model: CircularCountdownModel, viewModel: CircularCountdownViewModel) {
+    init(_ model: CircularCountdownModel) {
         self.model = model
-        self.viewModel = viewModel
         counter = model.counter
     }
 
@@ -60,15 +72,15 @@ struct CircularProgressView: View {
         ZStack {
             Circle()
                 .stroke(
-                    viewModel.backgroundColor,
-                    lineWidth: viewModel.lineWidth
+                    model.viewModel.backgroundColor,
+                    lineWidth: model.viewModel.lineWidth
                 )
             Circle()
                 .trim(from: 0, to: normalisedProgress())
                 .stroke(
-                    viewModel.foregroundColor,
+                    model.viewModel.foregroundColor,
                     style: StrokeStyle(
-                        lineWidth: viewModel.lineWidth,
+                        lineWidth: model.viewModel.lineWidth,
                         lineCap: .round,
                         lineJoin: .round
                     )
@@ -76,10 +88,10 @@ struct CircularProgressView: View {
                 .rotationEffect(Constants.startingAngle)
                 .animation(.linear, value: normalisedProgress())
             Text(String(Int(counter)))
-                .foregroundColor(Asset.textAndIconsPrimary.swiftUIColor)
+                .foregroundColor(model.viewModel.foregroundFontColor)
                 .font(Fontstyle.labelS.base)
         }
-        .frame(width: viewModel.size, height: viewModel.size, alignment: .center)
+        .frame(width: model.viewModel.size, height: model.viewModel.size, alignment: .center)
         .onReceive(timer) { _ in
             if counter > 0 {
                 counter -= Constants.animationPrecision
