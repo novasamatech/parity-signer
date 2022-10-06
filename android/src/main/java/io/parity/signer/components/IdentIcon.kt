68cbbdda8cf9efd7cf7b0660387ef318885606d9
Fedor Sakharov <fedor.sakharov@gmail.com>
2022-10-06 09:40:10 +0300

    Update android/src/main/java/io/parity/signer/components/IdentIcon.kt

--- android/src/main/java/io/parity/signer/components/IdentIcon.kt
+++ android/src/main/java/io/parity/signer/components/IdentIcon.kt
@@ -3,7 +3,6 @@ package io.parity.signer.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
