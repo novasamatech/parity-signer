67cd79b293db638a5312b4608e654f911b0d7d09
Krzysztof Rodak <krzysztof@parity.io>
2022-10-12 14:37:09 +0200

    feat: ios: error on Rust navigation errors

--- ios/NativeSigner/Core/Adapters/BackendNavigationAdapter.swift
+++ ios/NativeSigner/Core/Adapters/BackendNavigationAdapter.swift
@@ -7,6 +7,15 @@
 
 import Foundation
 
+struct NavigationError: Error, CustomStringConvertible {
+    let message: String
+
+    var description: String {
+        [Localizable.Error.Navigation.Label.prefix.string, message, Localizable.Error.Navigation.Label.suffix.string]
+            .joined(separator: "\n")
+    }
+}
+
 /// Protocol that reflects backend ability to navigate
 protocol BackendNavigationPerforming: AnyObject {
     /// Wrapper for Rust backend navigation public function that performs navigation.
@@ -16,21 +25,27 @@ protocol BackendNavigationPerforming: AnyObject {
     ///   - details: Additional data to be send along with `action`
     ///   - seedPhrase: Seed phrase required to be sent along with some `action`s
     /// - Returns: `ActionResult` if action was valid, `nil` otherwise
-    func performBackend(action: Action, details: String, seedPhrase: String) -> ActionResult?
+    func performBackend(action: Action, details: String, seedPhrase: String) -> Result<ActionResult, NavigationError>
 }
 
 /// We don't want to use module-wide public functions as there is no way of mocking them in unit  / UI tests
 /// This adapters acts as a wrapper for public function for navigation
 final class BackendNavigationAdapter: BackendNavigationPerforming {
-    func performBackend(action: Action, details: String, seedPhrase: String) -> ActionResult? {
-        guard let result = try? backendAction(
-            action: action,
-            details: details,
-            seedPhrase: seedPhrase
-        ) else { return nil }
-        if case let .sufficientCryptoReady(value) = result.modalData {
-            print(value)
+    func performBackend(action: Action, details: String, seedPhrase: String) -> Result<ActionResult, NavigationError> {
+        do {
+            guard let actionResult = try backendAction(
+                action: action,
+                details: details,
+                seedPhrase: seedPhrase
+            ) else {
+                return .failure(.init(
+                    message: Localizable.Error.Navigation.Label
+                        .noAction(String(describing: action), details)
+                ))
+            }
+            return .success(actionResult)
+        } catch {
+            return .failure(.init(message: Localizable.Error.Navigation.Label.message(error.localizedDescription)))
         }
-        return result
     }
 }
