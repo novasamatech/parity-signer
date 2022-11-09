93afb967752218bbcd5d0a4d5a63fafd777a2992
Dmitry Borodin <git@DmitryBorodin.com>
2022-11-09 19:15:29 +0100

    scan work in progress 2

--- android/src/main/java/io/parity/signer/screens/scan/ScanScreen.kt
+++ android/src/main/java/io/parity/signer/screens/scan/ScanScreen.kt
@@ -1,15 +1,21 @@
 package io.parity.signer.screens.scan
 
 import android.content.res.Configuration
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import io.parity.signer.components.base.CloseIcon
 import io.parity.signer.models.Callback
-import io.parity.signer.models.EmptyNavigator
 import io.parity.signer.models.KeySetDetailsModel
 import io.parity.signer.ui.theme.SignerNewTheme
 
@@ -18,6 +24,12 @@ import io.parity.signer.ui.theme.SignerNewTheme
 fun ScanScreen(
 	onClose: Callback
 ) {
+	val interactor: CameraViewModel = viewModel()
+	val lifecycleOwner = LocalLifecycleOwner.current
+	val context = LocalContext.current
+	val cameraProviderFuture =
+		remember { ProcessCameraProvider.getInstance(context) }
+
 	Box() {
 		ScanHeader(onClose)
 	}
@@ -27,7 +39,8 @@ fun ScanScreen(
 @Composable
 fun ScanHeader(onClose: Callback) {
 	Row() {
-
+		CloseIcon(modifier = Modifier.padding(vertical = 20.dp),
+			onCloseClicked = onClose)
 	}
 }
 
@@ -45,7 +58,7 @@ private fun PreviewScanScreen() {
 	val mockModel = KeySetDetailsModel.createStub()
 	SignerNewTheme {
 		Box(modifier = Modifier.size(350.dp, 550.dp)) {
-			ScanScreen(mockModel, EmptyNavigator(), {})
+			ScanScreen({})
 		}
 	}
 }
