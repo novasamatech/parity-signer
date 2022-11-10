d481a0f99f96cda3b27e071b705b5b5983efd91b
Dmitry Borodin <git@DmitryBorodin.com>
2022-11-10 14:31:52 +0100

    permission request added

--- android/src/main/java/io/parity/signer/screens/scan/ScanScreen.kt
+++ android/src/main/java/io/parity/signer/screens/scan/ScanScreen.kt
@@ -2,13 +2,14 @@ package io.parity.signer.screens.scan
 
 import android.content.res.Configuration
 import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalInspectionMode
@@ -16,10 +17,16 @@ import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.accompanist.permissions.shouldShowRationale
+import io.parity.signer.components.KeepScreenOn
 import io.parity.signer.components.base.CloseIcon
 import io.parity.signer.models.Callback
 import io.parity.signer.models.KeySetDetailsModel
 import io.parity.signer.ui.theme.SignerNewTheme
+import kotlinx.coroutines.launch
 
 //todo dmitry add kep scree on from phrase box like in old screen
 @Composable
@@ -33,17 +40,47 @@ fun ScanScreen(
 	val total = interactor.total.observeAsState()
 
 	val lifecycleOwner = LocalLifecycleOwner.current
-	val context = LocalContext.current
-	val cameraProviderFuture =
-		if (!LocalInspectionMode.current) {
-			remember { ProcessCameraProvider.getInstance(context) }
-		} else null
 
-	Box() {
+
+	Box {
 		ScanHeader(onClose)
+		CameraView()
 	}
+	KeepScreenOn()
 }
 
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun CameraView() {
+	if (LocalInspectionMode.current) return
+
+	val context = LocalContext.current
+	val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
+
+	val cameraPermissionState = rememberPermissionState(android.Manifest.permission.CAMERA)
+	if (cameraPermissionState.status.isGranted) {
+		Text("Camera permission Granted")
+	} else {
+		val scope = rememberCoroutineScope()
+		Column {
+			if (cameraPermissionState.status.shouldShowRationale) {
+				AlertDialog(
+					onDismissRequest = { },
+					confirmButton = {
+						Button(onClick = { scope.launch { cameraPermissionState.launchPermissionRequest() } }) {
+							Text(text = "OK")
+						}
+					},
+					title = { Text(text = "Camera required!")},
+					text = { Text(text = "To work with QR code we need camera permission, this is main functionality of this app!")},
+				)
+			} else {
+				Text("Camera not available")
+				scope.launch { cameraPermissionState.launchPermissionRequest() }
+			}
+		}
+	}
+}
 
 @Composable
 fun ScanHeader(onClose: Callback) {
