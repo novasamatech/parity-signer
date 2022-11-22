02fd3517d8069a1f54e87f18e10193cb51d63207
Dmitry Borodin <git@DmitryBorodin.com>
2022-11-23 00:28:30 +0200

    new key set screen work in progress

---
+++ android/src/main/java/io/parity/signer/screens/keysets/create/NewKeySetScreenFirst.kt
@@ -0,0 +1,118 @@
+package io.parity.signer.screens.keysets.create
+
+import android.content.res.Configuration
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import io.parity.signer.R
+import io.parity.signer.components.base.DotsIndicator
+import io.parity.signer.components.base.ScreenHeaderClose
+import io.parity.signer.components.sharedcomponents.KeyCard
+import io.parity.signer.components.sharedcomponents.KeySeedCard
+import io.parity.signer.models.*
+import io.parity.signer.ui.theme.*
+import io.parity.signer.uniffi.Action
+
+/**
+ * Default main screen with list Seeds/root keys
+ */
+@Composable
+fun NewKeySetScreenFirst(
+	rootNavigator: Navigator,
+	seedNames: Array<String>
+) {
+	Column(Modifier.background(MaterialTheme.colors.background)) {
+		Row() {
+			//close
+//			DotsIndicator()
+			//next button
+		}
+
+		Box(modifier = Modifier.weight(1f)) {
+			Column(
+				modifier = Modifier.verticalScroll(rememberScrollState())
+			) {
+				val qrRounding = dimensionResource(id = R.dimen.qrShapeCornerRadius)
+				val plateShape =
+					RoundedCornerShape(qrRounding, qrRounding, qrRounding, qrRounding)
+				Column(
+					modifier = Modifier
+						.padding(start = 24.dp, end = 24.dp, top = 50.dp, bottom = 8.dp)
+						.clip(plateShape)
+						.border(
+							BorderStroke(1.dp, MaterialTheme.colors.appliedStroke),
+							plateShape
+						)
+						.background(MaterialTheme.colors.fill6, plateShape)
+				) {
+					Box(
+						modifier = Modifier
+							.fillMaxWidth(1f)
+							.aspectRatio(1.1f)
+							.background(
+								Color.White,
+								RoundedCornerShape(qrRounding)
+							),
+						contentAlignment = Alignment.Center,
+					) {
+						Image(
+							bitmap = model.qr.intoImageBitmap(),
+							contentDescription = stringResource(R.string.qr_with_address_to_scan_description),
+							contentScale = ContentScale.Fit,
+							modifier = Modifier.size(264.dp)
+						)
+					}
+					if (model.isRootKey) {
+						KeySeedCard(
+							seedTitle = model.address.seedName,
+							base58 = model.address.base58
+						)
+					} else {
+						KeyCard(model.address)
+					}
+				}
+				if (model.secretExposed) {
+					ExposedKeyAlarm()
+				}
+			}
+		}
+	}
+}
+
+
+@Preview(
+	name = "light", group = "general", uiMode = Configuration.UI_MODE_NIGHT_NO,
+	showBackground = true, backgroundColor = 0xFFFFFFFF,
+)
+@Preview(
+	name = "dark", group = "general",
+	uiMode = Configuration.UI_MODE_NIGHT_YES,
+	showBackground = true, backgroundColor = 0xFF000000,
+)
+@Composable
+private fun PreviewNewKeySetScreenFirst() {
+	val state = remember { mutableStateOf(AlertState.Past) }
+	val mockModel = KeyDetailsModel.createStubRoot()
+	SignerNewTheme {
+		Box(modifier = Modifier.size(350.dp, 700.dp)) {
+			NewKeySetScreenFirst(mockModel, EmptyNavigator())
+		}
+	}
+}
