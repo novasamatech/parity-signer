1b883b87d4b5a8d73a3bedc6d51dee677b6e913c
Dmitry Borodin <git@DmitryBorodin.com>
2022-11-29 12:27:09 +0200

    added background for settings to not see old headers below

--- android/src/main/java/io/parity/signer/screens/settings/SettingsScreen.kt
+++ android/src/main/java/io/parity/signer/screens/settings/SettingsScreen.kt
@@ -1,14 +1,11 @@
 package io.parity.signer.screens.settings
 
 import android.content.res.Configuration
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -46,7 +43,7 @@ fun SettingsScreen(
 ) {
 	var confirm by remember { mutableStateOf(false) }
 
-	Column() {
+	Column(Modifier.background(MaterialTheme.colors.background)) {
 		ScreenHeader(stringId = R.string.settings_title)
 
 		Column(Modifier.verticalScroll(rememberScrollState())) {
