ceb43fd82397a4a32ff0ec6bc8fb822509e144c5
Dmitry Borodin <git@DmitryBorodin.com>
2022-12-05 20:20:11 +0200

    fix: android default error handler reimplemented - still crash for all default errors

--- android/src/main/java/io/parity/signer/SignerApp.kt
+++ android/src/main/java/io/parity/signer/SignerApp.kt
@@ -3,6 +3,7 @@ package io.parity.signer
 import android.app.Application
 import android.util.Log
 import io.parity.signer.dependencygraph.ServiceLocator
+import io.parity.signer.models.submitErrorState
 import io.parity.signer.uniffi.ErrorDisplayed
 import io.parity.signer.uniffi.initLogging
 import java.lang.Thread.UncaughtExceptionHandler
@@ -11,22 +12,40 @@ class SignerApp : Application() {
 	override fun onCreate() {
 		super.onCreate()
 		initLogging("SIGNER_RUST_LOG")
-		Thread.setDefaultUncaughtExceptionHandler(SignerExceptionHandler())
+
+		val defaultHandler = Thread.getDefaultUncaughtExceptionHandler()
+		Thread.setDefaultUncaughtExceptionHandler(
+			SignerExceptionHandler(defaultHandler)
+		)
+
 		ServiceLocator.initBackendDeps(this)
 	}
 }
 
 
-class SignerExceptionHandler : UncaughtExceptionHandler {
+class SignerExceptionHandler(private val defaultHandler: UncaughtExceptionHandler) :
+	UncaughtExceptionHandler {
 	private val TAG = "SignerExceptionHandler"
 
-	override tailrec fun uncaughtException(t: Thread, e: Throwable) {
-		if (e is ErrorDisplayed.Str) {
-			Log.e(TAG, "Rust caused ErrorDisplay message was: ${e.s}")
+	override fun uncaughtException(t: Thread, e: Throwable) {
+		val rustStr = findErrorDisplayed(e)
+		if (rustStr != null) {
+			Log.e(TAG, "Rust caused ErrorDisplay message was: ${rustStr.s}")
+			submitErrorState("rust error not handled, fix it!")
+		} else {
+			defaultHandler.uncaughtException(t, e)
 		}
-		val cause = e.cause
-		if (cause != null) {
-			uncaughtException(Thread.currentThread(), cause)
+	}
+
+	private tailrec fun findErrorDisplayed(exception: Throwable): ErrorDisplayed.Str? {
+		if (exception is ErrorDisplayed.Str) {
+			return exception
+		}
+		val cause = exception.cause
+		return if (cause != null) {
+			findErrorDisplayed(cause)
+		} else {
+			null
 		}
 	}
 }
