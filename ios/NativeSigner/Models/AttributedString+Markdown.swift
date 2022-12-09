9b4a88b8732b0e2fe033d6ed28bf74aba3952d05
Dmitry Borodin <11879032+Dmitry-Borodin@users.noreply.github.com>
2022-12-09 07:38:16 +0200

    feat: make rust to pass clean Strings for transaction messages instead of HEX (#1460)
    * feat: make rust to pass clean Strings for transaction messages that should be shown as String to the user.
    
    passing markdown String as Strings rather than character numbers encoded in HEX in String
    We don't need to hide symbols as we don't pass strings in JSON neither using JS code that can execute strings from messages. This to avoid confusion that some strings can be shown and some others need to be converted while they are same type and no docs pointing at such behaviour.
    
    * feat: iOS: simplify markdow display
    
    Co-authored-by: Krzysztof Rodak <krzysztof@parity.io>
    Co-authored-by: Fedor Sakharov <fedor.sakharov@gmail.com>

--- ios/NativeSigner/Models/AttributedString+Markdown.swift
+++ ios/NativeSigner/Models/AttributedString+Markdown.swift
@@ -11,9 +11,9 @@ import Foundation
 /// EDIT: I needed to correct it to return nil if string would end up empty, as without it,
 /// no fallback would work as invalid markdown would result in valid but empty `AttributedString`
 extension AttributedString {
-    static func build(fromHexDocs string: String, allowsEmptyValue: Bool = false) -> AttributedString? {
+    static func build(fromDocs string: String, allowsEmptyValue: Bool = false) -> AttributedString? {
         if let result = try? self.init(
-            markdown: Data(fromHexEncodedString: string) ?? Data(),
+            markdown: string,
             options: AttributedString.MarkdownParsingOptions(
                 interpretedSyntax: .inlineOnlyPreservingWhitespace,
                 failurePolicy: .returnPartiallyParsedIfPossible
