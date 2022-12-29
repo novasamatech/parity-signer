b5631f02ee8aabbb75bac49351b492abd1bc2c91
Dmitry Borodin <git@DmitryBorodin.com>
2022-12-29 08:16:32 -0500

    just preveiew updated

--- android/src/main/java/io/parity/signer/models/ViewModels.kt
+++ android/src/main/java/io/parity/signer/models/ViewModels.kt
@@ -41,7 +41,11 @@ data class KeySetDetailsModel(
 				secretExposed = false,
 				seedName = "sdsdsd",
 			),
-			network = NetworkInfoModel("network title", "network logo", "keyspeks..."),
+			network = NetworkInfoModel(
+				networkTitle = "network title",
+				networkLogo = "network logo",
+				networkSpecsKey = "01e143f23803ac50e8f6f8e62695d1ce9e4e1d68aa36c1cd2cfd15340213f3423e"
+			),
 		)
 	}
 }
