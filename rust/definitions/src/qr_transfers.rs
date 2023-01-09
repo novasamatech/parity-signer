8d638ebc53726b0fddf986e81dda76275da2d987
Fedor Sakharov <fedor.sakharov@gmail.com>
2023-01-09 18:25:08 +0300

    no whitespace changes

--- rust/definitions/src/qr_transfers.rs
+++ rust/definitions/src/qr_transfers.rs
@@ -1,25 +1,25 @@
-//! Information that could be send and received through air-gap as QR codes content
+//! Information that could be send and received through air-gap as QR codes content  
 //!
 //! All information that Signer could get without a complete wipe must arrive
 //! through the QR codes maintaining the air-gap. In addition to transactions
 //! that could be parsed and signed, this includes:
-//! - adding new networks,
-//! - updating the metadata of existing networks,
-//! - updating the types information,
-//! - bulk-importing the derivations
+//! - adding new networks,  
+//! - updating the metadata of existing networks,  
+//! - updating the types information,  
+//! - bulk-importing the derivations  
 //!
 //! QR codes for adding networks, loading metadata and updating types information
 //! all have similar structure:
-//! - prelude `53xxyy` where `xx` is the encryption type, and `yy` is the message type
-//! - verifier public key (if the QR code is signed by verifier)
-//! - content
-//! - verifier signature (if the QR code is signed by verifier)
+//! - prelude `53xxyy` where `xx` is the encryption type, and `yy` is the message type  
+//! - verifier public key (if the QR code is signed by verifier)  
+//! - content  
+//! - verifier signature (if the QR code is signed by verifier)  
 //!
 //! QR codes for importing derivations are never signed and have structure:
 //! - prelude `53ffde`
-//! - content
+//! - content  
 //!
-//! This module deals with content part of QR codes.
+//! This module deals with content part of QR codes.  
 
 use parity_scale_codec::{Decode, Encode};
 #[cfg(feature = "active")]
@@ -35,7 +35,7 @@ use sp_core::H256;
 #[cfg(feature = "signer")]
 use crate::navigation::SignerImage;
 
-/// `load_metadata` QR code content
+/// `load_metadata` QR code content  
 ///
 /// Messages `load_metadata` are used to update through air-gap the network
 /// metadata for networks already known to the Signer.
@@ -103,9 +103,9 @@ impl ContentLoadMeta {
     }
 }
 
-/// `add_specs` QR code content
+/// `add_specs` QR code content  
 ///
-/// Messages `add_specs` are used to add new networks to the Signer.
+/// Messages `add_specs` are used to add new networks to the Signer.  
 #[derive(Decode, Encode)]
 pub struct ContentAddSpecs(Vec<u8>);
 
@@ -161,15 +161,15 @@ impl ContentAddSpecs {
     }
 }
 
-/// `load_types` QR code content
+/// `load_types` QR code content  
 ///
-/// Messages `load_types` are used to add or update Signer types information.
+/// Messages `load_types` are used to add or update Signer types information.  
 ///
 /// Externally acquired types information is needed only for
 /// [`RuntimeMetadata`](https://docs.rs/frame-metadata/15.0.0/frame_metadata/enum.RuntimeMetadata.html)
 /// `V13` and below. After `V14` all types information is contained within the metadata.
 ///
-/// This kind of messages is expected to be used seldom, if ever.
+/// This kind of messages is expected to be used seldom, if ever.  
 #[derive(Decode, Encode)]
 pub struct ContentLoadTypes(Vec<u8>);
 
@@ -179,7 +179,7 @@ struct DecodedContentLoadTypes {
 }
 
 impl ContentLoadTypes {
-    /// Generate [`ContentLoadTypes`] from types information `&[TypeEntry]`.
+    /// Generate [`ContentLoadTypes`] from types information `&[TypeEntry]`.  
     pub fn generate(types: &[TypeEntry]) -> Self {
         Self(
             DecodedContentLoadTypes {
@@ -189,17 +189,17 @@ impl ContentLoadTypes {
         )
     }
 
-    /// Transform `&[u8]` slice into [`ContentLoadTypes`].
+    /// Transform `&[u8]` slice into [`ContentLoadTypes`].  
     pub fn from_slice(slice: &[u8]) -> Self {
         Self(slice.to_vec())
     }
 
-    /// Get types information `Vec<TypeEntry>` from [`ContentLoadTypes`].
+    /// Get types information `Vec<TypeEntry>` from [`ContentLoadTypes`].  
     pub fn types(&self) -> Result<Vec<TypeEntry>> {
         Ok(<DecodedContentLoadTypes>::decode(&mut &self.0[..])?.types)
     }
 
-    /// Write [`ContentLoadTypes`] into file that could be signed by the verifier.
+    /// Write [`ContentLoadTypes`] into file that could be signed by the verifier.  
     #[cfg(feature = "active")]
     pub fn write<P>(&self, path: P) -> Result<()>
     where
@@ -208,31 +208,31 @@ impl ContentLoadTypes {
         Ok(std::fs::write(path, self.to_sign())?)
     }
 
-    /// Transform [`ContentLoadTypes`] into `Vec<u8>` to be put in the database.
+    /// Transform [`ContentLoadTypes`] into `Vec<u8>` to be put in the database.  
     pub fn store(&self) -> Vec<u8> {
         self.0.to_vec()
     }
 
-    /// Transform [`ContentLoadTypes`] into `Vec<u8>` that could be signed by the verifier.
+    /// Transform [`ContentLoadTypes`] into `Vec<u8>` that could be signed by the verifier.  
     pub fn to_sign(&self) -> Vec<u8> {
         self.0.to_vec()
     }
 
     /// Transform [`ContentLoadTypes`] into `Vec<u8>` that is concatenated with
-    /// other parts of the QR code.
+    /// other parts of the QR code.  
     ///
     /// Note that it is different from `.to_sign()` function. Effectively, already
     /// SCALE-encoded `Vec<TypeEntry>` is encoded second time as an opaque
     /// `Vec<u8>`. This is done to have encoded piece length announced at the
-    /// beginning of the `u8` set, to simplify cutting the received message in Signer.
+    /// beginning of the `u8` set, to simplify cutting the received message in Signer.  
     pub fn to_transfer(&self) -> Vec<u8> {
         self.encode()
     }
 
-    /// Generate types information hash and corresponding id pic
+    /// Generate types information hash and corresponding id pic  
     ///
     /// Types information hash is calculated for `Vec<u8>` of encoded types information,
-    /// as it would be stored in the database
+    /// as it would be stored in the database  
     #[cfg(feature = "signer")]
     pub fn show(&self) -> (String, SignerImage) {
         use sp_core::blake2_256;
