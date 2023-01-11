6d9e307a43bb42b61663fc1c50cf7d3445fc90af
Dmitry Borodin <git@DmitryBorodin.com>
2023-01-10 19:11:41 -0500

    created network models

---
+++ android/src/main/java/io/parity/signer/screens/keyderivation/CreateDerivationViewModel.kt
@@ -0,0 +1,15 @@
+package io.parity.signer.screens.keyderivation
+
+import androidx.lifecycle.ViewModel
+import io.parity.signer.models.NetworkModel
+
+
+class CreateDerivationViewModel : ViewModel() {
+	fun getNetworks(): List<NetworkModel> {
+		return emptyList()
+	}
+}
+
+
+
+
