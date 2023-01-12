2498fe9639f2abaaec0acdaa44b162da913be512
Fedor Sakharov <fedor.sakharov@gmail.com>
2023-01-12 15:03:21 +0300

    chore: replace testing blobs with proper subxt calls

--- rust/parser/src/tests.rs
+++ rust/parser/src/tests.rs
@@ -1,8 +1,18 @@
+use std::str::FromStr;
+
 use crate::{parse_and_display_set, Error};
 use definitions::network_specs::ShortSpecs;
 use frame_metadata::RuntimeMetadata;
 use parity_scale_codec::Decode;
+use parity_scale_codec::Encode;
 use pretty_assertions::assert_eq;
+use sp_core::H256;
+
+use subxt::{
+    config::PolkadotConfig,
+    ext::{sp_core::crypto::Ss58Codec, sp_runtime::AccountId32},
+    tx::{BaseExtrinsicParams, BaseExtrinsicParamsBuilder, Era, ExtrinsicParams, PlainTip},
+};
 
 fn metadata(filename: &str) -> RuntimeMetadata {
     let metadata_hex = std::fs::read_to_string(filename).unwrap();
@@ -24,10 +34,81 @@ fn specs() -> ShortSpecs {
     }
 }
 
+#[allow(clippy::all)]
+#[subxt::subxt(runtime_metadata_path = "for_tests/westend9111.scale")]
+mod westend9111 {}
+
+#[derive(parity_scale_codec::Encode)]
+struct Method {
+    method: Vec<u8>,
+}
+
 #[test]
 fn tr_1() {
-    let data = hex::decode("4d0210020806000046ebddef8cd9bb167dc30878d7113b7e168e6f0646beffd77d69d39bad76b47a07001b2c3ef70006050c0008264834504a64ace1373f0c8ed5d57381ddf54a2f67a318fa42b1352681606d00aebb0211dbb07b4d335a657257b8ac5e53794c901e4f616d4a254f2490c43934009ae581fef1fc06828723715731adcf810e42ce4dadad629b1b7fa5c3c144a81d550008009723000007000000e143f23803ac50e8f6f8e62695d1ce9e4e1d68aa36c1cd2cfd15340213f3423e5b1d91c89d3de85a4d6eee76ecf3a303cf38b59e7d81522eb7cd24b02eb161ff").unwrap();
-    let reply = parse_and_display_set(&data, &metadata("for_tests/westend9111"), &specs()).unwrap();
+    use westend9111::runtime_types::{
+        pallet_staking::{pallet::pallet::Call as StakingCall, RewardDestination},
+        pallet_utility::pallet::Call as UtilityCall,
+        westend_runtime::Call,
+    };
+    let mut calls = Vec::new();
+
+    let staking_call_bond = StakingCall::bond {
+        controller: AccountId32::from_ss58check("5DfhGyQdFobKM8NsWvEeAKk5EQQgYe9AydgJ7rMB6E1EqRzV")
+            .unwrap()
+            .into(),
+        value: 1061900000000,
+        payee: RewardDestination::Staked,
+    };
+    calls.push(Call::Staking(staking_call_bond));
+
+    let staking_call_nominate = StakingCall::nominate {
+        targets: [
+            "5CFPcUJgYgWryPaV1aYjSbTpbTLu42V32Ytw1L9rfoMAsfGh",
+            "5G1ojzh47Yt8KoYhuAjXpHcazvsoCXe3G8LZchKDvumozJJJ",
+            "5FZoQhgUCmqBxnkHX7jCqThScS2xQWiwiF61msg63CFL3Y8f",
+        ]
+        .iter()
+        .map(|addr| AccountId32::from_ss58check(addr).unwrap().into())
+        .collect(),
+    };
+
+    let genesis_hash = H256::from_str(
+        "
+e143f23803ac50e8f6f8e62695d1ce9e4e1d68aa36c1cd2cfd15340213f3423e",
+    )
+    .unwrap();
+    let block_hash = H256::from_str(
+        "
+5b1d91c89d3de85a4d6eee76ecf3a303cf38b59e7d81522eb7cd24b02eb161ff",
+    )
+    .unwrap();
+    let params = BaseExtrinsicParams::<PolkadotConfig, PlainTip>::new(
+        0x2397,
+        7,
+        2,
+        genesis_hash,
+        BaseExtrinsicParamsBuilder::new()
+            .tip(PlainTip::new(0))
+            .era(Era::Mortal(64, 5), block_hash),
+    );
+
+    calls.push(Call::Staking(staking_call_nominate));
+
+    let batch = UtilityCall::batch_all { calls };
+
+    let tx = Call::Utility(batch);
+    let tx = tx.encode();
+
+    let call = Method { method: tx };
+
+    let mut extensions = vec![];
+    params.encode_extra_to(&mut extensions);
+    params.encode_additional_to(&mut extensions);
+    let mut tx = call.encode();
+
+    tx.extend_from_slice(extensions.as_slice());
+
+    let reply = parse_and_display_set(&tx, &metadata("for_tests/westend9111"), &specs()).unwrap();
     let reply_known = r#"
 Method:
 
@@ -62,12 +143,7 @@ tip: 0 pWND,
 network: westend9111,
 tx_version: 7,
 block_hash: 5b1d91c89d3de85a4d6eee76ecf3a303cf38b59e7d81522eb7cd24b02eb161ff"#;
-    assert!(
-        reply == reply_known,
-        "Expected: {}\nReceived: {}",
-        reply_known,
-        reply
-    );
+    assert_eq!(reply, reply_known,);
 }
 
 #[test]
