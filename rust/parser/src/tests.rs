98f5833f4f276b7eae0c555107d60d6d9fc15b44
Fedor Sakharov <fedor.sakharov@gmail.com>
2023-01-12 17:29:21 +0300

    more tests

--- rust/parser/src/tests.rs
+++ rust/parser/src/tests.rs
@@ -38,6 +38,9 @@ fn specs() -> ShortSpecs {
 #[subxt::subxt(runtime_metadata_path = "for_tests/westend9111.scale")]
 mod westend9111 {}
 
+#[subxt::subxt(runtime_metadata_path = "for_tests/westend9122.scale")]
+mod westend9122 {}
+
 #[derive(parity_scale_codec::Encode)]
 struct Method {
     method: Vec<u8>,
@@ -142,21 +145,79 @@ block_hash: 5b1d91c89d3de85a4d6eee76ecf3a303cf38b59e7d81522eb7cd24b02eb161ff"#;
 
 #[test]
 fn tr_2() {
-    let data = hex::decode("4d0210020806000046ebddef8cd9bb167dc30878d7113b7e168e6f0646beffd77d69d39bad76b47a07001b2c3ef70006050c0008264834504a64ace1373f0c8ed5d57381ddf54a2f67a318fa42b1352681606d00aebb0211dbb07b4d335a657257b8ac5e53794c901e4f616d4a254f2490c43934009ae581fef1fc06828723715731adcf810e42ce4dadad629b1b7fa5c3c144a81d550008009723000007000000e143f23803ac50e8f6f8e62695d1ce9e4e1d68aa36c1cd2cfd15340213f3423e5b1d91c89d3de85a4d6eee76ecf3a303cf38b59e7d81522eb7cd24b02eb161ff").unwrap();
+    use westend9111::runtime_types::{
+        pallet_staking::{pallet::pallet::Call as StakingCall, RewardDestination},
+        pallet_utility::pallet::Call as UtilityCall,
+        westend_runtime::Call,
+    };
+    let mut calls = Vec::new();
+
+    let staking_call_bond = StakingCall::bond {
+        controller: AccountId32::from_ss58check("5DfhGyQdFobKM8NsWvEeAKk5EQQgYe9AydgJ7rMB6E1EqRzV")
+            .unwrap()
+            .into(),
+        value: 1061900000000,
+        payee: RewardDestination::Staked,
+    };
+    calls.push(Call::Staking(staking_call_bond));
+
+    let staking_call_nominate = StakingCall::nominate {
+        targets: [
+            "5CFPcUJgYgWryPaV1aYjSbTpbTLu42V32Ytw1L9rfoMAsfGh",
+            "5G1ojzh47Yt8KoYhuAjXpHcazvsoCXe3G8LZchKDvumozJJJ",
+            "5FZoQhgUCmqBxnkHX7jCqThScS2xQWiwiF61msg63CFL3Y8f",
+        ]
+        .iter()
+        .map(|addr| AccountId32::from_ss58check(addr).unwrap().into())
+        .collect(),
+    };
+
+    let genesis_hash =
+        H256::from_str("e143f23803ac50e8f6f8e62695d1ce9e4e1d68aa36c1cd2cfd15340213f3423e").unwrap();
+    let block_hash =
+        H256::from_str("5b1d91c89d3de85a4d6eee76ecf3a303cf38b59e7d81522eb7cd24b02eb161ff").unwrap();
+    let params = BaseExtrinsicParams::<PolkadotConfig, PlainTip>::new(
+        9111,
+        7,
+        2,
+        genesis_hash,
+        BaseExtrinsicParamsBuilder::new()
+            .tip(PlainTip::new(0))
+            .era(Era::Mortal(64, 5), block_hash),
+    );
+
+    calls.push(Call::Staking(staking_call_nominate));
+
+    let batch = UtilityCall::batch_all { calls };
+
+    let tx = Call::Utility(batch);
+    let tx = tx.encode();
+
+    let call = Method { method: tx };
+
+    let mut extensions = vec![];
+    params.encode_extra_to(&mut extensions);
+    params.encode_additional_to(&mut extensions);
+    let mut tx = call.encode();
+
+    tx.extend_from_slice(extensions.as_slice());
+
     let reply =
-        parse_and_display_set(&data, &metadata("for_tests/westend9120"), &specs()).unwrap_err();
+        parse_and_display_set(&tx, &metadata("for_tests/westend9120"), &specs()).unwrap_err();
+
     if let Error::WrongNetworkVersion {
         as_decoded,
         in_metadata,
     } = reply
     {
         assert_eq!(as_decoded, "9111".to_string());
-        assert_eq!(in_metadata, 9120);
+        assert_eq!(in_metadata, 9120)
     } else {
         panic!("Expected Error::WrongNetworkVersion, got {:?}", reply);
     }
 }
 
+// Unable to use subxt, metadata v12.
 #[test]
 fn tr_3() {
     let data = hex::decode("a40403008eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a480700e8764817b501b8003223000005000000e143f23803ac50e8f6f8e62695d1ce9e4e1d68aa36c1cd2cfd15340213f3423e538a7d7a0ac17eb6dd004578cb8e238c384a10f57c999a3fa1200409cd9b3f33").unwrap();
@@ -191,8 +252,43 @@ block_hash: 538a7d7a0ac17eb6dd004578cb8e238c384a10f57c999a3fa1200409cd9b3f33";
 
 #[test]
 fn tr_4() {
-    let data = hex::decode("9c0403008eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a480284d717d5031504025a62029723000007000000e143f23803ac50e8f6f8e62695d1ce9e4e1d68aa36c1cd2cfd15340213f3423e98a8ee9e389043cd8a9954b254d822d34138b9ae97d3b7f50dc6781b13df8d84").unwrap();
-    let reply = parse_and_display_set(&data, &metadata("for_tests/westend9111"), &specs()).unwrap();
+    use westend9111::runtime_types::{
+        pallet_balances::pallet::Call as BalancesCall, westend_runtime::Call,
+    };
+
+    let dest = AccountId32::from_ss58check("5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty")
+        .unwrap()
+        .into();
+    let genesis_hash =
+        H256::from_str("e143f23803ac50e8f6f8e62695d1ce9e4e1d68aa36c1cd2cfd15340213f3423e").unwrap();
+    let block_hash =
+        H256::from_str("98a8ee9e389043cd8a9954b254d822d34138b9ae97d3b7f50dc6781b13df8d84").unwrap();
+    let params = BaseExtrinsicParams::<PolkadotConfig, PlainTip>::new(
+        9111,
+        7,
+        261,
+        genesis_hash,
+        BaseExtrinsicParamsBuilder::new()
+            .tip(PlainTip::new(10_000_000))
+            .era(Era::Mortal(64, 61), block_hash),
+    );
+    let call = BalancesCall::transfer_keep_alive {
+        dest,
+        value: 100_000_000,
+    };
+
+    let call = Call::Balances(call);
+    let call = call.encode();
+    let call = Method { method: call };
+
+    let mut extensions = vec![];
+    params.encode_extra_to(&mut extensions);
+    params.encode_additional_to(&mut extensions);
+    let mut tx = call.encode();
+
+    tx.extend_from_slice(extensions.as_slice());
+
+    let reply = parse_and_display_set(&tx, &metadata("for_tests/westend9111"), &specs()).unwrap();
     let reply_known = "
 Method:
 
@@ -213,18 +309,47 @@ tip: 10.000000 uWND,
 network: westend9111,
 tx_version: 7,
 block_hash: 98a8ee9e389043cd8a9954b254d822d34138b9ae97d3b7f50dc6781b13df8d84";
-    assert!(
-        reply == reply_known,
-        "Expected: {}\nReceived: {}",
-        reply_known,
-        reply
-    );
+    assert_eq!(reply, reply_known);
 }
 
 #[test]
 fn tr_5() {
-    let data = hex::decode("2509000115094c6f72656d20697073756d20646f6c6f722073697420616d65742c20636f6e73656374657475722061646970697363696e6720656c69742c2073656420646f20656975736d6f642074656d706f7220696e6369646964756e74207574206c61626f726520657420646f6c6f7265206d61676e6120616c697175612e20436f6e67756520657520636f6e7365717561742061632066656c697320646f6e65632e20547572706973206567657374617320696e7465676572206567657420616c6971756574206e696268207072616573656e742e204e6571756520636f6e76616c6c6973206120637261732073656d70657220617563746f72206e657175652e204e65747573206574206d616c6573756164612066616d6573206163207475727069732065676573746173207365642074656d7075732e2050656c6c656e746573717565206861626974616e74206d6f726269207472697374697175652073656e6563747573206574206e657475732065742e205072657469756d2076756c7075746174652073617069656e206e656320736167697474697320616c697175616d2e20436f6e76616c6c69732061656e65616e20657420746f72746f7220617420726973757320766976657272612e20566976616d757320617263752066656c697320626962656e64756d207574207472697374697175652065742065676573746173207175697320697073756d2e204d616c6573756164612070726f696e206c696265726f206e756e6320636f6e73657175617420696e74657264756d207661726975732e2045022c00a223000007000000e143f23803ac50e8f6f8e62695d1ce9e4e1d68aa36c1cd2cfd15340213f3423e1b2b0a177ad4f3f93f9a56dae700e938a40201a5beabbda160a74c70e612c66a").unwrap();
-    let reply = parse_and_display_set(&data, &metadata("for_tests/westend9122"), &specs()).unwrap();
+    use westend9122::runtime_types::westend_runtime::Call;
+    use westend9122::runtime_types::frame_system::pallet::Call as SystemCall;
+
+    let remark = 
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Congue eu consequat ac felis donec. Turpis egestas integer eget aliquet nibh praesent. Neque convallis a cras semper auctor neque. Netus et malesuada fames ac turpis egestas sed tempus. Pellentesque habitant morbi tristique senectus et netus et. Pretium vulputate sapien nec sagittis aliquam. Convallis aenean et tortor at risus viverra. Vivamus arcu felis bibendum ut tristique et egestas quis ipsum. Malesuada proin libero nunc consequat interdum varius. ".as_bytes().to_vec();
+
+    let call = SystemCall::remark { remark };
+    let call = Call::System(call);
+    let genesis_hash =
+        H256::from_str("e143f23803ac50e8f6f8e62695d1ce9e4e1d68aa36c1cd2cfd15340213f3423e").unwrap();
+    let block_hash =
+        H256::from_str(
+"1b2b0a177ad4f3f93f9a56dae700e938a40201a5beabbda160a74c70e612c66a"
+    ).unwrap();
+
+    let params = BaseExtrinsicParams::<PolkadotConfig, PlainTip>::new(
+        9122,
+        7,
+        11,
+        genesis_hash,
+        BaseExtrinsicParamsBuilder::new()
+            .tip(PlainTip::new(0))
+            .era(Era::Mortal(64, 36), block_hash),
+    );
+
+    let call = call.encode();
+    let call = Method { method: call };
+
+    let mut extensions = vec![];
+    params.encode_extra_to(&mut extensions);
+    params.encode_additional_to(&mut extensions);
+    let mut tx = call.encode();
+
+    tx.extend_from_slice(extensions.as_slice());
+
+    let reply = parse_and_display_set(&tx, &metadata("for_tests/westend9122"), &specs()).unwrap();
     let reply_known = "
 Method:
 
@@ -242,12 +367,8 @@ tip: 0 pWND,
 network: westend9122,
 tx_version: 7,
 block_hash: 1b2b0a177ad4f3f93f9a56dae700e938a40201a5beabbda160a74c70e612c66a";
-    assert!(
-        reply == reply_known,
-        "Expected: {}\nReceived: {}",
-        reply_known,
-        reply
-    );
+
+    assert_eq!(reply,reply_known);
 }
 
 #[test]
