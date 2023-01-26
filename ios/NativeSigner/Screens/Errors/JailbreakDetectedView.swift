43c6963ac5d570f10b6ee26f3f4dec87e0cecbf9
Krzysztof Rodak <krzysztof@parity.io>
2023-01-26 15:49:14 +0700

    feat: ios: check for jailbreak each time application becomes active (#1561)
    * ios: check for jailbreak each time application becomes active
    
    * feat: ios: clean merge conflict file

---
+++ ios/NativeSigner/Screens/Errors/JailbreakDetectedView.swift
@@ -0,0 +1,37 @@
+//
+//  JailbreakDetectedView.swift
+//  NativeSigner
+//
+//  Created by Krzysztof Rodak on 26/01/2023.
+//
+
+import SwiftUI
+
+struct JailbreakDetectedView: View {
+    var body: some View {
+        VStack(spacing: Spacing.small) {
+            Spacer()
+            Asset.jailbreak.swiftUIImage
+                .padding(.bottom, Spacing.small)
+            Localizable.Error.Jailbreak.Label.title.text
+                .font(PrimaryFont.titleL.font)
+                .foregroundColor(Asset.textAndIconsPrimary.swiftUIColor)
+                .padding(.horizontal, Spacing.x3Large)
+            Localizable.Error.Jailbreak.Label.subtitle.text
+                .font(PrimaryFont.bodyL.font)
+                .foregroundColor(Asset.textAndIconsTertiary.swiftUIColor)
+                .padding(.horizontal, Spacing.extraExtraLarge)
+            Spacer()
+        }
+        .multilineTextAlignment(.center)
+        .background(Asset.backgroundPrimary.swiftUIColor)
+    }
+}
+
+struct JailbreakDetectedView_Previews: PreviewProvider {
+    static var previews: some View {
+        JailbreakDetectedView()
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+    }
+}
