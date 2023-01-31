297233a98447a42737b4047fc198c139a94b7821
Fedor Sakharov <fedor.sakharov@gmail.com>
2023-01-31 09:06:31 +0300

    fix: remove logs for the time being

--- rust/transaction_parsing/src/message.rs
+++ rust/transaction_parsing/src/message.rs
@@ -101,7 +101,6 @@ pub fn process_message(database: &sled::Db, data_hex: &str) -> Result<Transactio
                         base58prefix: network_specs.specs.base58prefix,
                     }
                     .card(&mut index, indent);
-                    println!("here 1");
                     let warning_card =
                         Card::Warning(Warning::AuthorNotFound).card(&mut index, indent);
                     let message_card =
