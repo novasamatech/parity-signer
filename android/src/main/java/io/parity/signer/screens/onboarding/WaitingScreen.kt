0bd2f1aa3049e1d3a5624d3920860068b8c13b75
Dmitry Borodin <git@DmitryBorodin.com>
2023-01-31 19:08:56 -0500

    updated splash screen so it's prettier

--- android/src/main/java/io/parity/signer/screens/onboarding/WaitingScreen.kt
+++ android/src/main/java/io/parity/signer/screens/onboarding/WaitingScreen.kt
@@ -2,12 +2,18 @@ package io.parity.signer.screens.onboarding
 
 import android.content.res.Configuration
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.parity.signer.R
 
 /**
@@ -15,13 +21,20 @@ import io.parity.signer.R
  */
 @Composable
 fun WaitingScreen() {
-	Column {
-		Text("Please wait...")
-		CircularProgressIndicator()
-		Image(
-			painter = painterResource(id = R.drawable.app_logo),
-			contentDescription = "Icon"
-		)
+	Box(
+		modifier = Modifier.fillMaxSize(1f),
+		contentAlignment = Alignment.Center,
+	) {
+		Column(horizontalAlignment = Alignment.CenterHorizontally) {
+			Image(
+				painter = painterResource(id = R.drawable.app_logo),
+				modifier = Modifier.size(48.dp),
+				contentDescription = "Icon"
+			)
+			Text("Please wait...")
+			CircularProgressIndicator()
+
+		}
 	}
 }
 
