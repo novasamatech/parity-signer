b4944d0c217e522720621d73d000bb86f160e54e
Dmitry Borodin <git@DmitryBorodin.com>
2023-02-06 11:07:59 -0500

    fixing issue with blank screen in navigation for some Chinese vendors

--- android/src/main/java/io/parity/signer/ui/SignerMainNavigationGraph.kt
+++ android/src/main/java/io/parity/signer/ui/SignerMainNavigationGraph.kt
@@ -1,14 +1,24 @@
 package io.parity.signer.ui
 
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import io.parity.signer.domain.findActivity
 import io.parity.signer.screens.onboarding.airgap.enableAirgapAppFlow
 import io.parity.signer.screens.onboarding.initialUnlockAppScreenFlow
 import io.parity.signer.screens.onboarding.splash.splashScreen
 import io.parity.signer.screens.onboarding.termsconsent.termsConsentAppFlow
+import kotlinx.coroutines.delay
 
 
 @ExperimentalMaterialApi
@@ -18,12 +28,33 @@ fun SignerMainNavigationGraph(
 	navController: NavHostController,
 	startDestination: String = MainGraphRoutes.splashRoute,
 ) {
-	NavHost(navController = navController, startDestination = startDestination) {
-		splashScreen(navController)
-		termsConsentAppFlow(navController)
-		enableAirgapAppFlow(navController)
-		initialUnlockAppScreenFlow(navController)
-		mainSignerAppFlow(navController)
+	val context = LocalContext.current
+	LaunchedEffect(key1 = Unit, block = {
+		val window = context.findActivity()!!.window
+		/**
+		 * Due to this bug https://issuetracker.google.com/issues/227926002 when using
+		 * splash screen API on some devices with Compose Jetpack Navigation leads to a blank
+		 * screen. However wrapping the [NavHost] in a [Scaffold] or setting background solves this issue,
+		 * so here it is. And setting backgroudn back below
+		 */
+		delay(50)
+		window.setBackgroundDrawableResource(android.R.color.transparent)
+	})
+	Box(
+		modifier = Modifier
+			.fillMaxSize(1f)
+			.background(MaterialTheme.colors.background)
+	) {
+		NavHost(
+			navController = navController,
+			startDestination = startDestination
+		) {
+			splashScreen(navController)
+			termsConsentAppFlow(navController)
+			enableAirgapAppFlow(navController)
+			initialUnlockAppScreenFlow(navController)
+			mainSignerAppFlow(navController)
+		}
 	}
 }
 
