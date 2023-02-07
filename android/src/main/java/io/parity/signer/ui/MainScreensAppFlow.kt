920573743920fc6fbcc3270807486c805bd53732
Dmitry Borodin <git@DmitryBorodin.com>
2023-02-07 07:58:41 -0500

    git merge error fixed.

--- android/src/main/java/io/parity/signer/ui/MainScreensAppFlow.kt
+++ android/src/main/java/io/parity/signer/ui/MainScreensAppFlow.kt
@@ -5,6 +5,7 @@ import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
