1f5bab87e8a34dc2ea8c5a14e8d2c1acd096a03c
Krzysztof Rodak <krzysztof@parity.io>
2023-02-08 14:00:49 +0700

    refactor: ios: further changes to create key set, adding info box and creating view model

---
+++ ios/NativeSigner/Components/Text/AttributedTintInfoBox.swift
@@ -0,0 +1,39 @@
+//
+//  AttributedTintInfoBox.swift
+//  NativeSigner
+//
+//  Created by Krzysztof Rodak on 08/02/2023.
+//
+
+import SwiftUI
+
+struct AttributedTintInfoBox: View {
+    let text: AttributedString
+
+    var body: some View {
+        HStack {
+            Text(text)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Spacer().frame(width: Spacing.large)
+            Asset.helpOutline.swiftUIImage
+                .foregroundColor(Asset.accentPink300.swiftUIColor)
+        }
+        .padding(Spacing.medium)
+        .font(PrimaryFont.bodyM.font)
+        .background(
+            RoundedRectangle(cornerRadius: CornerRadius.medium)
+                .foregroundColor(Asset.accentPink300Fill8.swiftUIColor)
+        )
+    }
+}
+
+#if DEBUG
+    struct AttributedTintInfoBox_Previews: PreviewProvider {
+        static var previews: some View {
+            VStack {
+                AttributedTintInfoBox(text: Localizable.createKeySetSeedPhraseInfo())
+                    .preferredColorScheme(.dark)
+            }
+        }
+    }
+#endif
--- ios/NativeSigner/Extensions/Localizable+Formatted.swift
+++ ios/NativeSigner/Extensions/Localizable+Formatted.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 extension Localizable {
     static func createDerivedKeyModalPathInfo() -> AttributedString {
@@ -21,6 +22,24 @@ extension Localizable {
 
         let range = (mainText as NSString).range(of: highlightedText)
         attributedString.setAttributes([.foregroundColor: Asset.accentPink300.color], range: range)
+        return AttributedString(attributedString)
+    }
+
+    static func createKeySetSeedPhraseInfo() -> AttributedString {
+        let mainText = Localizable.NewSeed.Backup.Label.info.string
+        let underlinedText = Localizable.NewSeed.Backup.Label.Info.underline.string
+
+        let attributedString = NSMutableAttributedString(string: mainText)
+        attributedString.addAttribute(
+            .foregroundColor,
+            value: Asset.accentPink300.color,
+            range: NSRange(location: 0, length: mainText.count)
+        )
+        attributedString.addAttribute(
+            .underlineStyle,
+            value: NSUnderlineStyle.single.rawValue,
+            range: (mainText as NSString).range(of: underlinedText)
+        )
         return AttributedString(attributedString)
     }
 }
