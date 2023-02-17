90317cccd9c3bf6e8362bfcae348a1957ad11e52
Dmitry Borodin <git@DmitryBorodin.com>
2023-02-16 19:36:07 -0500

    tuned logic to update state of existing lock screen state

--- android/src/main/java/io/parity/signer/screens/onboarding/eachstartchecks/EachStartSubgraphScreen.kt
+++ android/src/main/java/io/parity/signer/screens/onboarding/eachstartchecks/EachStartSubgraphScreen.kt
@@ -2,8 +2,15 @@ package io.parity.signer.screens.onboarding.eachstartchecks
 
 import android.content.Context
 import android.util.Log
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
@@ -18,8 +25,11 @@ fun NavGraphBuilder.enableEachStartAppFlow(globalNavController: NavHostControlle
 	composable(route = MainGraphRoutes.enableAirgapRoute) {
 		val viewModel: EachStartViewModel = viewModel()
 		val context: Context = LocalContext.current
+		val isAuthPossible = remember {
+			mutableStateOf(viewModel.checkIsAuthPossible(context))
+		}
 
-		if (viewModel.checkIsAuthPossible(context)) {
+		if (isAuthPossible.value) {
 			// next screen - airgap
 			LaunchedEffect(viewModel) {
 				Log.d(NAVIGATION_TAG, "airgap screen opened")
@@ -32,6 +42,18 @@ fun NavGraphBuilder.enableEachStartAppFlow(globalNavController: NavHostControlle
 			EnableAirgapScreen()
 		} else {
 			//first show enable screen lock if needed
+			val lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current
+			DisposableEffect(this) {
+				val observer = LifecycleEventObserver { _, event ->
+					if (event.targetState == Lifecycle.State.RESUMED) {
+						isAuthPossible.value = viewModel.checkIsAuthPossible(context)
+					}
+				}
+				lifecycleOwner.lifecycle.addObserver(observer)
+				onDispose {
+					lifecycleOwner.lifecycle.removeObserver(observer)
+				}
+			}
 			SetScreenLockScreen()
 		}
 	}
