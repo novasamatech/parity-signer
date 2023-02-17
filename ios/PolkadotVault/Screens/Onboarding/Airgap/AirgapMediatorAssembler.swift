f4f5cb14a4e39854cc4a5defd114e16f4434b509
Krzysztof Rodak <krzysztof@parity.io>
2023-02-17 11:43:52 +0100

    chore: ios: ability to mock connectivity during onboarding

---
+++ ios/PolkadotVault/Screens/Onboarding/Airgap/AirgapMediatorAssembler.swift
@@ -0,0 +1,25 @@
+//
+//  AirgapMediatorAssembler.swift
+//  PolkadotVault
+//
+//  Created by Krzysztof Rodak on 17/02/2023.
+//
+
+import Foundation
+
+/// Assembler that prepares dependency for `AirgapMediating`
+final class AirgapMediatorAssembler {
+    private let runtimePropertiesProvider: RuntimePropertiesProviding
+
+    init(runtimePropertiesProvider: RuntimePropertiesProviding = RuntimePropertiesProvider()) {
+        self.runtimePropertiesProvider = runtimePropertiesProvider
+    }
+
+    func assemble() -> AirgapMediating {
+        if runtimePropertiesProvider.isInDevelopmentMode {
+            return AirgapMediatingStub()
+        } else {
+            return AirgapMediator()
+        }
+    }
+}
