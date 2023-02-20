90061ad0b8842a5a318e4dc9e62c65421bbc0dd4
Dmitry Borodin <git@DmitryBorodin.com>
2023-02-20 15:53:27 -0500

    implemented onboarding docs screen with states

--- android/src/main/java/io/parity/signer/screens/onboarding/termsconsent/DocumentsOnboardingScreen.kt
+++ android/src/main/java/io/parity/signer/screens/onboarding/termsconsent/DocumentsOnboardingScreen.kt
@@ -4,13 +4,15 @@ import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ChevronRight
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
@@ -23,65 +25,11 @@ import io.parity.signer.R
 import io.parity.signer.components.base.CheckboxWithText
 import io.parity.signer.components.base.PrimaryButtonWide
 import io.parity.signer.components.base.SignerDivider
-import io.parity.signer.components.documents.PpText
-import io.parity.signer.components.documents.TacText
 import io.parity.signer.domain.Callback
-import io.parity.signer.ui.theme.*
-
-@Composable
-fun Documents() {
-	//todo dmitry implement for onboarding
-	var document by remember { mutableStateOf(0) }
-	Column {
-		//Note to designer:
-		//to make the selector pretty, implement
-		//custom Tab:
-		//https://developer.android.com/reference/kotlin/androidx/compose/material/package-summary#TabRow(kotlin.Int,androidx.compose.ui.Modifier,androidx.compose.ui.graphics.Color,androidx.compose.ui.graphics.Color,kotlin.Function1,kotlin.Function0,kotlin.Function0)
-		TabRow(
-			selectedTabIndex = document,
-			backgroundColor = MaterialTheme.colors.Bg100,
-			modifier = Modifier.padding(horizontal = 20.dp, vertical = 10.dp)
-		) {
-			Tab(
-				content = {
-					Text(
-						text = stringResource(R.string.documents_terms_of_service),
-						style = Typography.button
-					)
-				},
-				selected = document == 0,
-				onClick = { document = 0 })
-			Tab(
-				content = {
-					Text(
-						text = stringResource(R.string.documents_privacy_policy),
-						style = Typography.button
-					)
-				},
-				selected = document == 1,
-				onClick = { document = 1 })
-		}
-		Column(
-			Modifier
-				.verticalScroll(rememberScrollState())
-				.padding(20.dp)
-		) {
-			when (document) {
-				0 -> {
-//					InstructionsSquare()
-					TacText()
-				}
-				1 -> {
-					PpText()
-				}
-				else -> {
-					Text("document selection error")
-				}
-			}
-			Spacer(Modifier.height(150.dp))
-		}
-	}
-}
+import io.parity.signer.ui.theme.SignerNewTheme
+import io.parity.signer.ui.theme.SignerTypeface
+import io.parity.signer.ui.theme.fill6
+import io.parity.signer.ui.theme.textDisabled
 
 
 @Composable
--- android/src/main/java/io/parity/signer/screens/onboarding/termsconsent/TermsConsentScreen.kt
+++ android/src/main/java/io/parity/signer/screens/onboarding/termsconsent/TermsConsentScreen.kt
@@ -3,29 +3,21 @@ package io.parity.signer.screens.onboarding.termsconsent
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.util.Log
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.selection.toggleable
-import androidx.compose.material.Checkbox
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.captionBarPadding
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
-import io.parity.signer.alerts.AndroidCalledConfirm
-import io.parity.signer.components.BigButton
+import io.parity.signer.components.documents.PpScreen
+import io.parity.signer.components.documents.TacScreen
 import io.parity.signer.ui.MainGraphRoutes
 import io.parity.signer.ui.NAVIGATION_TAG
-import io.parity.signer.ui.theme.Action400
-import io.parity.signer.ui.theme.Bg100
-import io.parity.signer.ui.theme.SignerOldTheme
 
 
 fun NavGraphBuilder.termsConsentAppFlow(globalNavController: NavHostController) {
@@ -45,9 +37,9 @@ fun NavGraphBuilder.termsConsentAppFlow(globalNavController: NavHostController) 
 				}
 			},
 			modifier = Modifier
-				.navigationBarsPadding()
-				.captionBarPadding()
-				.statusBarsPadding()
+                .navigationBarsPadding()
+                .captionBarPadding()
+                .statusBarsPadding()
 		)
 		LaunchedEffect(Unit) {
 			Log.d(NAVIGATION_TAG, "terms screen opened")
@@ -61,77 +53,30 @@ fun NavGraphBuilder.termsConsentAppFlow(globalNavController: NavHostController) 
  */
 @Composable
 private fun TermsConsentScreen(onBoard: () -> Unit, modifier: Modifier) {
-	var confirm by remember { mutableStateOf(false) }
-	var tacAccept by remember { mutableStateOf(false) }
-	var ppAccept by remember { mutableStateOf(false) }
+	var state by remember { mutableStateOf(TermsConsentScreenState.GENERAL_SCREEN) }
 
 	Box(modifier = modifier) {
-		Documents()
-		Column(Modifier.padding(horizontal = 20.dp)) {
-			Spacer(Modifier.weight(1f))
-			Surface(color = MaterialTheme.colors.Bg100) {
-				Column {
-					Spacer(Modifier.height(16.dp))
-					Row(
-						verticalAlignment = Alignment.CenterVertically,
-						modifier = Modifier.toggleable(
-							value = tacAccept,
-							role = Role.Checkbox,
-							onValueChange = { tacAccept = it }
-						)
-					) {
-						Checkbox(
-							checked = tacAccept,
-							onCheckedChange = null
-						)
-						Spacer(Modifier.width(8.dp))
-						Text(
-							"I agree to the terms and conditions",
-							color = MaterialTheme.colors.Action400
-						)
-						Spacer(Modifier.weight(1f))
-					}
-					Spacer(Modifier.height(16.dp))
-					Row(
-						verticalAlignment = Alignment.CenterVertically,
-						modifier = Modifier.toggleable(
-							value = ppAccept,
-							role = Role.Checkbox,
-							onValueChange = { ppAccept = it }
-						)
-					) {
-						Checkbox(
-							checked = ppAccept,
-							onCheckedChange = null
-						)
-						Spacer(Modifier.width(8.dp))
-						Text(
-							"I agree to the privacy policy",
-							color = MaterialTheme.colors.Action400
-						)
-						Spacer(Modifier.weight(1f))
-					}
-					Spacer(Modifier.height(16.dp))
-					BigButton(
-						text = "Next", action = { confirm = true },
-						isDisabled = !(tacAccept && ppAccept)
-					)
-					Spacer(Modifier.height(16.dp))
-				}
-			}
+		when (state) {
+			TermsConsentScreenState.GENERAL_SCREEN ->
+				OnboardingApproveDocumentsScreen(
+					onAgree = onBoard,
+					onTos = { state = TermsConsentScreenState.TERMS_OF_SERVICE },
+					onPp = { state = TermsConsentScreenState.PRIVACY_POLICY },
+				)
+			TermsConsentScreenState.TERMS_OF_SERVICE ->
+				TacScreen(onBack = { state = TermsConsentScreenState.GENERAL_SCREEN })
+			TermsConsentScreenState.PRIVACY_POLICY ->
+				PpScreen(onBack = { state = TermsConsentScreenState.GENERAL_SCREEN })
 		}
 	}
 
-	SignerOldTheme() {
-		AndroidCalledConfirm(
-			show = confirm,
-			header = "Accept terms and conditions and privacy policy?",
-			back = { confirm = false },
-			forward = { onBoard() },
-			backText = "Decline",
-			forwardText = "Accept"
-		)
+	DisposableEffect(key1 = Unit) {
+		onDispose { state = TermsConsentScreenState.GENERAL_SCREEN }
 	}
+}
+
+enum class TermsConsentScreenState {
+	GENERAL_SCREEN, TERMS_OF_SERVICE, PRIVACY_POLICY,
 }
 
 
