bb0995a93b3d29d74424f2a1baaf2b1f59328837
Dmitry Borodin <git@DmitryBorodin.com>
2023-02-20 15:25:49 -0500

    documents general screens implemented

--- android/src/main/java/io/parity/signer/screens/onboarding/termsconsent/DocumentsOnboardingScreen.kt
+++ android/src/main/java/io/parity/signer/screens/onboarding/termsconsent/DocumentsOnboardingScreen.kt
@@ -1,30 +1,32 @@
 package io.parity.signer.screens.onboarding.termsconsent
 
-import SignerCheckbox
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Tab
-import androidx.compose.material.TabRow
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ChevronRight
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.parity.signer.R
 import io.parity.signer.components.base.CheckboxWithText
 import io.parity.signer.components.base.PrimaryButtonWide
+import io.parity.signer.components.base.SignerDivider
 import io.parity.signer.components.documents.PpText
-import io.parity.signer.components.documents.TacScreen
 import io.parity.signer.components.documents.TacText
 import io.parity.signer.domain.Callback
-import io.parity.signer.ui.theme.Bg100
-import io.parity.signer.ui.theme.SignerNewTheme
-import io.parity.signer.ui.theme.SignerTypeface
-import io.parity.signer.ui.theme.Typography
+import io.parity.signer.ui.theme.*
 
 @Composable
 fun Documents() {
@@ -85,30 +87,91 @@ fun Documents() {
 @Composable
 fun OnboardingApproveDocumentsScreen(
 	onAgree: Callback,
-	onToc: Callback,
-	onPp:Callback,
+	onTos: Callback,
+	onPp: Callback,
 ) {
 	val isAccepted = remember { mutableStateOf(false) }
 
 	Column(Modifier.fillMaxSize(1f)) {
+		Spacer(modifier = Modifier.padding(top = 24.dp))
 		Text(
-			text = "Please Agree to our Terms of Service and Privacy Policy",
+			text = stringResource(R.string.documents_approve_title),
 			color = MaterialTheme.colors.primary,
 			style = SignerTypeface.TitleL,
-			modifier = Modifier.padding(horizontal = 24.dp, vertical = 16.dp)
+			textAlign = TextAlign.Center,
+			modifier = Modifier
+				.fillMaxWidth(1f)
+				.padding(horizontal = 24.dp, vertical = 16.dp)
 		)
 
-
-//		todo dmitry element for keyset area
-		Spacer(modifier = Modifier.weight(1f))
-		CheckboxWithText(checked = isAccepted.value,
-		text = stringResource(R.string.documents_accepted_checkmark),
+		Surface(
+			shape = RoundedCornerShape(dimensionResource(id = R.dimen.innerFramesCornerRadius)),
+			color = MaterialTheme.colors.fill6,
+			modifier = Modifier.padding(16.dp)
+		) {
+			Column(
+				horizontalAlignment = Alignment.CenterHorizontally,
+			) {
+				Row(
+					modifier = Modifier.clickable(onClick = onPp),
+					verticalAlignment = Alignment.CenterVertically,
 				) {
-
+					Text(
+						text = stringResource(R.string.documents_privacy_policy),
+						color = MaterialTheme.colors.primary,
+						style = SignerTypeface.TitleS,
+						modifier = Modifier
+							.padding(horizontal = 16.dp, vertical = 14.dp)
+							.weight(1f)
+					)
+					Image(
+						imageVector = Icons.Filled.ChevronRight,
+						contentDescription = null,
+						colorFilter = ColorFilter.tint(MaterialTheme.colors.textDisabled),
+						modifier = Modifier
+							.size(28.dp)
+							.padding(end = 8.dp)
+					)
+				}
+				SignerDivider()
+				Row(
+					modifier = Modifier.clickable(onClick = onTos),
+					verticalAlignment = Alignment.CenterVertically,
+				) {
+					Text(
+						text = stringResource(R.string.documents_terms_of_service),
+						color = MaterialTheme.colors.primary,
+						style = SignerTypeface.TitleS,
+						modifier = Modifier
+							.padding(horizontal = 16.dp, vertical = 14.dp)
+							.weight(1f)
+					)
+					Image(
+						imageVector = Icons.Filled.ChevronRight,
+						contentDescription = null,
+						colorFilter = ColorFilter.tint(MaterialTheme.colors.textDisabled),
+						modifier = Modifier
+							.size(28.dp)
+							.padding(end = 8.dp)
+					)
+				}
+			}
 		}
-		PrimaryButtonWide(label = stringResource(R.string.documents_accept_cta_button)) {
 
+		Spacer(modifier = Modifier.weight(1f))
+		CheckboxWithText(
+			checked = isAccepted.value,
+			text = stringResource(R.string.documents_accepted_checkmark),
+			modifier = Modifier.padding(horizontal = 24.dp, vertical = 8.dp),
+		) {
+			isAccepted.value = it
 		}
+		PrimaryButtonWide(
+			label = stringResource(R.string.documents_accept_cta_button),
+			modifier = Modifier.padding(24.dp),
+			isEnabled = isAccepted.value,
+			onClicked = onAgree,
+		)
 	}
 }
 
@@ -126,6 +189,6 @@ fun OnboardingApproveDocumentsScreen(
 private fun PreviewApproveDocumentsScreen() {
 	SignerNewTheme {
 		//doesn't work in dark mode? Check runtime, it's preview broken for this library
-		OnboardingApproveDocumentsScreen({},{},{})
+		OnboardingApproveDocumentsScreen({}, {}, {})
 	}
 }
