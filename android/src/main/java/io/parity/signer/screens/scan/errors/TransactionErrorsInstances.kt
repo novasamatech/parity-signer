27e27d2c5d313d63ab3e56f9a37dd36f6ffa3843
Dmitry Borodin <git@DmitryBorodin.com>
2023-03-07 17:17:32 -0500

    resource extracted for transaction errors strings

--- android/src/main/java/io/parity/signer/screens/scan/errors/TransactionErrorsInstances.kt
+++ android/src/main/java/io/parity/signer/screens/scan/errors/TransactionErrorsInstances.kt
@@ -3,51 +3,28 @@ package io.parity.signer.screens.scan.errors
 import android.content.res.Configuration
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.*
 import androidx.compose.ui.tooling.preview.Preview
+import io.parity.signer.R
 import io.parity.signer.ui.theme.SignerNewTheme
 import io.parity.signer.ui.theme.pink300
 import io.parity.signer.ui.theme.textTertiary
 
 const val COMPOSE_URL_TAG_ANNOTATION = "URL"
 
-@OptIn(ExperimentalTextApi::class)
 @Composable
 fun TransactionError.toBottomSheetModel(): TransactionErrorModel {
 	return when (this) {
 		is TransactionError.Generic -> TODO()
 		is TransactionError.MetadataAlreadyAdded -> TODO()
 		is TransactionError.MetadataForUnknownNetwork -> {
-			val firstStringElement: AnnotatedString = buildAnnotatedString {
-				append("Go to Portal where your network is stored")
-				append("\n\n")
-				withStyle(SpanStyle(color = MaterialTheme.colors.pink300)) {
-					withAnnotation(COMPOSE_URL_TAG_ANNOTATION, "https://metadata.parity.io") {
-						append("metadata.parity.io")
-					}
-				}
-				append(" for Polkadot, Kusama, and Westend")
-				append("\n\n")
-				withStyle(SpanStyle(color = MaterialTheme.colors.pink300)) {
-					withAnnotation(COMPOSE_URL_TAG_ANNOTATION, "https://metadata.novasama.io") {
-					append("metadata.novasama.io")
-				}
-				}
-				append(" for Parachains and Solochains")
-				append("\n\n")
-				withStyle(SpanStyle(color = MaterialTheme.colors.textTertiary)) {
-					append("ask network's developers directly if you haven’t found it in the portals")
-				}
-			}
 
 			TransactionErrorModel(
 				title = "Please add the Network",//todo dmitry work on it!!!
 				subtitle = "You’re trying to update metadata on a network that hasn’t yet been added.",
-				descriptionSteps = listOf<AnnotatedString>(
-					firstStringElement,
-					AnnotatedString("Choose the network you need"),
-					AnnotatedString("Scan \"Chain spec\" QR to add the missing network")
-				)
+				descriptionSteps = getDescriptionForUpdateMetadata()
 			)
 		}
 		is TransactionError.NetworkAlreadyAdded -> TransactionErrorModel(
@@ -58,6 +35,41 @@ fun TransactionError.toBottomSheetModel(): TransactionErrorModel {
 		is TransactionError.OutdatedMetadata -> TODO()
 		is TransactionError.UnknownNetwork -> TODO()
 	}
+}
+
+@Composable
+@OptIn(ExperimentalTextApi::class)
+private fun getDescriptionForUpdateMetadata(): List<AnnotatedString> {
+	val context = LocalContext.current
+	val firstStringElement = buildAnnotatedString {
+		append(stringResource(R.string.transaction_error_steps_1))
+		append("\n\n")
+		withStyle(SpanStyle(color = MaterialTheme.colors.pink300)) {
+			withAnnotation(COMPOSE_URL_TAG_ANNOTATION, "https://${context.getString(R.string.transaction_error_steps_2_url_core_networks)}") {
+				append(context.getString(R.string.transaction_error_steps_2_url_core_networks))
+			}
+		}
+		append(stringResource(R.string.transaction_error_steps_2_core_networks_description))
+		append("\n\n")
+		withStyle(SpanStyle(color = MaterialTheme.colors.pink300)) {
+			withAnnotation(
+				COMPOSE_URL_TAG_ANNOTATION,
+				"https://${context.getString(R.string.transaction_error_steps_3_url_parachains)}"
+			) {
+				append(context.getString(R.string.transaction_error_steps_3_url_parachains))
+			}
+		}
+		append(stringResource(R.string.transaction_error_steps_3_description_parachains))
+		append("\n\n")
+		withStyle(SpanStyle(color = MaterialTheme.colors.textTertiary)) {
+			append(stringResource(R.string.transaction_error_steps_4_notes_for_other_networks))
+		}
+	}
+	return listOf<AnnotatedString>(
+		firstStringElement,
+		AnnotatedString(stringResource(R.string.transaction_error_steps_choose_network)),
+		AnnotatedString(stringResource(R.string.transaction_error_steps_scan_qr_code))
+	)
 }
 
 @Preview(
