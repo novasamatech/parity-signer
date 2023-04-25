e960b3a6f343e8fc2c3fee21d15a4ae255c1ff1f
Dmitry Borodin <git@DmitryBorodin.com>
2023-04-25 15:59:46 +0200

    netwoprk help work in progress

--- android/src/main/java/io/parity/signer/screens/settings/networks/helper/HowAddNetworks.kt
+++ android/src/main/java/io/parity/signer/screens/settings/networks/helper/HowAddNetworks.kt
@@ -1,21 +1,24 @@
 package io.parity.signer.screens.settings.networks.helper
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import io.parity.signer.R
 import io.parity.signer.components.base.ScreenHeaderWithButton
 import io.parity.signer.domain.Callback
-import io.parity.signer.ui.theme.SignerNewTheme
-import io.parity.signer.ui.theme.SignerTypeface
-import io.parity.signer.ui.theme.pink300
-import io.parity.signer.ui.theme.textSecondary
+import io.parity.signer.ui.theme.*
 
 
 //todo text export
@@ -32,20 +35,53 @@ fun HowAddNetworks(
 			onClose = onClose,
 			onDone = onNext,
 		)
-		Text(
-			text = "Step 1/2",
-			style = SignerTypeface.CaptionM,
-			color = MaterialTheme.colors.textSecondary,
-			modifier = Modifier.padding(top = 16.dp)
-		)
-		Text(
-			text = "How to Add Networks",
-			style = SignerTypeface.TitleL,
-			color = MaterialTheme.colors.primary,
-			modifier = Modifier.padding(bottom = 16.dp)
-		)
-		Row() {
+		Column(
+			modifier = Modifier
+				.verticalScroll(rememberScrollState())
+				.padding(horizontal = 16.dp)
+		) {
+			Text(
+				text = "Step 1/2",
+				style = SignerTypeface.CaptionM,
+				color = MaterialTheme.colors.textSecondary,
+				modifier = Modifier.padding(top = 16.dp)
+			)
+			Text(
+				text = "How to Add Networks",
+				style = SignerTypeface.TitleL,
+				color = MaterialTheme.colors.primary,
+				modifier = Modifier.padding(bottom = 16.dp)
+			)
+			Row(
+				modifier = Modifier
+					.fillMaxWidth(1f)
+					.background(
+						MaterialTheme.colors.fill12,
+						RoundedCornerShape(dimensionResource(id = R.dimen.qrShapeCornerRadius))
+					)
+					.padding(16.dp)
+			) {
 
+				Box(
+					modifier = Modifier
+						.background(
+							MaterialTheme.colors.backgroundSystem,
+							CircleShape,
+						)
+						.defaultMinSize(32.dp, 32.dp),
+					contentAlignment = Alignment.Center,
+				) {
+					Text(
+						text = "1",
+						style = SignerTypeface.BodyL,
+						color = MaterialTheme.colors.primary,
+						modifier = Modifier.padding(end = 12.dp)
+					)
+				}
+				Column() {
+
+				}
+			}
 		}
 	}
 }
