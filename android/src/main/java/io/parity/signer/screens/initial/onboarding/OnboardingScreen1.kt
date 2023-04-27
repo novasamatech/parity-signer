51db36234f0c6b59bd4310ee0ad17dc9a2623bc0
Dmitry Borodin <git@DmitryBorodin.com>
2023-04-27 17:52:35 +0200

    onboarding implementation in progress 2

--- android/src/main/java/io/parity/signer/screens/initial/onboarding/OnboardingScreen1.kt
+++ android/src/main/java/io/parity/signer/screens/initial/onboarding/OnboardingScreen1.kt
@@ -4,9 +4,11 @@ import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
@@ -16,30 +18,55 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.parity.signer.R
 import io.parity.signer.components.base.PageIndicatorLine
-import io.parity.signer.ui.theme.SignerNewTheme
-import io.parity.signer.ui.theme.SignerTypeface
-import io.parity.signer.ui.theme.backgroundSecondary
-import io.parity.signer.ui.theme.pink500
+import io.parity.signer.ui.theme.*
 
 
 @Composable
 internal fun OnboardingScreen1() {
 	Column(
-		Modifier.fillMaxSize(1f).background(
-			brush = Brush.linearGradient(
-				start = Offset(Float.POSITIVE_INFINITY,0.0f),
-				end = Offset(0.0f, Float.POSITIVE_INFINITY),
-				colors = listOf(MaterialTheme.colors.pink500, Color(0xFF1E1E23)),
+		Modifier
+			.fillMaxSize(1f)
+			.background(
+				brush = Brush.linearGradient(
+					start = Offset(Float.POSITIVE_INFINITY, 0.0f),
+					end = Offset(0.0f, Float.POSITIVE_INFINITY),
+					colors = listOf(
+						MaterialTheme.colors.pink500,
+						Color(0xFF1E1E23)
+					),
+				)
 			)
-		)
 	) {
 		PageIndicatorLine(
 			totalDots = 4,
 			selectedIndex = 1,
 			modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
 		)
-		Row(modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp)) {
-
+		Row(
+			modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp),
+			verticalAlignment = Alignment.CenterVertically,
+		) {
+			Box(
+				modifier = Modifier
+					.background(
+						MaterialTheme.colors.fill12, RoundedCornerShape(40.dp)
+					)
+					.padding(vertical = 6.dp, horizontal = 16.dp),
+			) {
+				Text(
+					"Welcome to Polkadot Vault",
+					color = MaterialTheme.colors.textSecondary,
+					style = SignerTypeface.LabelS,
+				)
+			}
+			Spacer(modifier = Modifier.weight(1f))
+			Text(
+				"Skip",
+				color = MaterialTheme.colors.primary,
+				style = SignerTypeface.LabelS,
+				modifier = Modifier
+					.padding(horizontal = 6.dp)
+			)
 		}
 		Text(
 			text = "Turn Your Smartphone Into a Hardware Wallet",
@@ -47,15 +74,23 @@ internal fun OnboardingScreen1() {
 			style = SignerTypeface.TitleS,
 			modifier = Modifier.padding(horizontal = 24.dp, vertical = 8.dp)
 		)
-		Spacer(modifier = Modifier.padding(top = 30.dp).weight(0.2f))
+		Spacer(
+			modifier = Modifier
+				.padding(top = 30.dp)
+				.weight(0.2f)
+		)
 		Image(
 			painter = painterResource(id = R.drawable.onboarding_2),
 			contentDescription = null,
-			modifier = Modifier.padding(horizontal = 24.dp).fillMaxWidth(1f).weight(0.7f)
+			modifier = Modifier
+				.padding(horizontal = 24.dp)
+				.fillMaxWidth(1f)
+				.weight(0.7f)
 		)
 		Spacer(modifier = Modifier.padding(top = 16.dp))
 	}
 }
+
 
 @Preview(
 	name = "light", group = "themes", uiMode = Configuration.UI_MODE_NIGHT_NO,
