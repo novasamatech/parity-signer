0a968c5dacd3d12fcbf422b3207e9b3951cc21a9
Dmitry Borodin <git@DmitryBorodin.com>
2023-04-27 17:44:35 +0200

    onboarding implementation in progress

--- android/src/main/java/io/parity/signer/screens/initial/onboarding/OnboardingScreen1.kt
+++ android/src/main/java/io/parity/signer/screens/initial/onboarding/OnboardingScreen1.kt
@@ -2,38 +2,56 @@ package io.parity.signer.screens.initial.onboarding
 
 import android.content.res.Configuration
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.parity.signer.R
+import io.parity.signer.components.base.PageIndicatorLine
 import io.parity.signer.ui.theme.SignerNewTheme
 import io.parity.signer.ui.theme.SignerTypeface
+import io.parity.signer.ui.theme.backgroundSecondary
+import io.parity.signer.ui.theme.pink500
 
 
 @Composable
 internal fun OnboardingScreen1() {
-	Column() {
-		Row() {
+	Column(
+		Modifier.fillMaxSize(1f).background(
+			brush = Brush.linearGradient(
+				start = Offset(Float.POSITIVE_INFINITY,0.0f),
+				end = Offset(0.0f, Float.POSITIVE_INFINITY),
+				colors = listOf(MaterialTheme.colors.pink500, Color(0xFF1E1E23)),
+			)
+		)
+	) {
+		PageIndicatorLine(
+			totalDots = 4,
+			selectedIndex = 1,
+			modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+		)
+		Row(modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp)) {
 
 		}
 		Text(
 			text = "Turn Your Smartphone Into a Hardware Wallet",
 			color = MaterialTheme.colors.primary,
 			style = SignerTypeface.TitleS,
-			textAlign = TextAlign.Center,
+			modifier = Modifier.padding(horizontal = 24.dp, vertical = 8.dp)
 		)
-		Spacer(modifier = Modifier.padding(top = 30.dp))
+		Spacer(modifier = Modifier.padding(top = 30.dp).weight(0.2f))
 		Image(
 			painter = painterResource(id = R.drawable.onboarding_2),
 			contentDescription = null,
-			modifier = Modifier
+			modifier = Modifier.padding(horizontal = 24.dp).fillMaxWidth(1f).weight(0.7f)
 		)
 		Spacer(modifier = Modifier.padding(top = 16.dp))
 	}
@@ -48,8 +66,28 @@ internal fun OnboardingScreen1() {
 	showBackground = true, backgroundColor = 0xFF000000,
 )
 @Composable
-private fun PreviewOnboarding1() {
+private fun PreviewOnboarding1Small() {
 	SignerNewTheme {
-		OnboardingScreen1()
+		Box(modifier = Modifier.size(320.dp, 568.dp)) {
+			OnboardingScreen1()
+		}
 	}
 }
+
+@Preview(
+	name = "light", group = "themes", uiMode = Configuration.UI_MODE_NIGHT_NO,
+	showBackground = true, backgroundColor = 0xFFFFFFFF,
+)
+@Preview(
+	name = "dark", group = "themes", uiMode = Configuration.UI_MODE_NIGHT_YES,
+	showBackground = true, backgroundColor = 0xFF000000,
+)
+@Composable
+private fun PreviewOnboarding1Big() {
+	SignerNewTheme {
+		Box(modifier = Modifier.fillMaxSize(1f)) {
+			OnboardingScreen1()
+		}
+	}
+}
+
