c5a2a81b5a9018c27e49512395f5570b4258091c
Dmitry Borodin <git@DmitryBorodin.com>
2023-07-03 22:15:36 +0200

    network filters preferences added 2

--- android/src/main/java/io/parity/signer/domain/storage/PreferencesRepository.kt
+++ android/src/main/java/io/parity/signer/domain/storage/PreferencesRepository.kt
@@ -8,21 +8,21 @@ import androidx.datastore.preferences.core.stringSetPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.map
 
-val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "app_preferences")
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "app_preferences")
 
 class PreferencesRepository(private val context: Context) {
 
-	private val NETWORK_FILTER = stringSetPreferencesKey("network_filter")
+	private val networksFilterKey = stringSetPreferencesKey("network_filter")
 
 	val networksFilter = context.dataStore.data
 		.map { preferences ->
 			// No type safety.
-			preferences[NETWORK_FILTER] ?: emptySet()
+			preferences[networksFilterKey] ?: emptySet()
 		}
 
-	suspend fun setNetworkFilter(newFilters: Set<String>) {
+	suspend fun setNetworksFilter(newFilters: Set<String>) {
 		context.dataStore.edit { settings ->
-			settings[NETWORK_FILTER] = newFilters
+			settings[networksFilterKey] = newFilters
 		}
 	}
 }
