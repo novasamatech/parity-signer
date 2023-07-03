17d78b86e7d614d2fe1800e2a509856c78514f46
Dmitry Borodin <git@DmitryBorodin.com>
2023-07-03 20:03:40 +0200

    netfilter workin in progress

--- android/src/main/java/io/parity/signer/screens/keysetdetails/filtermenu/NetworkFilterMenu.kt
+++ android/src/main/java/io/parity/signer/screens/keysetdetails/filtermenu/NetworkFilterMenu.kt
@@ -3,14 +3,13 @@ package io.parity.signer.screens.keysetdetails.filtermenu
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import io.parity.signer.R
 import io.parity.signer.components.base.BottomSheetHeader
 import io.parity.signer.components.base.RowButtonsBottomSheet
@@ -18,37 +17,73 @@ import io.parity.signer.components.base.SignerDivider
 import io.parity.signer.components.items.NetworkItemMultiselect
 import io.parity.signer.domain.Callback
 import io.parity.signer.domain.NetworkModel
-import io.parity.signer.domain.NetworkState
 import io.parity.signer.ui.theme.SignerNewTheme
+
+
+@Composable
+private fun NetworkFilterMenu(
+	networks: List<NetworkModel>,
+	onConfirm: (List<NetworkModel>) -> Unit,
+) {
+
+	//todo dmitry viewmodel and data source for shared preferences
+
+	val selected: MutableState<Set<NetworkModel>> =
+		remember {
+			mutableStateOf(
+				networks.subList(0, 1).toSet()//todo dmitry get from viewmodel
+			)
+		}
+
+	NetworkFilterMenu(
+		networks = networks,
+		selectedNetwors = selected.value,
+		onClick = { network ->
+			if (selected.value.contains(network)) {
+				selected.value - network.key
+			} else {
+				selected.value + network.key
+			}
+		},
+		onConfirm = { onConfirm(selected.value.toList()) },
+		onCancel = { onConfirm(emptyList()) },
+	)
+}
 
 
 //todo dmitry work in progress
 @Composable
-fun NetworkFilterMenu(
+private fun NetworkFilterMenu(
 	networks: List<NetworkModel>,
-	onConfirm: (List<NetworkModel>) -> Unit,
+	selectedNetwors: Set<NetworkModel>,
+	onClick: (NetworkModel) -> Unit,
+	onConfirm: Callback,
 	onCancel: Callback,
 ) {
 	Column(
 		modifier = Modifier
 			.fillMaxWidth(),
 	) {
-		BottomSheetHeader(title = "Filter keys by network", //todo dmitry export
-			onCloseClicked = onCancel)
+		BottomSheetHeader(
+			title = stringResource(R.string.network_filters_header),
+			onCloseClicked = onCancel
+		)
 		SignerDivider()
-		networks.forEach {network ->
-			NetworkItemMultiselect(network = network, //todo dmitry pass properly selected as this item handled elseware
-				isSelected = false, onClick = {})
+		networks.forEach { network ->
+			NetworkItemMultiselect(
+				network = network,
+				isSelected = selectedNetwors.contains(network),
+				onClick = onClick
+			)
 		}
 		RowButtonsBottomSheet(
 			labelCancel = stringResource(R.string.generic_clear_selection),
 			labelCta = stringResource(id = R.string.generic_done),
-			onClickedCancel = { onConfirm(emptyList()) },
-			onClickedCta = { /*TODO*/ })
+			onClickedCancel = onCancel,
+			onClickedCta = onConfirm,
+		)
 	}
 }
-
-
 
 
 @Preview(
@@ -85,8 +120,10 @@ private fun PreviewNetworkFilterMenu() {
 	SignerNewTheme {
 		NetworkFilterMenu(
 			networks = networks,
-				onConfirm = {},
-				onCancel = {},
+			selectedNetwors = networks.subList(1, 1).toSet(),
+			onClick = {},
+			onConfirm = {},
+			onCancel = {},
 		)
 	}
 }
