e56defec0402d405b22d1416a3528fe9a8d4c6ea
Krzysztof Rodak <krzysztof@parity.io>
2023-07-19 21:55:24 +0200

    ios: extend eth blockies preview test data

--- ios/PolkadotVault/Components/BlockiesIdenticonViewPreviews.swift
+++ ios/PolkadotVault/Components/BlockiesIdenticonViewPreviews.swift
@@ -17,12 +17,29 @@ struct BlockiesIdenticonView_Previews: PreviewProvider {
                 height: 96
             )
             BlockiesIdenticonView(
-                configuration: .init(seed: "0x999999cf1046e68e36E1aA2E0E07105eDDD1f08E"),
+                configuration: .init(
+                    seed: "0x999999cf1046e68e36E1aA2E0E07105eDDD1f08E",
+                    size: 5,
+                    scale: 8
+                ),
                 width: 48,
                 height: 48
             )
             BlockiesIdenticonView(
-                configuration: .init(seed: "0xD2AAD5732c980AaddDe38CEAD950dBa91Cd2C726"),
+                configuration: .init(
+                    seed: "0xD2AAD5732c980AaddDe38CEAD950dBa91Cd2C726",
+                    size: 10,
+                    scale: 8
+                ),
+                width: 128,
+                height: 128
+            )
+            BlockiesIdenticonView(
+                configuration: .init(
+                    seed: "0x1524d026FCAa9F1ceeE3540dEeeE3359BAD6bfF9",
+                    size: 10,
+                    scale: 8
+                ),
                 width: 32,
                 height: 32
             )
