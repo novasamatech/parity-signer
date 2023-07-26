b29cef9e88d38a2e4094c0bde5dab6567dcfa48c
Krzysztof Rodak <krzysztof@parity.io>
2023-07-26 11:26:37 +0200

    feat: ios: new function to create keys based on DdDetail

--- ios/PolkadotVault/Backend/CreateDerivedKeyService.swift
+++ ios/PolkadotVault/Backend/CreateDerivedKeyService.swift
@@ -45,6 +45,25 @@ enum CreateDerivedKeyForKeySetsError: Error {
     }
 }
 
+enum ImportDerivedKeyError: Error {
+    case noKeysImported(errors: [String])
+    case keyNotImported([(key: String, error: String)])
+
+    var localizedDescription: String {
+        switch self {
+        case let .keyNotImported(errorInfo):
+            return errorInfo.reduce(into: "") {
+                $0 += (
+                    Localizable.AddDerivedKeys.Error
+                        .derivedKeyForNetwork($1.key, $1.error) + "\n"
+                )
+            }
+        case let .noKeysImported(errors):
+            return errors.reduce(into: "") { $0 += ($1 + "\n") }
+        }
+    }
+}
+
 final class CreateDerivedKeyService {
     private let databaseMediator: DatabaseMediating
     private let callQueue: Dispatching
@@ -163,6 +182,42 @@ final class CreateDerivedKeyService {
                 result = .success(())
             } else {
                 result = .failure(.keysNotCreated(occuredErrors))
+            }
+            self.callbackQueue.async {
+                completion(result)
+            }
+        }
+    }
+
+    func createDerivedKeys(
+        _ seedName: String,
+        _ seedPhrase: String,
+        keysToImport: [DdDetail],
+        completion: @escaping (Result<Void, ImportDerivedKeyError>) -> Void
+    ) {
+        var occuredErrors: [(key: String, error: String)] = []
+        callQueue.async {
+            let result: Result<Void, ImportDerivedKeyError>
+            keysToImport.forEach {
+                do {
+                    try tryCreateAddress(
+                        seedName: seedName,
+                        seedPhrase: seedPhrase,
+                        path: $0.path,
+                        network: $0.networkSpecsKey
+                    )
+                } catch let displayedError as ErrorDisplayed {
+                    occuredErrors.append((key: $0.path, error: displayedError.localizedDescription))
+                } catch {
+                    occuredErrors.append((key: $0.path, error: error.localizedDescription))
+                }
+            }
+            if occuredErrors.isEmpty {
+                result = .success(())
+            } else if occuredErrors.count == keysToImport.count {
+                result = .failure(.noKeysImported(errors: occuredErrors.map(\.error)))
+            } else {
+                result = .failure(.keyNotImported(occuredErrors))
             }
             self.callbackQueue.async {
                 completion(result)
