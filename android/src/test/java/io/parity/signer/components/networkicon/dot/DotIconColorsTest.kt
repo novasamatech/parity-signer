e388a4aece22a36ee74c878a9adbba2248518868
Dmitry Borodin <git@DmitryBorodin.com>
2023-08-04 21:28:24 +0200

    test added for dots - wrong colors calculated at the moment

---
+++ android/src/test/java/io/parity/signer/components/networkicon/dot/DotIconColorsTest.kt
@@ -0,0 +1,144 @@
+package io.parity.signer.components.networkicon.dot
+
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+
+class DotIconColorsTest {
+
+	@Test
+	fun checkColors() {
+		val seed = DotIconConstants.previewAlice
+		val colors = DotIconColors.getColors(seed)
+		assert(colors[2].red == 214.toUByte())
+	}
+
+	@Test
+	fun checkColorsFull() {
+		val seed = DotIconConstants.previewAlice
+		val colors = DotIconColors.getColors(seed)
+		Assert.assertEquals(aliceColor, colors)
+	}
+
+	//taken from rust samples
+	private val aliceColor
+		get() = listOf(
+			DotIconColors.DotIconColorRgb(
+				red = 165u,
+				green = 227u,
+				blue = 156u,
+				alpha = 255u
+			),
+			DotIconColors.DotIconColorRgb(
+				red = 60u,
+				green = 40u,
+				blue = 17u,
+				alpha = 255u
+			),
+			DotIconColors.DotIconColorRgb(
+				red = 184u,
+				green = 68u,
+				blue = 202u,
+				alpha = 255u
+			),
+			DotIconColors.DotIconColorRgb(
+				red = 139u,
+				green = 39u,
+				blue = 88u,
+				alpha = 255u
+			),
+			DotIconColors.DotIconColorRgb(
+				red = 135u,
+				green = 68u,
+				blue = 202u,
+				alpha = 255u
+			),
+			DotIconColors.DotIconColorRgb(
+				red = 225u,
+				green = 156u,
+				blue = 227u,
+				alpha = 255u
+			),
+			DotIconColors.DotIconColorRgb(
+				red = 139u,
+				green = 39u,
+				blue = 88u,
+				alpha = 255u
+			),
+			DotIconColors.DotIconColorRgb(
+				red = 135u,
+				green = 68u,
+				blue = 202u,
+				alpha = 255u
+			),
+			DotIconColors.DotIconColorRgb(
+				red = 184u,
+				green = 68u,
+				blue = 202u,
+				alpha = 255u
+			),
+			DotIconColors.DotIconColorRgb(
+				red = 165u,
+				green = 227u,
+				blue = 156u,
+				alpha = 255u
+			),
+			DotIconColors.DotIconColorRgb(
+				red = 60u,
+				green = 40u,
+				blue = 17u,
+				alpha = 255u
+			),
+			DotIconColors.DotIconColorRgb(
+				red = 162u,
+				green = 202u,
+				blue = 68u,
+				alpha = 255u
+			),
+			DotIconColors.DotIconColorRgb(
+				red = 39u,
+				green = 139u,
+				blue = 139u,
+				alpha = 255u
+			),
+			DotIconColors.DotIconColorRgb(
+				red = 187u,
+				green = 202u,
+				blue = 68u,
+				alpha = 255u
+			),
+			DotIconColors.DotIconColorRgb(
+				red = 38u,
+				green = 60u,
+				blue = 17u,
+				alpha = 255u
+			),
+			DotIconColors.DotIconColorRgb(
+				red = 39u,
+				green = 139u,
+				blue = 139u,
+				alpha = 255u
+			),
+			DotIconColors.DotIconColorRgb(
+				red = 187u,
+				green = 202u,
+				blue = 68u,
+				alpha = 255u
+			),
+			DotIconColors.DotIconColorRgb(
+				red = 162u,
+				green = 202u,
+				blue = 68u,
+				alpha = 255u
+			),
+			DotIconColors.DotIconColorRgb(
+				red = 61u,
+				green = 39u,
+				blue = 139u,
+				alpha = 255u
+			),
+		)
+}
+
+
