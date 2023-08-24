d8db91f9fc538894d54b442e11638b389fbb4f94
Krzysztof Rodak <krzysztof@parity.io>
2023-08-24 12:35:48 +0200

    feat: ios: Backend Service abstraction using generics and operation queues

---
+++ ios/PolkadotVault/Backend/BaseService/BackendService.swift
@@ -0,0 +1,59 @@
+//
+//  BackendService.swift
+//  PolkadotVault
+//
+//  Created by Krzysztof Rodak on 24/08/2023.
+//
+
+import Foundation
+
+/// A class defining the behavior of a backend service.
+final class BackendService {
+    /// The dispatch queue for performing the service call.
+    let callQueue: Dispatching
+    /// The dispatch queue for handling the completion callback.
+    let callbackQueue: Dispatching
+
+    /// Initializes an instance of `BackendService`.
+    ///
+    /// - Parameters:
+    ///   - callQueue: The dispatch queue for performing the service call.
+    ///   - callbackQueue: The dispatch queue for handling the completion callback.
+    init(
+        callQueue: Dispatching = DispatchQueue.global(qos: .userInteractive),
+        callbackQueue: Dispatching = DispatchQueue.main
+    ) {
+        self.callQueue = callQueue
+        self.callbackQueue = callbackQueue
+    }
+
+    /// Performs a backend service call.
+    ///
+    /// This method bridges communication between Swift and Rust backend codebases
+    /// by using the provided closure to execute Rust-specific logic.
+    ///
+    /// - Parameters:
+    ///   - call: A closure encapsulating the logic of the Rust backend call.
+    ///   - completion: A closure to be called when the service call completes.
+    func performCall<T, Success>(
+        _ call: @escaping () throws -> T,
+        completion: @escaping (Result<Success, ServiceError>) -> Void
+    ) {
+        callQueue.async {
+            var result: Result<Success, ServiceError>
+            do {
+                let successValue = try call()
+                if let mappedSuccess = successValue as? Success {
+                    result = .success(mappedSuccess)
+                } else {
+                    result = .failure(.init(message: Localizable.ErrorDisplayed.invalidType.string))
+                }
+            } catch {
+                result = .failure(.init(message: error.backendDisplayError))
+            }
+            self.callbackQueue.async {
+                completion(result)
+            }
+        }
+    }
+}
