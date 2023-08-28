2339390823fdb11e75dd6fc3f887e52276c22569
Krzysztof Rodak <krzysztof@parity.io>
2023-08-28 11:11:07 +0200

    ios: clean up remaining existing services

--- ios/PolkadotVault/Backend/NavigationServices/PublicKeyDetailsService.swift
+++ ios/PolkadotVault/Backend/NavigationServices/PublicKeyDetailsService.swift
@@ -8,14 +8,11 @@
 import Foundation
 
 final class PublicKeyDetailsService {
-    private let navigation: NavigationCoordinator
     private let backendService: BackendService
 
     init(
-        navigation: NavigationCoordinator = NavigationCoordinator(),
         backendService: BackendService = BackendService()
     ) {
-        self.navigation = navigation
         self.backendService = backendService
     }
 
@@ -27,12 +24,5 @@ final class PublicKeyDetailsService {
         backendService.performCall({
             try removeDerivedKey(address: address, networkSpecsKey: networkSpecsKey)
         }, completion: completion)
-    }
-
-    func resetNavigationState(_ keyName: String, _ publicKeyDetails: String) {
-        navigation.performFake(navigation: .init(action: .start))
-        navigation.performFake(navigation: .init(action: .navbarKeys))
-        navigation.performFake(navigation: .init(action: .selectSeed, details: keyName))
-        navigation.performFake(navigation: .init(action: .selectKey, details: publicKeyDetails))
     }
 }
---
+++ ios/PolkadotVault/Backend/Services/ImportDerivedKeysService.swift
@@ -0,0 +1,48 @@
+//
+//  ImportDerivedKeysService.swift
+//  Polkadot Vault
+//
+//  Created by Krzysztof Rodak on 02/01/2023.
+//
+
+import Foundation
+
+final class ImportDerivedKeysService {
+    private let seedsMediator: SeedsMediating
+    private let backendService: BackendService
+
+    init(
+        seedsMediator: SeedsMediating = ServiceLocator.seedsMediator,
+        backendService: BackendService = BackendService()
+    ) {
+        self.seedsMediator = seedsMediator
+        self.backendService = backendService
+    }
+
+    func importDerivedKeys(
+        _ seedPreviews: [SeedKeysPreview],
+        _ completion: @escaping (Result<Void, ServiceError>) -> Void
+    ) {
+        var seedPreviewsToImport = seedPreviews
+        for (index, seedPreview) in seedPreviewsToImport.enumerated() {
+            seedPreviewsToImport[index].derivedKeys = seedPreview.derivedKeys
+                .filter { $0.status == .importable }
+        }
+        backendService.performCall({
+            try importDerivations(seedDerivedKeys: seedPreviewsToImport)
+        }, completion: completion)
+    }
+
+    func updateWithSeeds(
+        _ seedPreviews: [SeedKeysPreview],
+        completion: @escaping (Result<[SeedKeysPreview], ServiceError>) -> Void
+    ) {
+        let seeds = seedsMediator.getAllSeeds()
+        backendService.performCall({
+            try populateDerivationsHasPwd(
+                seeds: seeds,
+                seedDerivedKeys: seedPreviews
+            )
+        }, completion: completion)
+    }
+}
