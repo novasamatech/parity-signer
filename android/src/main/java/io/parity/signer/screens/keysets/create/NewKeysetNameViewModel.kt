160a50eb636a64b38577bde56b9cceb83e7fc00d
Dmitry Borodin <git@DmitryBorodin.com>
2023-08-28 11:01:51 +0200

    work in progress integrating keyset details 2

--- android/src/main/java/io/parity/signer/screens/keysets/create/NewKeysetNameViewModel.kt
+++ android/src/main/java/io/parity/signer/screens/keysets/create/NewKeysetNameViewModel.kt
@@ -4,7 +4,14 @@ import androidx.lifecycle.ViewModel
 import io.parity.signer.dependencygraph.ServiceLocator
 
 
-class NewKeysetNameViewModel: ViewModel() {
+class NewKeysetNameViewModel : ViewModel() {
+
+	private val uniffi = ServiceLocator.uniffiInteractor
+
 	val seedNames =
 		ServiceLocator.seedStorage.lastKnownSeedNames
+
+	fun getSeedPhrase(): String {
+		return "ababab"//todo dmitry get it from uniffi
+	}
 }
