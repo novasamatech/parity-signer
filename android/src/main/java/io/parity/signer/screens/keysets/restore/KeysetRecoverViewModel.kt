cb5d58ceed6d4e8fa49f319e3556229ac51c298a
Dmitry Borodin <git@DmitryBorodin.com>
2023-09-12 15:54:34 +0200

    recover state managing - using update api

--- android/src/main/java/io/parity/signer/screens/keysets/restore/KeysetRecoverViewModel.kt
+++ android/src/main/java/io/parity/signer/screens/keysets/restore/KeysetRecoverViewModel.kt
@@ -6,6 +6,7 @@ import io.parity.signer.domain.backend.RecoverSeedInteractor
 import io.parity.signer.domain.backend.mapError
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.runBlocking
 
 class KeysetRecoverViewModel : ViewModel() {
@@ -36,15 +37,19 @@ class KeysetRecoverViewModel : ViewModel() {
 		val currentModel = _recoverSeed.value
 		if (currentModel.enteredWords.size <= KeysetRecoverModel.WORDS_CAP) {
 			if (rawUserInput.isEmpty()) {
-				_recoverSeed.value = currentModel.copy(
-					rawUserInput = KeysetRecoverModel.SPACE_CHARACTER.toString(),
-					enteredWords = currentModel.enteredWords.dropLast(1)
-				)
+				_recoverSeed.update {
+					it.copy(
+						rawUserInput = KeysetRecoverModel.SPACE_CHARACTER.toString(),
+						enteredWords = currentModel.enteredWords.dropLast(1)
+					)
+				}
 			} else if (rawUserInput.first() != KeysetRecoverModel.SPACE_CHARACTER) {
 				//user removed first symbol?
-				_recoverSeed.value = currentModel.copy(
-					rawUserInput = KeysetRecoverModel.SPACE_CHARACTER.toString() + rawUserInput,
-				)
+				_recoverSeed.update {
+					it.copy(
+						rawUserInput = KeysetRecoverModel.SPACE_CHARACTER.toString() + rawUserInput,
+					)
+				}
 			} else {
 				//valid word input handling
 				if (rawUserInput.endsWith(KeysetRecoverModel.SPACE_CHARACTER)) {
@@ -54,22 +59,26 @@ class KeysetRecoverViewModel : ViewModel() {
 						onAddword(input)
 					}
 				} else if (getGuessWords(rawUserInput.trimStart()).isNotEmpty()) {
-					_recoverSeed.value = currentModel.copy(
-						rawUserInput = rawUserInput,
-					)
+					_recoverSeed.update {
+						it.copy(
+							rawUserInput = rawUserInput,
+						)
+					}
 				}
 			}
 		} else {
-			_recoverSeed.value = currentModel.copy(
-				rawUserInput = KeysetRecoverModel.SPACE_CHARACTER.toString(),
-			)
+			_recoverSeed.update {
+				it.copy(
+					rawUserInput = KeysetRecoverModel.SPACE_CHARACTER.toString(),
+				)
+			}
 		}
 	}
 
 	fun onAddword(word: String) {
-		_recoverSeed.value = _recoverSeed.value.let { model: KeysetRecoverModel ->
-			val newDraft = model.enteredWords + word
-			model.copy(
+		_recoverSeed.update {
+			val newDraft = it.enteredWords + word
+			it.copy(
 				rawUserInput = KeysetRecoverModel.SPACE_CHARACTER.toString(),
 				enteredWords = newDraft,
 				validSeed = validateSeedPhrase(newDraft),
