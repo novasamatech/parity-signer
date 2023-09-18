a9c926df9db46896032a2494f6e99cae8018f9a8
Dmitry Borodin <git@DmitryBorodin.com>
2023-09-19 00:40:22 +0200

    todo cleanup

--- android/src/main/java/io/parity/signer/bottomsheets/password/EnterPassword.kt
+++ android/src/main/java/io/parity/signer/bottomsheets/password/EnterPassword.kt
@@ -36,7 +36,7 @@ import io.parity.signer.ui.theme.red500
 import io.parity.signer.ui.theme.textTertiary
 import io.parity.signer.uniffi.MEnterPassword
 
-//todo dmitry check where it should be used - remove from here when navigation not used
+//todo dmitry export qr code
 //it's in signsufficient Crypto and transaction, as well as export qr code
 //navstate.rs:365
 //navstate.rs:473
