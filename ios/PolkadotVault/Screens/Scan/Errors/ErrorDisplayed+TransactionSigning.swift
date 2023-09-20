c93b07b5f7f6aff34bf45aa5fab9b9ac3126619c
Krzysztof Rodak <krzysztof@parity.io>
2023-09-20 07:58:16 -0700

    ios: use Localizable instead of hardcoded string inline

--- ios/PolkadotVault/Screens/Scan/Errors/ErrorDisplayed+TransactionSigning.swift
+++ ios/PolkadotVault/Screens/Scan/Errors/ErrorDisplayed+TransactionSigning.swift
@@ -29,7 +29,7 @@ extension ErrorDisplayed {
         case let .Str(errorMessage):
             return .generic(errorMessage)
         case .WrongPassword:
-            return .generic("Wrong password")
+            return .generic(Localizable.ErrorDisplayed.wrongPassword.string)
         }
     }
 }
