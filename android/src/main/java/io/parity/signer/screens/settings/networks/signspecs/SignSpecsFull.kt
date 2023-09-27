48ac67067a6cbf6d0afb5832ffe66ae2670a11ae
Dmitry Borodin <11879032+Dmitry-Borodin@users.noreply.github.com>
2023-09-27 02:49:27 -0500

    feat: Android crypto new api (#2089)
    * removed navigator not used anymore
    
    * implementation sign specs new api in progress
    
    * implementation sign specs new api in progress 2
    
    * implementation sign specs new api in progress 3
    
    * new api migrated, errors are left over
    
    * added errors for sign specs
    
    * some sign specs strings extracted
    
    * implemented errors for new crypto api
    
    * hide keyboard for create derivation flow
    
    ---------
    
    Co-authored-by: Pavel Rybalko <pavel@parity.io>

--- android/src/main/java/io/parity/signer/screens/settings/networks/signspecs/SignSpecsFull.kt
+++ android/src/main/java/io/parity/signer/screens/settings/networks/signspecs/SignSpecsFull.kt
@@ -7,7 +7,9 @@ import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import io.parity.signer.bottomsheets.password.EnterPassword
+import io.parity.signer.components.sharedcomponents.KeyCardModelBase
 import io.parity.signer.domain.Callback
+import io.parity.signer.screens.scan.errors.LocalErrorBottomSheet
 import io.parity.signer.screens.settings.networks.signspecs.view.SignSpecsListScreen
 import io.parity.signer.screens.settings.networks.signspecs.view.SignSpecsResultBottomSheet
 import io.parity.signer.ui.BottomSheetWrapperRoot
@@ -15,13 +17,15 @@ import io.parity.signer.ui.BottomSheetWrapperRoot
 
 @Composable
 fun SignSpecsFull(
-	sc: SignSpecsListModel,
+	model: SignSpecsListModel,
+	inputData: SignSpecsInput,
 	onBack: Callback
 ) {
 	val vm: SignSpecsViewModel = viewModel()
 
 	val passwordState = vm.password.collectAsStateWithLifecycle()
 	val signatureState = vm.signature.collectAsStateWithLifecycle()
+	val errorState = vm.localError.collectAsStateWithLifecycle()
 
 	val backAction = {
 		val wasState = vm.isHasStateThenClear()
@@ -30,18 +34,36 @@ fun SignSpecsFull(
 	BackHandler(onBack = backAction)
 
 	SignSpecsListScreen(
-		model = sc,
+		model = model,
 		onBack = onBack,
-		signSufficientCrypto = vm::onSignSufficientCrypto,
+		signSufficientCrypto = { key: KeyCardModelBase, addressKey64: String, hasPassword: Boolean ->
+			if (hasPassword) {
+				vm.requestPassword(key, addressKey64)
+			} else {
+				vm.onSignSpecs(inputData, key, addressKey64, null)
+			}
+		},
 		modifier = Modifier.statusBarsPadding(),
 	)
 
-	passwordState.value?.let { enterPasswordModel ->
+	errorState.value?.let { error ->
+		BottomSheetWrapperRoot(onClosedAction = vm::clearError) {
+			LocalErrorBottomSheet(
+				error = error,
+				onOk = vm::clearError,
+			)
+		}
+	} ?: passwordState.value?.let { enterPasswordModel ->
 		BottomSheetWrapperRoot(onClosedAction = vm::isHasStateThenClear) {
 			EnterPassword(
-				data = enterPasswordModel,
+				data = enterPasswordModel.model,
 				proceed = { password ->
-					vm.passwordAttempt(password)
+					vm.onSignSpecs(
+						input = inputData,
+						keyModel = enterPasswordModel.model.keyCard,
+						addressKey = enterPasswordModel.addressKey64,
+						password = password
+					)
 				},
 				onClose = vm::isHasStateThenClear,
 			)
