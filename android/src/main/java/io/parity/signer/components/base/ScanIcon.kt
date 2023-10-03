a54cbd6fa87b449480ffbd731c9cf2e8b984dadf
Dmitry Borodin <git@DmitryBorodin.com>
2023-10-03 12:36:04 +0200

    scan icon created

---
+++ android/src/main/java/io/parity/signer/components/base/ScanIcon.kt
@@ -0,0 +1,67 @@
+package io.parity.signer.components.base
+
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import io.parity.signer.R
+import io.parity.signer.domain.Callback
+import io.parity.signer.ui.theme.SignerNewTheme
+import io.parity.signer.ui.theme.pink300
+
+@Composable
+fun ScanIcon(
+	onClick: Callback,
+) {
+	Box(
+		modifier = Modifier
+			.size(72.dp)
+			.clickable(onClick = onClick)
+			.border(6.dp, MaterialTheme.colors.primary, CircleShape),
+		contentAlignment = Alignment.Center,
+	) {
+		Box(
+			modifier = Modifier
+				.background(MaterialTheme.colors.pink300, CircleShape)
+				.size(56.dp)
+		)
+		Image(
+			painter = painterResource(R.drawable.ic_qr_code_2),
+			contentDescription = stringResource(R.string.description_scan_icon),
+			colorFilter = ColorFilter.tint(MaterialTheme.colors.primary),
+			modifier = Modifier.size(32.dp)
+		)
+	}
+}
+
+
+@Preview(
+	name = "light", group = "themes", uiMode = Configuration.UI_MODE_NIGHT_NO,
+	showBackground = true, backgroundColor = 0xFFFFFFFF,
+)
+@Preview(
+	name = "dark", group = "themes", uiMode = Configuration.UI_MODE_NIGHT_YES,
+	showBackground = true, backgroundColor = 0xFF000000,
+)
+@Composable
+private fun PreviewScanIcon() {
+	SignerNewTheme {
+		Column() {
+			ScanIcon(onClick = {})
+		}
+	}
+}
