9bf9672c35c553deb8b89e56bc1a59c2754ecd5b
Krzysztof Rodak <krzysztof@parity.io>
2023-10-04 23:31:56 -0700

    ios: database version mediator and check on initial app launch

--- ios/PolkadotVault/Screens/Containers/MainScreenContainer.swift
+++ ios/PolkadotVault/Screens/Containers/MainScreenContainer.swift
@@ -13,18 +13,18 @@ struct MainScreenContainer: View {
     @StateObject var onboarding: OnboardingStateMachine
 
     var body: some View {
-//        switch viewModel.viewState {
-//        case .authenticated:
-//            AuthenticatedScreenContainer(viewModel: .init())
-//        case .deviceLocked:
-//            UnlockDeviceView(viewModel: .init())
-//        case .onboarding:
-//            onboarding.currentView()
-//        case .updateRequired:
-        ApplicationUpdateRequiredView(viewModel: .init())
-//        case .noPincode:
-//            DevicePincodeRequired(viewModel: .init())
-//        }
+        switch viewModel.viewState {
+        case .authenticated:
+            AuthenticatedScreenContainer(viewModel: .init())
+        case .deviceLocked:
+            UnlockDeviceView(viewModel: .init())
+        case .onboarding:
+            onboarding.currentView()
+        case .updateRequired:
+            ApplicationUpdateRequiredView(viewModel: .init())
+        case .noPincode:
+            DevicePincodeRequired(viewModel: .init())
+        }
     }
 }
 
@@ -41,18 +41,39 @@ extension MainScreenContainer {
         private let authenticationStateMediator: AuthenticatedStateMediator
         private let onboardingMediator: OnboardingMediator
         private let passwordProtectionStatePublisher: PasswordProtectionStatePublisher
+        private let databaseVersionMediator: DatabaseVersionMediator
         private let cancelBag = CancelBag()
         @Published var viewState: ViewState = .deviceLocked
 
         init(
             authenticationStateMediator: AuthenticatedStateMediator = ServiceLocator.authenticationStateMediator,
             onboardingMediator: OnboardingMediator = ServiceLocator.onboardingMediator,
-            passwordProtectionStatePublisher: PasswordProtectionStatePublisher = PasswordProtectionStatePublisher()
+            passwordProtectionStatePublisher: PasswordProtectionStatePublisher = PasswordProtectionStatePublisher(),
+            databaseVersionMediator: DatabaseVersionMediator = DatabaseVersionMediator()
         ) {
             self.authenticationStateMediator = authenticationStateMediator
             self.onboardingMediator = onboardingMediator
             self.passwordProtectionStatePublisher = passwordProtectionStatePublisher
-            listenToStateChanges()
+            self.databaseVersionMediator = databaseVersionMediator
+            checkInitialState()
+        }
+
+        private func checkInitialState() {
+            databaseVersionMediator.checkDatabaseScheme { result in
+                switch result {
+                case .success:
+                    self.listenToStateChanges()
+                case let .failure(error):
+                    switch error {
+                    case .invalidVersion:
+                        self.viewState = .updateRequired
+                    case let .error(serviceError):
+                        // todo what we should really do here?
+                        self.viewState = .updateRequired
+                        print(serviceError)
+                    }
+                }
+            }
         }
 
         private func listenToStateChanges() {
