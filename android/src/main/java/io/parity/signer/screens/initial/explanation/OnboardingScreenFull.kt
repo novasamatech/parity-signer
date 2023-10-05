fb728e49b4c7b413f0dc90846444adec0e8348f8
Dmitry Borodin <git@DmitryBorodin.com>
2023-10-05 11:30:09 +0200

    feat: android be able to click on left and right on corousel to navigate. THIS currently blocking skip action, do not merge like that!

--- android/src/main/java/io/parity/signer/screens/initial/explanation/OnboardingScreenFull.kt
+++ android/src/main/java/io/parity/signer/screens/initial/explanation/OnboardingScreenFull.kt
@@ -2,21 +2,28 @@ package io.parity.signer.screens.initial.explanation
 
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.captionBarPadding
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import io.parity.signer.domain.Callback
+import kotlinx.coroutines.launch
 
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun OnboardingExplanationScreenFull(navigateNext: Callback) {
+	val scope = rememberCoroutineScope()
 	Box(
 		modifier = Modifier
 			.navigationBarsPadding()
@@ -27,6 +34,7 @@ fun OnboardingExplanationScreenFull(navigateNext: Callback) {
 		val pagerState = rememberPagerState(pageCount = {
 			4
 		})
+		//actual screens
 		HorizontalPager(state = pagerState) {
 			when (it) {
 				0 -> OnboardingScreen1(navigateNext)
@@ -34,6 +42,35 @@ fun OnboardingExplanationScreenFull(navigateNext: Callback) {
 				2 -> OnboardingScreen3(navigateNext)
 				3 -> OnboardingScreen4(navigateNext)
 			}
+		}
+		//things to handle clicks for scrolling
+		Row(Modifier.fillMaxSize(1f)) {
+			Box(
+				Modifier
+					.weight(0.3f)
+					.fillMaxHeight()
+					.clickable {
+						//back action area
+						scope.launch {
+							if (pagerState.canScrollBackward) {
+								pagerState.animateScrollToPage(pagerState.currentPage - 1)
+							}
+						}
+					}
+			)
+			Box(Modifier.weight(0.6f))
+			Box(
+				Modifier
+					.weight(0.3f)
+					.fillMaxHeight()
+					.clickable {
+						//back action area
+						scope.launch {
+							if (pagerState.canScrollForward) {
+								pagerState.animateScrollToPage(pagerState.currentPage + 1)
+							}
+						}
+					})
 		}
 	}
 }
