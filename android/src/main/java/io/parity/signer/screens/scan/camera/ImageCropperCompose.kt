046384798c09ab7c84936d37710c65e3fefb2e53
Dmitry Borodin <git@DmitryBorodin.com>
2023-12-06 18:24:03 +0400

    work in progress logic to cut camera input

--- android/src/main/java/io/parity/signer/screens/scan/camera/ImageCropperCompose.kt
+++ android/src/main/java/io/parity/signer/screens/scan/camera/ImageCropperCompose.kt
@@ -1,5 +1,6 @@
 package io.parity.signer.screens.scan.camera
 
+import android.content.res.Configuration
 import android.graphics.Rect
 import androidx.camera.core.ImageProxy
 import androidx.compose.runtime.Composable
@@ -9,10 +10,27 @@ import androidx.compose.ui.unit.dp
 
 object ImageCropperCompose {
 	@Composable
-	fun makeCropRect(image: ImageProxy): Rect {
-		val configuration = LocalConfiguration.current
+	@androidx.annotation.OptIn(androidx.camera.core.ExperimentalGetImage::class)
+	fun makeCropRect(image: ImageProxy): Rect? {
+		val configuration: Configuration = LocalConfiguration.current
 		val screenHeight = configuration.screenHeightDp.dp
 		val screenWidth = configuration.screenWidthDp.dp
+
+		val (photoHeight, photoWIdth) = if (image.imageInfo.rotationDegrees == 90 || image.imageInfo.rotationDegrees == 270) {
+			// need to rotate image
+			Pair(image.image?.height, image.image?.width)
+		} else {
+			// rotation probably vertical - don't won't
+			Pair(image.image?.width, image.image?.height)
+		}
+		if (photoHeight == null || photoWIdth == null) {
+			return null
+		}
+
+		//todo dmitry check that forther logic if we changed rotation here
+
+
+
 
 			//todo dmitry calculate what to crop taking rotation into account.
 //		and move this logic into our usual cropper or even cache it.
