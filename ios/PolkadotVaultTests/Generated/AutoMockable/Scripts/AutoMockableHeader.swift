6545a431b6eca366b80416420ac5720fb62447e7
Krzysztof Rodak <krzysztof@parity.io>
2023-12-07 13:29:48 +0100

    chore: ios: Sourcery integration for automatic mocks generation for better unit testing (#2218)
    * chore: ios: Add mocks generation automation using Sourcery
    
    * chore: ios: Example mock
    
    * chore: ios: fix sourcery installation as running unit tests triggers environment sandboxing
    
    * chore: ios: Runscript improvements with output and input paths as CI doesn't pick up files due to git ignore
    
    * chore: ios: further pbxproj fix

---
+++ ios/PolkadotVaultTests/Generated/AutoMockable/Scripts/AutoMockableHeader.swift
@@ -0,0 +1,8 @@
+import LocalAuthentication
+@testable import PolkadotVault
+import UIKit
+
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable implicit_return
+// swiftlint:disable vertical_whitespace_closing_braces
+// swiftlint:disable weak_delegate
