908f49a78087992b62620b325cbc1ac2d1dd44b6
Dmitry Borodin <git@DmitryBorodin.com>
2024-01-06 16:16:10 +0100

    implemented security patch check

--- android/src/main/java/io/parity/signer/screens/initial/eachstartchecks/osversion/WrongOsVersionViewModel.kt
+++ android/src/main/java/io/parity/signer/screens/initial/eachstartchecks/osversion/WrongOsVersionViewModel.kt
@@ -2,23 +2,41 @@ package io.parity.signer.screens.initial.eachstartchecks.osversion
 
 import android.os.Build
 import androidx.lifecycle.ViewModel
+import io.parity.signer.domain.DateUtils
+import java.util.Calendar
 
 
 class WrongOsVersionViewModel() : ViewModel() {
 
 	fun isShouldShow(): Boolean {
-		return Build.VERSION.SDK_INT < MinRecommendedOsVersion
-	}
-
-	fun getCurrentOSVersion(): String {
-		return Build.VERSION.RELEASE
-	}
-
-	fun getMinRecommendedOsVersion(): String {
-		return MinRecommendedOsVersionString
+		return getVulnerabilities().isNotEmpty()
 	}
 }
 
-//todo dmitry set values given my security team
-const val MinRecommendedOsVersion: Int = Build.VERSION_CODES.O
-const val MinRecommendedOsVersionString: String = "Oreo"
+//todo dmitry use it
+private fun getVulnerabilities(): List<KnownOSIssue> {
+ return listOfNotNull(
+	 if (is20465Exposed()) KnownOSIssue.CVE_2022_20465 else null,
+ )
+}
+
+private fun is20465Exposed(): Boolean {
+	return if (Build.VERSION.SDK_INT < Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+		val fixPatch = Calendar.getInstance().also {
+			it.set(Calendar.YEAR, 2022)
+			it.set(Calendar.MONTH, Calendar.NOVEMBER)
+		}
+		if (DateUtils.parseAndroidPatchDate(Build.VERSION.SECURITY_PATCH)
+				?.before(fixPatch) == true) {
+			true
+		} else {
+			false
+		}
+	} else {
+		false
+	}
+}
+enum class KnownOSIssue {
+	CVE_2022_20465 // CVE-2022-20465 https://source.android.com/docs/security/bulletin/2022-11-01
+}
+
