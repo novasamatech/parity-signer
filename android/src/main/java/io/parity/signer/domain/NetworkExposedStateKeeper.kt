6418c931026cbb79e1e21720a6018125eb1dd2fd
Dmitry Borodin <git@DmitryBorodin.com>
2024-01-20 18:42:00 +0100

    removed outdated log

--- android/src/main/java/io/parity/signer/domain/NetworkExposedStateKeeper.kt
+++ android/src/main/java/io/parity/signer/domain/NetworkExposedStateKeeper.kt
@@ -89,7 +89,6 @@ class NetworkExposedStateKeeper(
 		val intentFilter = IntentFilter("android.hardware.usb.action.USB_STATE")
 		val receiver: BroadcastReceiver = object : BroadcastReceiver() {
 			override fun onReceive(context: Context, intent: Intent) {
-				Timber.e("TODO DMITRY", "usb broadcast")
 				reactOnUsb(intent)
 			}
 		}
