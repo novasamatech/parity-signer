9eead04d1e3f84f036731382c441032951a2faea
Stepan Lavrentev <lawrentievsv@gmail.com>
2025-02-18 12:04:57 +0300

    fix build script

--- ios/scripts/build_libsigner.sh
+++ ios/scripts/build_libsigner.sh
@@ -4,6 +4,7 @@ source $HOME/.cargo/env
 
 IOS_ARCHS=(aarch64-apple-ios x86_64-apple-ios)
 LIB_NAME=signer
+IOS_VERSION=18.2
 
 printf "Building iOS targets...";
 
@@ -15,8 +16,10 @@ cd "$(dirname "${0}")/../../rust/signer"
 for i in "${IOS_ARCHS[@]}";
 do
   rustup target add "$i";
-  env -i PATH="${PATH}" \
-  "${HOME}"/.cargo/bin/cargo build --locked --target "$i" --release --no-default-features
+  env -i PATH="${PATH}" IPHONEOS_DEPLOYMENT_TARGET="${IOS_VERSION}" \
+  "${HOME}"/.cargo/bin/cargo build --locked --target "$i" --release --no-default-features \
+  -Z build-std \
+  -- -C link-arg=-mios-version-min="${IOS_VERSION}"
 done
 
 lipo -create -output "../../ios/PolkadotVault/lib${LIB_NAME}.a" ../target/x86_64-apple-ios/release/libsigner.a ../target/aarch64-apple-ios/release/libsigner.a
