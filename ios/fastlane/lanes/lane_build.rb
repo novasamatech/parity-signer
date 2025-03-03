a48d99a8ab120555702aca84ad91e9c70c87f9d3
Stepan Lavrentev <lawrentievsv@gmail.com>
2025-03-03 11:14:46 +0300

    fix: use match to sign

--- ios/fastlane/lanes/lane_build.rb
+++ ios/fastlane/lanes/lane_build.rb
@@ -48,19 +48,26 @@ end
 
 desc "Prepares certificate and provisioning profile"
 lane :prepare_code_signing do |options|
-  api_key = lane_context[SharedValues::APP_STORE_CONNECT_API_KEY]
   app_identifier = CredentialsManager::AppfileConfig.try_fetch_value(:app_identifier)
-  profile_name = "Polkadot Vault Distribution"
 
-  cert(
-    api_key: api_key,
-    keychain_path: ENV["KEYCHAIN_PATH"],
+  if is_ci?
+    create_keychain(
+      name: "github_actions_keychain",
+      password: ENV["KEYCHAIN_PASSWORD"],
+      default_keychain: true,
+      unlock: true,
+      timeout: 3600,
+      add_to_search_list: true,
+      lock_when_sleeps: false
+    )
+  end
+
+  match(
+    type: "adhoc",
+    app_identifier: app_identifier,
+    readonly: false,
+    keychain_name: "github_actions_keychain",
     keychain_password: ENV["KEYCHAIN_PASSWORD"]
   )
-  sigh(
-    api_key: api_key,
-    app_identifier: app_identifier,
-    provisioning_name: profile_name,
-    force: false
- )
+
 end
