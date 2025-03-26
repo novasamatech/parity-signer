c184bbf63cc87c12bf881dfeaad377968ddd5454
Ruslan <emkil.russel@gmail.com>
2025-03-26 15:38:24 +0100

    feat: support signing via v15 metadata proof (#2448)
    * feat: ios: update tos and pp files
    
    * chore: ios: update Fastlane configuration
    
    * Test build (#2425)
    
    * Revert "chore(deps): bump raptorq from 1.8.1 to 2.0.0 in /rust (#2372)"
    
    This reverts commit d31f5534f1a794dbf05207eac7eadc6be03bb61c.
    
    * fix: allow ded_code
    
    * fix: permission for semantic workflow
    
    * fix: clippy
    
    * fix: change semantic permission to write
    
    * fix: change token usage
    
    * fix: pull_request event
    
    * fix: add concurency
    
    * fix: add actor
    
    * fix: lint problem
    
    * fix: change signing cecrt
    
    * fix: change appfile
    
    * change team id
    
    * fix: semantic triggers
    
    * fix: change actors
    
    * fix: remove unnecessary changes
    
    * fix: return semantic run back
    
    * fix: temp add stepanLav
    
    ---------
    
    Co-authored-by: valentunn <70131744+valentunn@users.noreply.github.com>
    
    * fix: XCode 16 build
    
    * fix: fix opencv library update
    
    * fix warning
    
    * feat: metadata proof definition and decoding
    
    * make test work
    
    * reorganize the code
    
    * linting
    
    * [wip] call parsing
    
    * [wip] state
    
    * state machine improvements
    
    * fix visitor
    
    * refactoring
    
    * add call parsing tests
    
    * call pallet and method states
    
    * improve tests
    
    * refactoring
    
    * provide type name to the field
    
    * add states for parsing account and balance
    
    * add call parsing
    
    * handle void
    
    * add proof verification
    
    * add handling mortality and genesis hash
    
    * introduce transaction with proof
    
    * fix POC
    
    * fix poc
    
    * error handling
    
    * fix tests
    
    * fix tests
    
    * move dependencies to merkleized metadata
    
    * add cards for tip, tx version and spec version
    
    * add staking tests
    
    * implement default behavior for the cards in the state trait
    
    * move long test lines into files
    
    * improve type path deriving
    
    * improve path derivation for composite
    
    * fix tests
    
    * fix linting
    
    * fix account state
    
    * fix clippy
    
    * fix clippy
    
    * fix fmt
    
    * add docs
    
    * fix doc
    
    * fix state
    
    * fix number state
    
    ---------
    
    Co-authored-by: Krzysztof Rodak <krodak.konta@gmail.com>
    Co-authored-by: Krzysztof Rodak <krzysztof@parity.io>
    Co-authored-by: Stepan Lavrentev <40560660+stepanLav@users.noreply.github.com>
    Co-authored-by: valentunn <70131744+valentunn@users.noreply.github.com>

---
+++ rust/parser/src/number_state.rs
@@ -0,0 +1,227 @@
+use crate::{
+    cards::ParserCard,
+    decoding_commons::OutputCard,
+    state::DefaultState,
+    state::{State, StateError, StateOutput},
+};
+
+use merkleized_metadata::ExtraInfo;
+
+use printing_balance::convert_balance_pretty;
+
+use num_bigint::{BigInt, BigUint};
+
+pub trait NumberCardProducing: Sync + Send + Clone {
+    fn produce_card(&self, number: String, indent: u32) -> OutputCard;
+}
+
+/// A decoding state that detects numeric values and outputs a single
+/// card, as defined by the provided `card_producer` implementation.
+///
+/// This approach allows for flexible customization of how numeric values
+/// are displayed based on their context. For example, a "tip" value shown
+/// in an extension may be rendered with a different style than a balance
+/// value inside a transfer call.
+///
+/// By abstracting card creation through `card_producer`, this state enables
+/// context-aware formatting and styling of numbers during decoding.
+#[derive(Debug, Clone)]
+pub struct NumberState<T: NumberCardProducing> {
+    pub card_producer: T,
+}
+
+impl<T: NumberCardProducing> NumberState<T> {
+    fn process_number(&self, number: String, indent: u32) -> StateOutput {
+        let card = self.card_producer.produce_card(number, indent);
+        StateOutput {
+            next_state: Box::new(DefaultState),
+            cards: vec![card],
+            indent,
+        }
+    }
+}
+
+impl<T: NumberCardProducing + 'static> State for NumberState<T> {
+    fn clone_box(&self) -> Box<dyn State> {
+        Box::new(self.clone())
+    }
+
+    fn process_u8(&self, input: u8, indent: u32) -> Result<StateOutput, StateError> {
+        let output = self.process_number(input.to_string(), indent);
+
+        Ok(output)
+    }
+
+    fn process_u16(&self, input: u16, indent: u32) -> Result<StateOutput, StateError> {
+        let output = self.process_number(input.to_string(), indent);
+
+        Ok(output)
+    }
+
+    fn process_u32(&self, input: u32, indent: u32) -> Result<StateOutput, StateError> {
+        let output = self.process_number(input.to_string(), indent);
+
+        Ok(output)
+    }
+
+    fn process_u64(&self, input: u64, indent: u32) -> Result<StateOutput, StateError> {
+        let output = self.process_number(input.to_string(), indent);
+
+        Ok(output)
+    }
+
+    fn process_u128(&self, input: u128, indent: u32) -> Result<StateOutput, StateError> {
+        let output = self.process_number(input.to_string(), indent);
+
+        Ok(output)
+    }
+
+    fn process_u256(&self, input: &[u8; 32], indent: u32) -> Result<StateOutput, StateError> {
+        let target_value = BigUint::from_bytes_le(input);
+        let output = self.process_number(target_value.to_string(), indent);
+
+        Ok(output)
+    }
+
+    fn process_i8(&self, input: i8, indent: u32) -> Result<StateOutput, StateError> {
+        let output = self.process_number(input.to_string(), indent);
+
+        Ok(output)
+    }
+
+    fn process_i16(&self, input: i16, indent: u32) -> Result<StateOutput, StateError> {
+        let output = self.process_number(input.to_string(), indent);
+
+        Ok(output)
+    }
+
+    fn process_i32(&self, input: i32, indent: u32) -> Result<StateOutput, StateError> {
+        let output = self.process_number(input.to_string(), indent);
+
+        Ok(output)
+    }
+
+    fn process_i64(&self, input: i64, indent: u32) -> Result<StateOutput, StateError> {
+        let output = self.process_number(input.to_string(), indent);
+
+        Ok(output)
+    }
+
+    fn process_i128(&self, input: i128, indent: u32) -> Result<StateOutput, StateError> {
+        let output = self.process_number(input.to_string(), indent);
+
+        Ok(output)
+    }
+
+    fn process_i256(&self, input: &[u8; 32], indent: u32) -> Result<StateOutput, StateError> {
+        let target_value = BigInt::from_signed_bytes_le(input);
+        let output = self.process_number(target_value.to_string(), indent);
+
+        Ok(output)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct BalanceCardProducer(pub ExtraInfo);
+
+impl NumberCardProducing for BalanceCardProducer {
+    fn produce_card(&self, number: String, indent: u32) -> OutputCard {
+        let balance_output = convert_balance_pretty(&number, self.0.decimals, &self.0.token_symbol);
+
+        OutputCard {
+            card: ParserCard::Balance {
+                number: balance_output.number,
+                units: balance_output.units,
+            },
+            indent,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct NonceCardProducer;
+
+impl NumberCardProducing for NonceCardProducer {
+    fn produce_card(&self, number: String, indent: u32) -> OutputCard {
+        OutputCard {
+            card: ParserCard::Nonce(number),
+            indent,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct TxVersionCardProducer;
+
+impl NumberCardProducing for TxVersionCardProducer {
+    fn produce_card(&self, number: String, indent: u32) -> OutputCard {
+        OutputCard {
+            card: ParserCard::TxVersion(number),
+            indent,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct SpecVersionCardProducer(pub ExtraInfo);
+
+impl NumberCardProducing for SpecVersionCardProducer {
+    fn produce_card(&self, number: String, indent: u32) -> OutputCard {
+        OutputCard {
+            card: ParserCard::NetworkNameVersion {
+                name: self.0.spec_name.clone(),
+                version: number,
+            },
+            indent,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct TipCardProducer(pub ExtraInfo);
+
+impl NumberCardProducing for TipCardProducer {
+    fn produce_card(&self, number: String, indent: u32) -> OutputCard {
+        let balance_output = convert_balance_pretty(&number, self.0.decimals, &self.0.token_symbol);
+
+        OutputCard {
+            card: ParserCard::Tip {
+                number: balance_output.number,
+                units: balance_output.units,
+            },
+            indent,
+        }
+    }
+}
+
+impl<T: NumberCardProducing> NumberState<T> {
+    pub fn balance_state(extra_info: ExtraInfo) -> NumberState<BalanceCardProducer> {
+        NumberState {
+            card_producer: BalanceCardProducer(extra_info),
+        }
+    }
+
+    pub fn nonce_state() -> NumberState<NonceCardProducer> {
+        NumberState {
+            card_producer: NonceCardProducer,
+        }
+    }
+
+    pub fn tx_version_state() -> NumberState<TxVersionCardProducer> {
+        NumberState {
+            card_producer: TxVersionCardProducer,
+        }
+    }
+
+    pub fn spec_version_state(extra_info: ExtraInfo) -> NumberState<SpecVersionCardProducer> {
+        NumberState {
+            card_producer: SpecVersionCardProducer(extra_info),
+        }
+    }
+
+    pub fn tip_state(extra_info: ExtraInfo) -> NumberState<TipCardProducer> {
+        NumberState {
+            card_producer: TipCardProducer(extra_info),
+        }
+    }
+}
