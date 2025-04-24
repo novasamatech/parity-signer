9a1fda76e374e86a5f82bbbf78a3064126726cf5
kogeler <25884155+kogeler@users.noreply.github.com>
2025-04-24 22:39:13 +0300

    fix

--- ios/fastlane/lanes/lane_firebase.rb
+++ ios/fastlane/lanes/lane_firebase.rb
@@ -1,6 +1,7 @@
 desc "Submit a new build to Firebase"
 lane :upload_firebase do
 	firebase_app_distribution(
+		app: ENV["FIREBASE_APP_ID"],
     groups: ENV["FIREBASE_GROUPS"],
     release_notes: ENV["FIREBASE_RELEASE_NOTES"]
   )
