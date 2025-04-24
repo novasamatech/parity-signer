468b270a32c574a8bf5060db15ddd15634d5dd52
kogeler <25884155+kogeler@users.noreply.github.com>
2025-04-24 23:24:42 +0300

    fix

--- ios/fastlane/lanes/lane_firebase.rb
+++ ios/fastlane/lanes/lane_firebase.rb
@@ -3,6 +3,7 @@ lane :upload_firebase do
 	firebase_app_distribution(
 		app: ENV["FIREBASE_APP_ID"],
     groups: ENV["FIREBASE_GROUPS"],
-    release_notes: ENV["FIREBASE_RELEASE_NOTES"]
+    release_notes: ENV["FIREBASE_RELEASE_NOTES"],
+    service_credentials_file: ENV["FIREBASE_SERVICE_CREDENTIALS_FILE"]
   )
 end
