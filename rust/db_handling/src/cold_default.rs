9f1dc1844cbe15050e3a5e222f4dc7d09d5c2472
Russel <emkil.russel@gmail.com>
2025-06-27 18:09:24 +0200

    fix spelling

--- rust/db_handling/src/cold_default.rs
+++ rust/db_handling/src/cold_default.rs
@@ -309,7 +309,7 @@ pub fn populate_cold_nav_test_with_ethereum_based_networks(database: &sled::Db) 
     populate_all_network_specs(database)
 }
 
-/// Generate database with networks specs both for substrate and evm
+/// Generate database with networks specs both for substrate and ethereum
 pub fn populate_all_network_specs(database: &sled::Db) -> Result<()> {
     use constants::{METATREE, SETTREE, SPECSTREE, VERIFIERS};
 
