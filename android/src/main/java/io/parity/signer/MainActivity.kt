66854781bf6d1b0000c114e86f5624907d28a1e9
antonijzelinskij <antonijzelinskij@gmail.com>
2025-12-11 14:56:00 +0100

    Removed unused imports

--- android/src/main/java/io/parity/signer/MainActivity.kt
+++ android/src/main/java/io/parity/signer/MainActivity.kt
@@ -5,14 +5,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.ui.Modifier
-import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import io.parity.signer.dependencygraph.ServiceLocator
 import io.parity.signer.domain.addVaultLogger
